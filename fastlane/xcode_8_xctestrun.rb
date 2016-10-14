5dbce8fda8b532a85c0d685f5677b45f52bc21b9
bootstraponline <code@bootstraponline.com>
2016-10-14 17:24:54 -0400

    Use Runner.xcodebuild instead of custom action (#34)

--- fastlane/xcode_8_xctestrun.rb
+++ fastlane/xcode_8_xctestrun.rb
@@ -3,36 +3,30 @@ require 'scan'
 
 module Xcode8
   class << self
+    attr_accessor :runner
+
+    def xcodebuild(args)
+      raise 'nil runner' unless runner
+      runner.execute_action(:xcodebuild, ::Fastlane::Actions::XcodebuildAction, [args], custom_dir: nil)
+    end
+
     # args :scheme - required
     def _xcodebuild(command_name, args)
       raise 'missing opts' unless args
       raise 'opts must be a hash' unless args.is_a?(Hash)
 
-      workspace = args.fetch(:workspace, 'AllTheThings.xcworkspace ')
+      workspace = args.fetch(:workspace, 'AllTheThings.xcworkspace')
       scheme = args.fetch(:scheme)
       destination = args.fetch(:destination, 'platform=iOS Simulator,name=iPhone 6,OS=latest')
       derived_data = args.fetch(:derived_data, 'xctestrun')
 
-      command = [
-          'set -o pipefail &&',
-          'xcodebuild',
-          "-workspace #{workspace}",
-          "-scheme #{scheme}",
-          "-destination '#{destination}'",
-          "-derivedDataPath '#{derived_data}'",
-          "#{command_name} | xcpretty"
-      ].join(' ')
-
-      Fastlane::Actions.execute_action(command_name) do
-        Dir.chdir(File.join(__dir__, '..')) do # 'fastlane' is the default cwd. change to parent dir.
-          FastlaneCore::CommandExecutor.execute(command: command,
-                                                print_all: true,
-                                                print_command: true,
-                                                error: proc do |output|
-                                                  ::Scan::ErrorHandler.handle_build_error(output)
-                                                end)
-        end # Dir.chdir
-      end # execute_action
+      xcodebuild(
+          workspace: workspace,
+          scheme: scheme,
+          destination: destination,
+          derivedDataPath: derived_data,
+          xcargs: command_name
+      )
     end # def _xcodebuild
 
     # Executes xcodebuild build-for-testing
