19c7571e2154837f68107f02ba0a265e3506b771
Layne Moseley <laynemoseley@users.noreply.github.com>
2017-01-23 15:22:19 -0700

    The test target runs now, yay (#203)
    * The test target runs now, yay
    
    * Add a dummy test

---
+++ Teacher/TeachTests/TeachTests.swift
@@ -0,0 +1,24 @@
+//
+//  TeachTests.swift
+//  TeachTests
+//
+//  Created by Layne Moseley on 1/19/17.
+//  Copyright © 2017 Instructure. All rights reserved.
+//
+
+import XCTest
+
+class TeachTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testTestsWork() {
+        XCTAssertTrue(true)
+    }
+}
