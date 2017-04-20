7b359b1a1f7f3179a974031e263fb03e19541d53
bootstraponline <code@bootstraponline.com>
2017-04-20 15:37:16 -0400

    Retry jest tests (#453)

--- rn/Teacher/ios/buddybuild_prebuild.sh
+++ rn/Teacher/ios/buddybuild_prebuild.sh
@@ -62,7 +62,8 @@ fi
 pushd ../
 yarn run lint
 yarn run flow
-yarn run test
+# jest tests are flaky so we retry them.
+retry_command yarn run test
 
 if [ "$BUDDYBUILD_BASE_BRANCH" != "" ]; then
     aws s3 cp s3://inseng-code-coverage/ios-teacher/coverage/coverage-summary.json ./coverage-summary-develop.json
