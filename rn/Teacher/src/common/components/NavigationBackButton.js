8fbfc14bb771da4e80e91c83f88984599291863a
Layne Moseley <laynemoseley@users.noreply.github.com>
2017-05-02 14:06:14 -0600

    [MBL-7667] Fix accessibility for a back button (#509)
    * update accessibilty on back button
    
    * Use the correct traits
    
    * Update snaps

--- rn/Teacher/src/common/components/NavigationBackButton.js
+++ rn/Teacher/src/common/components/NavigationBackButton.js
@@ -4,11 +4,12 @@ import React from 'react'
 import { View, Image, StyleSheet } from 'react-native'
 import Button from 'react-native-button'
 import Images from '../../images'
+import i18n from 'format-message'
 
 export default class NavigationBackButton extends React.Component {
   render (): React.Element<View> {
     return <Button style={styles.backButton} {...this.props}>
-            <View style={{ paddingRight: 31 }}>
+            <View style={{ paddingRight: 31 }} accessible={true} accessibilityTraits={'button'} accessibilityLabel={i18n('Back')}>
               <Image source={Images.backIcon} style={styles.navButtonImage} />
             </View>
            </Button>
