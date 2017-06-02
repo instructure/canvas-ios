79dab66808cececf4596e982c58e83e4a2791cff
Nate Armstrong <natearmstrong2@gmail.com>
2017-06-02 14:48:56 -0600

    Version persisted store (#616)

--- rn/Teacher/src/redux/middleware/__tests__/persist.test.js
+++ rn/Teacher/src/redux/middleware/__tests__/persist.test.js
@@ -2,19 +2,24 @@
 
 import { AsyncStorage } from 'react-native'
 import mockStore from '../../../../test/helpers/mockStore'
+import { hydrateStoreFromPersistedState } from '../persist'
 
 jest.mock('AsyncStorage', () => ({
   getItem: jest.fn(),
   setItem: jest.fn(),
+  removeItem: jest.fn(),
+  getAllKeys: jest.fn(),
+  multiRemove: jest.fn(),
 }))
 jest.mock('../../../api/session')
 
 let wait = () => new Promise(resolve => setTimeout(resolve, 1))
 
 describe('persistMiddleware', () => {
-  let store = mockStore()
+  let store
   beforeEach(() => {
     jest.resetAllMocks()
+    store = mockStore()
   })
 
   it('persists into async storage', async () => {
@@ -25,5 +30,21 @@ describe('persistMiddleware', () => {
 
     await wait()
     expect(AsyncStorage.setItem).toHaveBeenCalled()
+  })
+
+  it('removes old states', async () => {
+    AsyncStorage.getItem = jest.fn(() => null)
+    AsyncStorage.getAllKeys = jest.fn(() => {
+      return ['redux.state.1.0', 'something-else']
+    })
+    AsyncStorage.multiRemove = jest.fn()
+    AsyncStorage.setItem = jest.fn()
+    store.dispatch({
+      type: 'action',
+      payload: {},
+    })
+
+    await hydrateStoreFromPersistedState(store)
+    expect(AsyncStorage.multiRemove).toHaveBeenCalledWith(['redux.state.1.0'])
   })
 })
--- rn/Teacher/src/redux/middleware/persist.js
+++ rn/Teacher/src/redux/middleware/persist.js
@@ -4,6 +4,19 @@ import { getSession } from '../../api/session'
 import type { MiddlewareAPI } from 'redux'
 import hydrate from '../hydrate-action'
 
+const STORE_VERSION = '1'
+
+function storeName (session: Session): string {
+  return `redux.state.${session.user.id}.${STORE_VERSION}`
+}
+
+async function removeOldStates (session: Session) {
+  await AsyncStorage.multiRemove(
+    (await AsyncStorage.getAllKeys())
+      .filter(k => k.startsWith(`redux.state.${session.user.id}`))
+  )
+}
+
 async function persistState (store) {
   let state = store.getState()
   let session = getSession()
@@ -16,13 +29,16 @@ async function persistState (store) {
     state,
   }
 
-  await AsyncStorage.setItem(`redux.state.${session.user.id}`, JSON.stringify(cache))
+  await AsyncStorage.setItem(storeName(session), JSON.stringify(cache))
 }
 
 export async function hydrateStoreFromPersistedState (store: any): any {
   let session = getSession()
   if (session) {
-    let cachedState = await AsyncStorage.getItem(`redux.state.${session.user.id}`)
+    let cachedState = await AsyncStorage.getItem(storeName(session))
+    if (!cachedState) {
+      await removeOldStates(session)
+    }
     let appState
     try {
       appState = JSON.parse(cachedState)
