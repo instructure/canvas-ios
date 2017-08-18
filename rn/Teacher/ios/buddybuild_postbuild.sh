14d381f3cb7c6fd5d873636866102508e01ec030
Layne Moseley <laynemoseley@users.noreply.github.com>
2017-08-18 10:09:29 -0600

    Moved uploading source maps out of Xcode (#937)
    * Update version string
    
    * Fix the bundle script for archive builds
    
    * Move the upload source maps outside of xcode

--- rn/Teacher/ios/buddybuild_postbuild.sh
+++ rn/Teacher/ios/buddybuild_postbuild.sh
@@ -5,3 +5,9 @@ TEACHER_APP_ID=58b0b2116096900100863eb8
 if [ "$BUDDYBUILD_BRANCH" = "develop" ] && [ "$BUDDYBUILD_APP_ID" = "$TEACHER_APP_ID" ]; then
     aws s3 sync ../coverage s3://inseng-code-coverage/ios-teacher/coverage
 fi
+
+if [ "$SHOULD_UPLOAD_JS_SOURCE_MAPS" = "YES" ]; then
+    version=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "./Teacher/Info.plist")
+    echo "Uploading source maps for version $version"
+    node ../node_modules/bugsnag-sourcemaps/cli.js upload --api-key 671fc47c3812aa323469568f33244e2c --app-version ${version} --minified-file main.jsbundle --source-map main.jsbundle.map --minified-url main.jsbundle --upload-sources --overwrite
+fi
