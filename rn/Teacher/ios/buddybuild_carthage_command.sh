c284d8da8c500acc7aff49ff7740490e43cb425a
bootstraponline <code@bootstraponline.com>
2017-08-31 18:04:18 -0400

    Carthage must be at project level (#976)

--- rn/Teacher/ios/buddybuild_carthage_command.sh
+++ rn/Teacher/ios/buddybuild_carthage_command.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 # https://docs.buddybuild.com/builds/dependencies/carthage.html
+# Note: This file must exist next to the project.
+# The custom carthage command will be ignored when located at the repo root.
 cd "$BUDDYBUILD_WORKSPACE"
 carthage checkout --no-use-binaries --use-ssh
