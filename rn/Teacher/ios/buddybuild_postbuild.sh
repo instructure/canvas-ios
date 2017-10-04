e9df8f2c107d29a06ff9e111943a65511db35785
Layne Moseley <laynemoseley@users.noreply.github.com>
2017-10-04 13:34:59 -0600

    Only upload test results if we are on the right scheme (#1084)

--- rn/Teacher/ios/buddybuild_postbuild.sh
+++ rn/Teacher/ios/buddybuild_postbuild.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-TEACHER_APP_ID=58b0b2116096900100863eb8
-
-if [ "$BUDDYBUILD_BRANCH" = "develop" ] && [ "$BUDDYBUILD_APP_ID" = "$TEACHER_APP_ID" ]; then
+if [ "$BUDDYBUILD_BRANCH" = "develop" ] && [ "$BUDDYBUILD_SCHEME" != "Teacher - BB - Jest" ]; then
     aws s3 sync ../coverage s3://inseng-code-coverage/ios-teacher/coverage
 fi
