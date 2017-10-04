ebb71fa349e6ca30d65c8f9acfc849ed884fa2b3
Layne Moseley <laynemoseley@users.noreply.github.com>
2017-10-04 14:17:14 -0600

    Jest upload scheme (#1085)
    * Only upload test results if we are on the right scheme
    
    * Whoops, what even is !=

--- rn/Teacher/ios/buddybuild_postbuild.sh
+++ rn/Teacher/ios/buddybuild_postbuild.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-if [ "$BUDDYBUILD_BRANCH" = "develop" ] && [ "$BUDDYBUILD_SCHEME" != "Teacher - BB - Jest" ]; then
+if [ "$BUDDYBUILD_BRANCH" = "develop" ] && [ "$BUDDYBUILD_SCHEME" = "Teacher - BB - Jest" ]; then
     aws s3 sync ../coverage s3://inseng-code-coverage/ios-teacher/coverage
 fi
