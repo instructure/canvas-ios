4b4d08e34dc3e2b78f1a926d9d638b4c3dbe4234
Derrick Hathaway <derr@me.com>
2017-12-04 09:32:46 -0700

    Restrict access to modules tab if hidden (#1262)

---
+++ CanvasCore/CanvasCore/Session/Session+ContentAccess.swift
@@ -0,0 +1,65 @@
+//
+//  Session+ModulesTab.swift
+//  CanvasCore
+//
+//  Created by Derrick Hathaway on 11/28/17.
+//  Copyright © 2017 Instructure, Inc. All rights reserved.
+//
+
+import Foundation
+
+@objc
+public class ContentAccess: NSObject {
+    public enum Access {
+        case granted
+        case denied(String)
+    }
+    public let access: Access
+    
+    public init(_ access: Access) {
+        self.access = access
+    }
+    
+    @objc
+    public var accessDeniedMessage: String? {
+        guard case let .denied(reason) = access else {
+            return nil
+        }
+        return reason
+    }
+}
+
+extension Session {
+    @objc
+    public func accessForContent(with url: URL) -> ContentAccess {
+        guard let context = ContextID(url: url) else {
+            return ContentAccess(.granted)
+        }
+
+        // default to granting access since we'v never restricted access in past
+        guard let enrollmentRoles = enrollmentsDataSource[context]?.roles else {
+            return ContentAccess(.granted)
+        }
+        
+        if enrollmentRoles.contains(.Teacher)
+            || enrollmentRoles.contains(.TA)
+            || enrollmentRoles.contains(.Designer) {
+            return ContentAccess(.granted)
+        }
+
+        let pageAccessDenied = NSLocalizedString(
+            "That page has been disabled for this course",
+            tableName: "Localizable",
+            bundle: .core,
+            value: "",
+            comment: "")
+        
+        if url.path.contains("/modules") {
+            guard let tab = (try? Tab.modulesTab(for: context, in: self)).flatMap({ $0 }), !tab.hidden else {
+                return ContentAccess(.denied(pageAccessDenied))
+            }
+        }
+        
+        return ContentAccess(.granted)
+    }
+}
