6a014ef557b21623cd8a00b16a445c6e8e24d5ef
Layne Moseley <laynemoseley@users.noreply.github.com>
2018-01-05 15:20:24 -0700

    Fix issues with bugsnag upload (#1367)

--- Canvas/buddybuild_postbuild.sh
+++ Canvas/buddybuild_postbuild.sh
@@ -4,6 +4,11 @@ if [ "$SHOULD_UPLOAD_JS_SOURCE_MAPS" = "YES" ]; then
   version=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "./Canvas/Info.plist")
   buildnumber=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "./Canvas/Info.plist")
   appversion="$version-$buildnumber"
-  echo "Uploading source maps for $appversion"
+  echo "sourcemap upload for $appversion"
+  cd $BUDDYBUILD_WORKSPACE
+  cd rn/Teacher
+  echo "Generating sourcemap"
+  react-native bundle --platform ios --dev false --entry-file index.ios.js --bundle-output main.jsbundle --sourcemap-output main.jsbundle.map
+  echo "Uploading sourcemap"
   node ../rn/Teacher/node_modules/bugsnag-sourcemaps/cli.js upload --api-key ef596806053571e7315440f6d8d36bca --app-version ${appversion} --minified-file main.jsbundle --source-map main.jsbundle.map --minified-url main.jsbundle --upload-sources --overwrite
 fi
