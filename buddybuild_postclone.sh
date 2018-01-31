8fddb43126bdad67e97e35c29fa01a19e777508b
Taylor Wilson <rolyatwilson@users.noreply.github.com>
2018-01-31 12:52:28 -0700

    Remove Fastlane and fix AWSCLI upgrade in postclone script (#1443)
    * Fix awscli upgrade logic
    
    * Farewell Fastlane
    
    * Remove awscli install because coverage uploads happen on Bitrise
    
    * Remove unneeded teacher build id

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 
-TEACHER_UI_JOB_ID=58c981b73749ca0100c1abb3
-
-if [[ "$BUDDYBUILD_BRANCH" = "develop" ]]; then
-    # install the aws cli
-    pip install --upgrade --user awscli
-fi
-
 cd rn/Teacher
 yarn install
