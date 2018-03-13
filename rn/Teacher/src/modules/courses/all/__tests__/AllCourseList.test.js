81365e0244a39d69b6d4d220e79b76fa75e865cd
vtsoup <17718811+vtsoup@users.noreply.github.com>
2018-03-13 16:34:00 -0600

    [MBL-9782] Comment text changes (#1626)
    * Comment text changes
    
    * Update snapshot

--- rn/Teacher/src/modules/courses/all/__tests__/AllCourseList.test.js
+++ rn/Teacher/src/modules/courses/all/__tests__/AllCourseList.test.js
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2016-present Instructure, Inc.
+// Copyright (C) 2018-present Instructure, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -25,7 +25,7 @@ const template = {
 }
 
 describe('AllCourseList', () => {
-  it('refresh courses when empty', () => {
+  it('refreshes courses when empty', () => {
     const refreshCourses = jest.fn()
     const refreshProps = {
       courses: [],
