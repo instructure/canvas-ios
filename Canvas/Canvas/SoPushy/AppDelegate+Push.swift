2619d40d40a4cd5077447336a253d1f2d520e02e
Layne Moseley <laynemoseley@users.noreply.github.com>
2018-04-05 16:42:12 -0600

    Fix some bad swift code in routeToPushNotificationPayloadURL (#1761)

--- Canvas/Canvas/SoPushy/AppDelegate+Push.swift
+++ Canvas/Canvas/SoPushy/AppDelegate+Push.swift
@@ -21,29 +21,28 @@ import CanvasKeymaster
 
 extension AppDelegate {
     func routeToPushNotificationPayloadURL(_ payload: [AnyHashable: Any]) {
-        if let urlString: NSString = payload["html_url"] as! NSString? {
-            let actualURLString: String = swappedPushBetaURLString((urlString as String)) ?? (urlString as String)
-            if let notificationURL = URL(string: actualURLString) {
-                self.openCanvasURL(notificationURL)
-            }
+        guard let urlString = payload["html_url"] as? String else { return }
+        let actualURLString = swappedPushBetaURLString(urlString) ?? urlString
+        if let notificationURL = URL(string: actualURLString) {
+            self.openCanvasURL(notificationURL)
         }
     }
     
     fileprivate func swappedPushBetaURLString(_ urlString: String) -> String? {
-        if let baseURLString = CanvasKeymaster.the().currentClient?.baseURL?.absoluteString {
-            if baseURLString.range(of: "beta") != nil { // if we are currently in beta, the payload's url doesn't have it (a known issue) so we need to swap it out
-                let nonBetaBaseURLString = baseURLString.replacingOccurrences(of: ".beta", with: "")
-                // Get each host, compare. If they are the same then we are good to add in the .beta to the url
-                if let baseURLComponents = URLComponents(string: baseURLString), let nonBetaBaseURLComponents = URLComponents(string: nonBetaBaseURLString), var actualURLComponents = URLComponents(string: (urlString as String)) {
-                    if let baseURLHost = baseURLComponents.host, let nonBetaBaseURLHost = nonBetaBaseURLComponents.host, let actualURLHost = actualURLComponents.host {
-                        if nonBetaBaseURLHost == actualURLHost { // if the stripped "beta" stripped base url == the url given in the push, do the swap with the one containing the "beta"
-                            actualURLComponents.host = baseURLHost
-                            return actualURLComponents.string
-                        }
-                    }
-                }
-            }
+        guard let baseURLString = CanvasKeymaster.the().currentClient?.baseURL?.absoluteString else { return nil }
+        // if we are currently in beta, the payload's url doesn't have it (a known issue) so we need to swap it out
+        guard let _ = baseURLString.range(of: "beta") else { return nil }
+        
+        let nonBetaBaseURLString = baseURLString.replacingOccurrences(of: ".beta", with: "")
+        // Get each host, compare. If they are the same then we are good to add in the .beta to the url
+        guard let baseURLComponents = URLComponents(string: baseURLString), let nonBetaBaseURLComponents = URLComponents(string: nonBetaBaseURLString), var actualURLComponents = URLComponents(string: urlString) else { return nil }
+        guard let baseURLHost = baseURLComponents.host, let nonBetaBaseURLHost = nonBetaBaseURLComponents.host, let actualURLHost = actualURLComponents.host else { return nil }
+        
+        if nonBetaBaseURLHost == actualURLHost { // if the stripped "beta" stripped base url == the url given in the push, do the swap with the one containing the "beta"
+            actualURLComponents.host = baseURLHost
+            return actualURLComponents.string
         }
+        
         return nil
     }
 }
