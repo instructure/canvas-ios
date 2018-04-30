09964cc0a3a189517e8918e594fe65aa629c7f0f
Layne Moseley <laynemoseley@users.noreply.github.com>
2018-04-30 15:36:10 -0600

    CanvasPlayground deep linking list (#1864)
    * Add deep linking for student
    
    * Fix warnings

--- rn/CanvasPlayground/App.js
+++ rn/CanvasPlayground/App.js
@@ -1,21 +1,8 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React from 'react'
+import { Text, View } from 'react-native'
 import { TabNavigator, StackNavigator } from 'react-navigation'; // Version can be specified in package.json
-
-class DeepLinkingScreen extends React.Component {
-
-  static navigationOptions = {
-    title: 'Deep Linking',
-  };
-
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>This is where deep linking will go.</Text>
-      </View>
-    );
-  }
-}
+import DeepLinkingScreen from './DeepLinking'
+import { Ionicons } from '@expo/vector-icons'
 
 const DeepLinkStack = StackNavigator({
   'Deep Linking': { screen: DeepLinkingScreen },
@@ -23,4 +10,11 @@ const DeepLinkStack = StackNavigator({
 
 export default TabNavigator({
   'Deep Linking': { screen: DeepLinkStack },
-});
+},
+{
+  navigationOptions: ({ navigation }) => ({
+    tabBarIcon: ({ focused, tintColor }) => {
+      return <Ionicons name={'ios-link-outline'} size={25} color={tintColor} />
+    },
+  }),
+})
---
+++ rn/CanvasPlayground/DeepLinking.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import { 
+  Text,
+  View,
+  FlatList,
+  TouchableHighlight,
+  Alert,
+  TextInput,
+  LayoutAnimation,
+  AsyncStorage,
+  StyleSheet,
+  Linking,
+  Button,
+} from 'react-native';
+
+export default class DeepLinkingScreen extends React.Component {
+
+  state = {
+    items: [],
+  }
+
+  static navigationOptions = {
+    title: 'Deep Linking',
+  };
+
+  componentDidMount = async () => {
+    let data = await AsyncStorage.getItem('route-items')
+    let items = JSON.parse(data || []).filter((item) => !!item)
+    this.setState({ items })
+  }
+
+  onPress = (item) => {
+    let url = `canvas-student://${item}`
+    Linking.openURL(url)
+  }
+
+  onBlur = () => {
+    LayoutAnimation.easeInEaseOut()
+    let items = [...this.state.items, this.state.text]
+    this.setState({
+      text: null,
+      items,
+    })
+    AsyncStorage.setItem('route-items', JSON.stringify(items))
+  }
+
+  deleteItem = (item) => {
+    let items = this.state.items.filter((thing) => thing !== item)
+    this.setState({ items })
+    AsyncStorage.setItem('route-items', JSON.stringify(items))
+  }
+
+  renderItem = ({ item }) => {
+    return (<TouchableHighlight onPress={() => this.onPress(item)}>
+              <View style={{ backgroundColor: 'white', padding: 8, flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Text numberOfLines={0}>{item}</Text>
+                <Button title={'Delete'} onPress={() => this.deleteItem(item) } />
+              </View>
+            </TouchableHighlight>)
+  }
+
+  renderSeperator = () => {
+    return <View style={{ height: StyleSheet.hairlineWidth, backgroundColor: 'lightgrey' }} />
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1, backgroundColor: 'white' }}>
+        <View>
+          <TextInput
+            style={{ height: 34, padding: 8 }}
+            onChangeText={(text) => this.setState({text})}
+            value={this.state.text}
+            placeholder={'Enter a new route'}
+            onBlur={this.onBlur}
+            autoCapitalize={'none'}
+          />
+        </View>
+        <Text style={{ padding: 8 }}>Saved Routes:</Text>
+        <FlatList 
+          data={this.state.items}
+          renderItem={this.renderItem}
+          ItemSeparatorComponent={this.renderSeperator}
+          ListFooterComponent={this.renderSeperator} />
+      </View>
+    )
+  }
+}
