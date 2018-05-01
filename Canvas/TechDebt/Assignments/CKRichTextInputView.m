9bb45cca918eac5086ed5d5fe0c82a20d50c3078
Andy VanWagoner <andy@thetalecrafter.com>
2018-05-01 13:20:20 -0600

    [MBL-10368] Fix flash of bad placeholder (#1867)
    Move placeholder text setting to setup, instead of waiting for the webview to
    be finished loading.
    
    Test Plan:
    - open an assignment with a text submission
    - tap Turn In
    - verify the placeholder text is immediately "Enter Submission", and there is
      no flash of other text like "Label".

--- Canvas/TechDebt/Assignments/CKRichTextInputView.m
+++ Canvas/TechDebt/Assignments/CKRichTextInputView.m
@@ -155,6 +155,8 @@ UIColor *CKPostButtonDisabledColor() {
     
     self.imageCache = [[NSCache alloc] init];
     loadingImages = [NSMutableDictionary new];
+    
+    self.placeholderLabel.text = _placeholderText;
 }
 
 - (CKAllowedAttachmentType)allowedAttachmentTypes
@@ -442,8 +444,6 @@ UIColor *CKPostButtonDisabledColor() {
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView
 {
-    self.placeholderLabel.text = _placeholderText;
-
     // This is to get around a nasty web view issue where initially text wasn't pasteable, so to get around that you set and unset text...
     NSString *js = [NSString stringWithFormat:@"setInitialText('%@');", @"INITIAL SET"];
     [self.webView stringByEvaluatingJavaScriptFromString:js];
