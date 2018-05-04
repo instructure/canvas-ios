ad54d9fef8e0e65068e7ed156ec66b74b234507e
Layne Moseley <laynemoseley@users.noreply.github.com>
2018-05-04 09:55:01 -0600

    Let HelmNavigationController have sweet sweet interactive pop gestures (#1872)

--- CanvasCore/CanvasCore/Helm/HelmNavigationController.swift
+++ CanvasCore/CanvasCore/Helm/HelmNavigationController.swift
@@ -33,6 +33,11 @@ public class HelmNavigationController: UINavigationController {
         view.backgroundColor = .white
     }
     
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        self.interactivePopGestureRecognizer?.delegate = self
+    }
+    
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -46,3 +51,9 @@ public class HelmNavigationController: UINavigationController {
     }
 }
 
+extension HelmNavigationController: UIGestureRecognizerDelegate {
+    
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+}
