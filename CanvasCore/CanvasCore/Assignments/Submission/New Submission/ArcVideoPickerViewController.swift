5467972b9e858a4f6083f77801b5435ded43265b
Layne Moseley <laynemoseley@users.noreply.github.com>
2018-05-10 12:54:58 -0600

    [MBL-10494 MBL-10495] - Fix arc uploads (#1897)
    * Use UIWebView instead of WKWebView for arc submissions
    
    * Cleanup. Fixes: MBL-10494 for the short term
    
    * No more silly html scraping
    
    * Cleanup and comments

--- CanvasCore/CanvasCore/Assignments/Submission/New Submission/ArcVideoPickerViewController.swift
+++ CanvasCore/CanvasCore/Assignments/Submission/New Submission/ArcVideoPickerViewController.swift
@@ -23,24 +23,20 @@ public class ArcVideoPickerViewController: UIViewController {
     let arcLTIURL: URL
     let videoPickedAction: (URL)->()
     
-    private let webView: WKWebView
-    fileprivate var loaded = false
-    
+    // Reverting back to UIWebView because can actually get the request httpBody, WKWebView didn't work for that
+    private let webView: UIWebView
     
     /// Initializer
     ///
     /// - parameter arcLTIURL:          The arc lti launch URL. See `Assignment`s function to create one.
     /// - parameter videoPickedAction:  The handler to be executed after picking a video. This class dismisses itself automatically prior to calling this handler.
     public init(arcLTIURL: URL, videoPickedAction: @escaping (URL)->() = { _ in }) {
-        let config = WKWebViewConfiguration()
-        config.websiteDataStore = WKWebsiteDataStore.default()
-        
-        self.webView = WKWebView(frame: CGRect.zero, configuration: config)
+        self.webView = UIWebView(frame: .zero)
         self.arcLTIURL = arcLTIURL
         self.videoPickedAction = videoPickedAction
         super.init(nibName: nil, bundle: nil)
         
-        webView.navigationDelegate = self
+        webView.delegate = self
     }
     
     required public init?(coder aDecoder: NSCoder) {
@@ -58,18 +54,15 @@ public class ArcVideoPickerViewController: UIViewController {
         webView.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor).isActive = true
         webView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         webView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-    }
-    
-    override public func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+        
         // https://mobiledev.instructure.com/courses/24219/external_tools/282032/resource_selection?launch_type=homework_submission&assignment_id=405404
         APIBridge.shared().call("getAuthenticatedSessionURL", args: [arcLTIURL.absoluteString]) { [weak self] response, error in
             if let data = response as? [String: Any],
                 let sessionURL = data["session_url"] as? String,
                 let url = URL(string: sessionURL) {
-                self?.webView.load(URLRequest(url: url))
+                self?.webView.loadRequest(URLRequest(url: url))
             } else if let url = self?.arcLTIURL {
-                self?.webView.load(URLRequest(url: url))
+                self?.webView.loadRequest(URLRequest(url: url))
             }
         }
     }
@@ -79,57 +72,31 @@ public class ArcVideoPickerViewController: UIViewController {
     }
 }
 
-extension ArcVideoPickerViewController: WKNavigationDelegate {
-    
-    // For those of you who think this is hack:
-    // It totally is. Deal with it.
-    // Why? When selecting a video from the device, Arc is doing (or iOS) some sort of refresh of the page. That
-    // Was causing the upload to cancel too soon. So, this is what I could figure out to prevent that.
-    public func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-        if loaded && navigationAction.request.url == arcLTIURL {
-            decisionHandler(.cancel)
-        } else {
-            decisionHandler(.allow)
-        }
-    }
-    
-    public func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
-        if !loaded && navigationResponse.response.url == arcLTIURL {
-            loaded = true
-        }
+extension ArcVideoPickerViewController: UIWebViewDelegate {
+    public func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
         
-        if navigationResponse.response.url?.absoluteString.contains("success/external_tool_dialog") == true {
-            webView.evaluateJavaScript("document.documentElement.outerHTML.toString()") { (html: Any?, error: Error?) in
-
-                // The video url is in the redirect's page's content, in between a @id":" marker and the following comma
-                let mark = "@id&quot;:&quot;"
-                if let html = html as? String, let startIndex = html.range(of: mark) {
-                    let startFRD = startIndex.upperBound
-                    let r = Range(uncheckedBounds: (lower: startFRD, upper: html.endIndex))
-                    if let nextCommaRange = html.range(of: "&quot;,", options: [], range: r, locale: nil) {
-                        let dirtyURLRange = Range(uncheckedBounds: (lower: startFRD, upper: nextCommaRange.lowerBound))
-                        let dirtyURLString = URL(string: html.substring(with: dirtyURLRange))?.absoluteString
-                        if let dirtyURLString = dirtyURLString, let cleanURLString = String(htmlEncodedString: dirtyURLString), let url = URL(string: cleanURLString) {
-                            self.close()
-                            self.videoPickedAction(url)
-                        }
-                    }
+        if request.url?.absoluteString.contains("success/external_tool_dialog") == true {
+            // This is a poor mans way to parse a form body. It works though, and works well!
+            // It uses a fake base url and then puts the form body as the query string.
+            // URLComponents will then parse the query string correctly!
+            if let data = request.httpBody,
+                let dataString = String(data: data, encoding: String.Encoding.utf8) {
+                let fakeBaseURL = "http://base.url?\(dataString)"
+                let components = URLComponents(string: fakeBaseURL)
+                let contentItems = components?.queryItems?.filter { $0.name == "content_items" }
+                if let jsonData = contentItems?.first?.value?.data(using: .utf8),
+                    let json = (try? JSONSerialization.jsonObject(with: jsonData, options: [])) as? [String: Any],
+                    let graph = json["@graph"] as? [[String: Any]],
+                    let urlString = graph.first?["url"] as? String,
+                    let url = URL(string: urlString) {
+                    
+                    self.close()
+                    self.videoPickedAction(url)
+                    return false
                 }
             }
         }
         
-        decisionHandler(.allow)
+        return true
     }
 }
-
-extension String {
-    init?(htmlEncodedString: String) {
-        let encodedData = htmlEncodedString.data(using: String.Encoding.utf8)!
-        let attributedOptions: [String: Any] = [
-            NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType,
-        ]
-        guard let attributedString = try? NSAttributedString(data: encodedData, options: attributedOptions, documentAttributes: nil) else { return nil }
-        self.init(attributedString.string)
-    }
-}
-
