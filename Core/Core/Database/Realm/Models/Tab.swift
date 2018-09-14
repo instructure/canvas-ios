77caad5c9e12c2a8c744d0bd142578eb257a4383
G Richards <bluwave@users.noreply.github.com>
2018-09-14 15:15:10 -0600

    initial commit GroupNavigation (#2273)
    refs: none
    affects: none
    release note: none
    test plan: make sure it builds

---
+++ Core/Core/Database/Realm/Models/Tab.swift
@@ -0,0 +1,30 @@
+//
+// Copyright (C) 2018-present Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+import Foundation
+import RealmSwift
+
+public class Tab: Object {
+    @objc public dynamic var id: String = ""
+    @objc public dynamic var htmlUrl: String?
+    @objc public dynamic var fullUrl: String?
+    @objc public dynamic var label: String = ""
+    @objc public dynamic var position: Int = 0
+
+    override public class func primaryKey() -> String? {
+        return #keyPath(Tab.id)
+    }
+}
---
+++ Student/Student/Groups/GroupNavigation/GroupNavigationTableViewController.swift
@@ -0,0 +1,47 @@
+//
+// Copyright (C) 2018-present Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+import UIKit
+
+class GroupNavigationTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension GroupNavigationTableViewController {
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+}
