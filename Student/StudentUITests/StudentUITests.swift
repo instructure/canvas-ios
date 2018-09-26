b9ac4492ef899143cf1696bb5357d0af28ec42e5
bootstraponline <code@bootstraponline.com>
2018-09-26 15:09:40 -0400

    EarlGrey v2 (#2313)

---
+++ Student/StudentUITests/StudentUITests.swift
@@ -0,0 +1,33 @@
+//
+// Copyright (C) 2018-present Instructure, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import XCTest
+
+class StudentUITestsEG2: XCTestCase {
+
+    override func setUp() {
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+
+    override func tearDown() {
+    }
+
+    func testExample() {
+      EarlGrey.selectElement(with: grey_keyWindow())
+        .perform(grey_tap())
+    }
+}
