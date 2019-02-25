255cc09cb6095c0baa24a72f8da36d6174ae6f39
Nate Armstrong <natearmstrong2@gmail.com>
2019-02-25 15:39:15 -0700

    Place teacher modules behind a feature flag
    [ignore-commit-lint]
    
    We don't want to accidentally release this.

--- rn/Teacher/src/common/feature-flags.js
+++ rn/Teacher/src/common/feature-flags.js
@@ -43,7 +43,8 @@ export type FeatureFlagName = 'favoriteGroups' |
                               'newGroupNavigation' |
                               'simpleDiscussionRenderer' |
                               'newStudentAssignmentView' |
-                              'conferences'
+                              'conferences' |
+                              'modules'
 
 // if a feature is listed here it will be turned off
 // unless in development, the current user is on a domain
@@ -55,6 +56,7 @@ export const featureFlags: { [FeatureFlagName]: FeatureFlag } = {
   simpleDiscussionRenderer: {},
   newStudentAssignmentView: {},
   conferences: {},
+  modules: {},
 }
 
 var enableAllFeatureFlags = false
