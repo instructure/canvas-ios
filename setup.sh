f32cf37f9b7c9223829072d466b3364504f69b29
bootstraponline <matt@bootstraponline.com>
2019-03-08 12:50:13 -0500

    Commit Cartfile with pspdfkit approved for open source

--- setup.sh
+++ setup.sh
@@ -1,9 +1,3 @@
-if [ ! -f Cartfile ]; then
-  cp Cartfile.example Cartfile
-fi
-if [ ! -f Cartfile.resolved ]; then
-  cp Cartfile.resolved.example Cartfile.resolved
-fi
 carthage bootstrap
 
 pushd
