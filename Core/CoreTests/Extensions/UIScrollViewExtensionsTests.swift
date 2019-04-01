d8047c316e887f0c5609dda381e9ad900b418626
Nate Armstrong <natearmstrong2@gmail.com>
2019-04-01 15:25:35 -0600

    One more test for good measure

--- Core/CoreTests/Extensions/UIScrollViewExtensionsTests.swift
+++ Core/CoreTests/Extensions/UIScrollViewExtensionsTests.swift
@@ -37,5 +37,6 @@ class UIScrollViewExtensionsTests: XCTestCase {
 
         scrollView.contentOffset.y = 200
         XCTAssertTrue(scrollView.isBottomReached())
+        XCTAssertTrue(scrollView.isBottomReached(threshold: 0))
     }
 }
