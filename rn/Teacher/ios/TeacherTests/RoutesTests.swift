9040ac2ceb6334993164cbbdcedc6a79a612f664
Nate Armstrong <natearmstrong2@gmail.com>
2019-04-15 09:12:37 -0600

    Add RouteTests

---
+++ rn/Teacher/ios/TeacherTests/RoutesTests.swift
@@ -0,0 +1,37 @@
+//
+// Copyright (C) 2019-present Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+import Foundation
+import XCTest
+@testable import Teacher
+
+class RoutesTests: XCTestCase {
+    func testRouteSendsNotification() {
+        let route = URLComponents(string: "https://canvas.instructure.com/api/v1/courses/1")!
+        let expectation = self.expectation(description: "route notification")
+        let name = NSNotification.Name("route")
+        var userInfo: [AnyHashable: Any]?
+        let observer = NotificationCenter.default.addObserver(forName: name, object: nil, queue: nil) { note in
+            userInfo = note.userInfo
+            expectation.fulfill()
+        }
+        router.route(to: route, from: UIViewController(), options: nil)
+        wait(for: [expectation], timeout: 0.5)
+        XCTAssertNotNil(userInfo)
+        XCTAssertEqual(userInfo?["url"] as? String, route.url!.absoluteString)
+        NotificationCenter.default.removeObserver(observer)
+    }
+}
