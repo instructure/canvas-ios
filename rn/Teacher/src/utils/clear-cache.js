bf3e4c111c9ba2829a7a884332fa3ebd93e3eb8f
Nate Armstrong <natearmstrong2@gmail.com>
2019-04-19 13:37:03 -0600

    Clear caches directory
    refs: MBL-12261
    affects: teacher, student
    release note: none

---
+++ rn/Teacher/src/utils/clear-cache.js
@@ -0,0 +1,40 @@
+//
+// Copyright (C) 2017-present Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+// @flow
+
+
+import { AsyncStorage } from 'react-native'
+import {
+  unlink,
+  mkdir,
+  exists,
+  CachesDirectoryPath,
+} from 'react-native-fs'
+
+export default async function clearCache () {
+  const key = 'lastClearCacheDate'
+  const date = await AsyncStorage.getItem(key)
+  if (date) return
+  if (await exists(CachesDirectoryPath) == false) {
+    await mkdir(CachesDirectoryPath)
+    return
+  }
+  await unlink(CachesDirectoryPath)
+  await mkdir(CachesDirectoryPath)
+  await AsyncStorage.setItem(key, Date())
+}
+
