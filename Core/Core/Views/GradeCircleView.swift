fa2ad32e79d8dafe42f6da2d0df44a1028ec2a6d
Matthew Sessions <me@matthewsessions.com>
2019-05-06 19:03:50 -0400

    Move GradeCircle to Core (#158)
    * Move GradeCircle to Core
    
    refs: MBL-12314
    affects: Student
    release note: none
    
    * Update headers
    
    * Remove self

---
+++ Core/Core/Views/GradeCircleView.swift
@@ -0,0 +1,128 @@
+//
+// Copyright (C) 2019-present Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+import UIKit
+
+public class GradeCircleView: UIView {
+    @IBOutlet weak var circlePoints: UILabel!
+    @IBOutlet weak var circleLabel: UILabel!
+    @IBOutlet weak var circleComplete: UIImageView!
+    @IBOutlet weak var gradeCircle: GradeCircle?
+    @IBOutlet weak var displayGrade: UILabel!
+    @IBOutlet weak var outOfLabel: UILabel!
+    @IBOutlet weak var latePenaltyLabel: UILabel!
+    @IBOutlet weak var finalGradeLabel: UILabel!
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadFromXib()
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        loadFromXib()
+    }
+
+    public func update(_ assignment: Assignment) {
+        // in this case the submission should always be there because canvas generates
+        // submissions for every user for every assignment but just in case
+        guard let submission = assignment.submission, submission.workflowState != .unsubmitted else {
+            isHidden = true
+            return
+        }
+
+        guard submission.grade != nil else {
+            isHidden = true
+            return
+        }
+
+        isHidden = false
+
+        let isPassFail = assignment.gradingType == .pass_fail
+        circlePoints.isHidden = isPassFail
+        circleLabel.isHidden = isPassFail
+        circleComplete.isHidden = isPassFail ? (submission.score == nil || submission.score == 0) : true
+
+        // Update grade circle
+        if let score = submission.score, let pointsPossible = assignment.pointsPossible {
+            circlePoints.text = NumberFormatter.localizedString(from: NSNumber(value: score), number: .decimal)
+            gradeCircle?.progress = score / pointsPossible
+
+            gradeCircle?.accessibilityLabel = assignment.scoreOutOfPointsPossibleText
+        }
+
+        circleLabel.text = assignment.pointsText
+
+        // Update the display grade
+        displayGrade.isHidden = assignment.gradingType == .points || submission.late == true
+        displayGrade.text = assignment.gradeText
+
+        // Update the outOf label
+        outOfLabel.text = assignment.outOfText
+
+        // Update the Late penalty and Final Grade
+        latePenaltyLabel.isHidden = true
+        finalGradeLabel.isHidden = true
+        if assignment.hasLatePenalty {
+            latePenaltyLabel.isHidden = false
+            finalGradeLabel.isHidden = false
+
+            latePenaltyLabel.text = assignment.latePenaltyText
+            finalGradeLabel.text = assignment.finalGradeText
+        }
+    }
+}
+
+public class GradeCircle: UIView {
+    public var progress: Double? {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+
+    override public func draw(_ rect: CGRect) {
+        drawRing(progress: 1, color: UIColor.named(.borderMedium).cgColor)
+        drawRing(progress: progress ?? 0, color: Brand.shared.primary.ensureContrast(against: .named(.backgroundLightest)).cgColor)
+    }
+
+    func drawRing(progress: Double, color: CGColor) {
+        let halfSize: CGFloat = min(bounds.size.width/2, bounds.size.height/2)
+        let desiredLineWidth: CGFloat = 3
+
+        // The circle path is done in radians and radians do not start at the
+        // top of the circle, they start on the right of the circle. Our progress
+        // starts at the top so we subtract pi/2 to shift the circle by 90 degrees
+        let start = 0 - (Double.pi / 2)
+        let end = (Double.pi * 2 * progress) - (Double.pi / 2)
+
+        let circlePath = UIBezierPath(
+            arcCenter: CGPoint(x: halfSize, y: halfSize),
+            radius: CGFloat(halfSize - (desiredLineWidth/2)),
+            startAngle: CGFloat(start),
+            endAngle: CGFloat(end),
+            clockwise: true
+        )
+
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = circlePath.cgPath
+
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeColor = color
+        shapeLayer.lineWidth = desiredLineWidth
+
+        layer.addSublayer(shapeLayer)
+    }
+}
---
+++ Core/CoreTests/Views/GradeCircleViewTests.swift
@@ -0,0 +1,160 @@
+//
+// Copyright (C) 2019-present Instructure, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import XCTest
+@testable import Core
+
+class GradeCircleViewTests: XCTestCase {
+    var view: GradeCircleView!
+
+    override func setUp() {
+        super.setUp()
+        view = GradeCircleView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+    }
+
+    func testItHidesWhenNoSubmission() {
+        let a = Assignment.make()
+        view.update(a)
+        XCTAssertTrue(view.isHidden)
+    }
+
+    func testItHidesWhenSubmissionIsUnsubmitted() {
+        let a = Assignment.make()
+        a.submission = Submission.make(["workflowStateRaw": "unsubmitted"])
+        view.update(a)
+        XCTAssertTrue(view.isHidden)
+    }
+
+    func testItHidesWhenNoGrade() {
+        let a = Assignment.make()
+        a.submission = Submission.make([
+            "workflowStateRaw": "submitted",
+            "grade": nil,
+        ])
+        view.update(a)
+        XCTAssertTrue(view.isHidden)
+    }
+
+    func testItShowsCorrectViewsForPassFail() {
+        let a = Assignment.make(["gradingTypeRaw": "pass_fail"])
+        a.submission = Submission.make([
+            "workflowStateRaw": "graded",
+            "grade": "Complete",
+            "scoreRaw": nil,
+        ])
+        view.update(a)
+        XCTAssertTrue(view.circlePoints.isHidden)
+        XCTAssertTrue(view.circleLabel.isHidden)
+        XCTAssertTrue(view.circleComplete.isHidden)
+
+        a.submission?.score = 10
+        view.update(a)
+        XCTAssertFalse(view.circleComplete.isHidden)
+
+        a.submission?.score = 0
+        view.update(a)
+        XCTAssertTrue(view.circleComplete.isHidden)
+    }
+
+    func testItShowsCorrectViewsForNonPassFail() {
+        let a = Assignment.make(["gradingTypeRaw": "points"])
+        a.submission = Submission.make([
+            "workflowStateRaw": "graded",
+            "grade": "10",
+            "scoreRaw": 10,
+        ])
+        view.update(a)
+        XCTAssertFalse(view.circlePoints.isHidden)
+        XCTAssertFalse(view.circleLabel.isHidden)
+        XCTAssertTrue(view.circleComplete.isHidden)
+    }
+
+    func testItUpdatesCircle() {
+        let a = Assignment.make([
+            "gradingTypeRaw": "points",
+            "pointsPossibleRaw": 100,
+        ])
+        a.submission = Submission.make([
+            "workflowStateRaw": "graded",
+            "scoreRaw": 80,
+            "grade": "80",
+        ])
+        view.update(a)
+        XCTAssertEqual(view.circlePoints.text, "80")
+        XCTAssertEqual(view.gradeCircle?.progress, 0.8)
+        XCTAssertEqual(view.gradeCircle?.accessibilityLabel, "80 out of 100 points possible")
+    }
+
+    func testItShowsLatePenalty() {
+        let a = Assignment.make([
+            "gradingTypeRaw": "points",
+            "pointsPossibleRaw": 100,
+        ])
+        a.submission = Submission.make([
+            "workflowStateRaw": "graded",
+            "scoreRaw": 80,
+            "grade": "80",
+            "late": true,
+            "pointsDeductedRaw": 10,
+        ])
+        view.update(a)
+        XCTAssertFalse(view.latePenaltyLabel.isHidden)
+        XCTAssertFalse(view.finalGradeLabel.isHidden)
+        XCTAssertEqual(view.latePenaltyLabel.text, "Late penalty (-10 pts)")
+        XCTAssertEqual(view.finalGradeLabel.text, "Final Grade: 80 pts")
+    }
+
+    func testDisplayGrade() {
+        let a = Assignment.make([
+            "gradingTypeRaw": "points",
+            "pointsPossibleRaw": 100,
+        ])
+        a.submission = Submission.make([
+            "workflowStateRaw": "graded",
+            "scoreRaw": 80,
+            "grade": "80",
+        ])
+        view.update(a)
+        XCTAssertTrue(view.displayGrade.isHidden)
+
+        a.gradingType = .gpa_scale
+        a.submission?.grade = "3.8"
+        view.update(a)
+        XCTAssertFalse(view.displayGrade.isHidden)
+        XCTAssertEqual(view.displayGrade.text, "3.8 GPA")
+
+        a.submission?.late = true
+        view.update(a)
+        XCTAssertTrue(view.displayGrade.isHidden)
+    }
+
+    func testItRendersGradeInformation() {
+        let a = Assignment.make([
+            "gradingTypeRaw": "points",
+            "pointsPossibleRaw": 100,
+        ])
+        a.submission = Submission.make([
+            "workflowStateRaw": "graded",
+            "scoreRaw": 80,
+            "grade": "80",
+        ])
+        view.update(a)
+        XCTAssertEqual(view.circleLabel.text, "Points")
+        XCTAssertEqual(view.outOfLabel.text, "Out of 100 pts")
+        XCTAssertTrue(view.latePenaltyLabel.isHidden)
+        XCTAssertTrue(view.finalGradeLabel.isHidden)
+    }
+}
