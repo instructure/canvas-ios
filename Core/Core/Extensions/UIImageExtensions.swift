3b78ff4f44acbfd0f5c8b441172faf4bb913d3d4
Nate Armstrong <natearmstrong2@gmail.com>
2019-05-29 08:19:10 -0600

    Use solid icons for selected tab bar items (#230)
    refs: MBL-12378
    affects: student
    release note: Accessibility improvements

--- Core/Core/Extensions/UIImageExtensions.swift
+++ Core/Core/Extensions/UIImageExtensions.swift
@@ -19,7 +19,8 @@ import Foundation
 
 extension UIImage {
     public enum IconName: String, CaseIterable {
-        case attendance, collaborations, conferences, todo, cameraSolid
+        case attendance, cameraSolid, collaborations, conferences
+        case dashboardCustomSolid, todo, todoSolid
         case addAudioLine, addCameraLine, addDocumentLine, addImageLine, addVideoCameraLine
         case publish, unpublish
         case warning
