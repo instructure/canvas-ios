3e905f7e092285ef23c472cbab9812d88208600e
Gabriel Palmer <gabriel.blane.palmer@gmail.com>
2019-06-11 15:58:00 -0600

    Added UI test to login dashboard

--- Canvas/CanvasUITests/Login/LoginTests.swift
+++ Canvas/CanvasUITests/Login/LoginTests.swift
@@ -16,3 +16,42 @@
 
 import XCTest
 import SwiftUITest
+
+class LoginTests: XCTestCase {
+    func testLoginToDashboard() {
+        let app = XCUIApplication()
+        app.launch()
+        let driver = DriverFactory.getXCUITestDriver(app, testCase: self)
+
+        // Find my school
+        let button = driver.find(label: "Find my school")
+        XCTAssert(button.exists)
+        button.tap()
+        let textField = driver.find(id: "LoginFindSchool.searchField")
+        textField.typeText("iosauto\r")
+
+        // Email
+        let email = app.webViews.textFields["Email"]
+        XCTAssert(email.waitForExistence(timeout: 10))
+        XCTAssert(email.isHittable)
+        email.tap()
+        email.typeText("student1")
+
+        // Password
+        let password = app.webViews.secureTextFields["Password"]
+        XCTAssert(password.waitForExistence(timeout: 10))
+        XCTAssert(password.isHittable)
+        password.tap()
+        password.typeText("password")
+
+        // Submit
+        let submit = app.webViews.buttons["Log In"]
+        submit.tap()
+
+        // Dashboard
+        XCTAssert(driver.find(label: "Courses").exists)
+        XCTAssert(driver.find(id: "course-247").exists)
+        XCTAssert(driver.find(label: "Login ").exists)
+        XCTAssert(driver.find(label: "Dashboard").exists)
+    }
+}
