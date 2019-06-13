16b1cf22be07ae1a77edee4d0d6c80647609825f
GabrielPalmer <42744107+GabrielPalmer@users.noreply.github.com>
2019-06-13 12:28:07 -0600

    Added test for maintaining session after termination (#306)
    refs: MBL-12530
    affects: student
    release none: none

--- Canvas/CanvasUITests/Login/LoginTests.swift
+++ Canvas/CanvasUITests/Login/LoginTests.swift
@@ -136,6 +136,23 @@ class LoginTests: CanvasUITests {
         XCTAssert(LoginStart.previousUser(studentNumber: "Two").exists)
     }
 
+    func testSessionMaintainedAfterTermination() {
+        loginUser(username: "student1", password: "password")
+
+        // Dashboard
+        XCTAssert(Dashboard.courses.exists)
+        XCTAssert(Dashboard.courseCard(id: "247").exists)
+        XCTAssert(Dashboard.dashboardTab.exists)
+
+        XCUIApplication().terminate()
+        XCUIApplication().launch()
+
+        // Dashboard
+        XCTAssert(Dashboard.courses.exists)
+        XCTAssert(Dashboard.courseCard(id: "247").exists)
+        XCTAssert(Dashboard.dashboardTab.exists)
+    }
+
     func loginUser(username: String, password: String) {
         // Find my school
         LoginStart.findMySchool.tap()
