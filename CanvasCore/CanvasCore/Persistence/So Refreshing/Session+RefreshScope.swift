3844924a6f6acdd0ac3e8bd77ef34ebfaec1f4b0
inst-danger <ios@instructure.com>
2019-07-09 09:14:27 -0600

    update headers

--- CanvasCore/CanvasCore/Persistence/So Refreshing/Session+RefreshScope.swift
+++ CanvasCore/CanvasCore/Persistence/So Refreshing/Session+RefreshScope.swift
@@ -1,23 +1,20 @@
 //
-// Copyright (C) 2016-present Instructure, Inc.
+// This file is part of Canvas.
+// Copyright (C) 2016-present  Instructure, Inc.
 //
 // This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, version 3 of the License.
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
 //
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// GNU Affero General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
-    
-    
-
-
-
 
 internal let SoRefreshingStoreID = StoreID(storeName: "SoRefreshing", modelFileName: "SoRefreshing", modelFileBundle: Bundle(for: Refresh.self), localizedErrorDescription: NSLocalizedString("Error loading cache management database.", tableName: "Localizable", bundle: .core, value: "", comment: "error message for when the cache management database fails to load"))
 
--- rn/Teacher/rn-mods.sh
+++ rn/Teacher/rn-mods.sh
@@ -1,3 +1,21 @@
+#
+# This file is part of Canvas.
+# Copyright (C) 1086-present  Instructure, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 # see https://github.com/facebook/react-native/issues/10865#issuecomment-302661876
 # and here https://github.com/facebook/react-native/pull/10941
 # Apparently this is a hot debate in the rn community :)
