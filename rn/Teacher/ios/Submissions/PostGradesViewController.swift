9dc88453131aa427bd7950ac99d8963764fd9db2
Garrett Richards <garrettrichards@gmail.com>
2019-08-08 15:32:09 -0600

    MBL-11702 - UI for manual post grades screen refs: MBL-11702 affects: Teacher release note: none

---
+++ rn/Teacher/ios/Submissions/PostGradesViewController.swift
@@ -0,0 +1,144 @@
+//
+// This file is part of Canvas.
+// Copyright (C) 2019-present  Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import UIKit
+import Core
+
+class PostGradesViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+
+    private enum Row: Int, CaseIterable {
+        case postTo, section
+    }
+
+    private var sections: [String] = []
+    private var showSections: Bool = false
+    private var sectionToggles: [Bool] = []
+    private var gradesCurrentlyHidden = 0
+
+    static func create() -> PostGradesViewController {
+        let controller = loadFromStoryboard()
+        return controller
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTableView()
+        setupSections()
+    }
+
+    func setupTableView() {
+        tableView.register(SectionCell.self, forCellReuseIdentifier: String(describing: SectionCell.self))
+        tableView.register(PostToCell.self, forCellReuseIdentifier: String(describing: PostToCell.self))
+    }
+
+    func setupSections() {
+        sections = ["section A", "section B"]     // FIXME: remove these static sections once we have real sections
+        sectionToggles = Array(repeating: false, count: sections.count)
+    }
+}
+
+extension PostGradesViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Row.allCases.count +  (showSections ? sections.count : 0)
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell
+        if indexPath.row == 0 {
+            cell = tableView.dequeue(for: indexPath) as PostToCell
+        } else {
+            cell = tableView.dequeue(for: indexPath) as SectionCell
+        }
+
+        if indexPath.row < Row.allCases.count { //  first two rows (Row enum)
+            if let row = Row(rawValue: indexPath.row) {
+                switch row {
+                case .postTo:
+                    cell.textLabel?.text = NSLocalizedString("Post to...", comment: "")
+                    cell.detailTextLabel?.text = NSLocalizedString("Everyone", comment: "")
+                    cell.accessoryType = .disclosureIndicator
+                    cell.selectionStyle = .default
+                case .section:
+                    cell.textLabel?.text = NSLocalizedString("Specific Sections", comment: "")
+                    cell.selectionStyle = .none
+                    if let cell = cell as? SectionCell {
+                        cell.toggle.removeTarget(self, action: #selector(actionDidToggleShowSections(sender:)), for: UIControl.Event.valueChanged)
+                        cell.toggle.isOn = showSections
+                        cell.toggle.addTarget(self, action: #selector(actionDidToggleShowSections(sender:)), for: UIControl.Event.valueChanged)
+                    }
+                }
+            }
+        } else {    //  sections
+            let index = indexPath.row - Row.allCases.count
+            cell.textLabel?.text = sections[index]
+            cell.selectionStyle = .none
+            if let cell = cell as? SectionCell {
+                cell.toggle.removeTarget(self, action: #selector(actionDidToggleSection(toggle:)), for: UIControl.Event.valueChanged)
+                cell.toggle.isOn = sectionToggles[index]
+                cell.toggle.tag = index
+                cell.toggle.addTarget(self, action: #selector(actionDidToggleSection(toggle:)), for: UIControl.Event.valueChanged)
+            }
+        }
+
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            let localized = NSLocalizedString("%d grades currently hidden", comment: "number of grades hidden")
+            return String(format: localized, gradesCurrentlyHidden)
+        }
+        return nil
+    }
+
+    @objc
+    func actionDidToggleShowSections(sender: UISwitch) {
+        showSections = sender.isOn
+        tableView.reloadData()
+    }
+
+    @objc
+    func actionDidToggleSection(toggle: UISwitch) {
+        sectionToggles[toggle.tag] = toggle.isOn
+    }
+
+    class SectionCell: UITableViewCell {
+        var toggle: UISwitch
+
+        override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+            toggle = UISwitch(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+            super.init(style: style, reuseIdentifier: reuseIdentifier)
+            accessoryView = toggle
+        }
+
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+    }
+
+    class PostToCell: UITableViewCell {
+        override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+            super.init(style: .value1, reuseIdentifier: reuseIdentifier)
+        }
+
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+    }
+}
