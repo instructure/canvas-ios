7c9f2a72f0e3eefa8f80ce1cc250acdee64afc9f
Garrett Richards <garrettrichards@gmail.com>
2019-08-09 16:57:22 -0600

    add combined settings screen

--- rn/Teacher/ios/Submissions/PostSettingsViewController.swift
+++ rn/Teacher/ios/Submissions/PostSettingsViewController.swift
@@ -16,4 +16,127 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
-import Foundation
+import UIKit
+import Core
+
+class PostSettingsViewController: UIViewController {
+    @IBOutlet weak var menu: HorizontalMenuView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var menuHeight: NSLayoutConstraint!
+    private var postGradesViewController: PostGradesViewController!
+    private var hideGradesViewController: HideGradesViewController!
+
+    enum MenuItem: Int {
+        case post, hide
+    }
+
+    static func create() -> PostSettingsViewController {
+        let controller = loadFromStoryboard()
+        return controller
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = NSLocalizedString("Post Settings", comment: "")
+        configureMenu()
+        configurePost()
+        configureHide()
+        scrollView.delegate = self
+    }
+
+    func configureMenu() {
+        menu.delegate = self
+        menuHeight.constant = 1.0 / UIScreen.main.scale
+    }
+
+    func configurePost() {
+        postGradesViewController = PostGradesViewController.create()
+        embed(postGradesViewController, in: scrollView) { child, _ in
+            child.view.addConstraintsWithVFL("H:|[view(==superview)]")
+            child.view.addConstraintsWithVFL("V:|[view(==superview)]|")
+        }
+    }
+
+    func configureHide() {
+        hideGradesViewController = HideGradesViewController.create()
+        embed(hideGradesViewController, in: scrollView) { [weak self] child, _ in
+            guard let post = self?.postGradesViewController.view else { return }
+            child.view.addConstraintsWithVFL("H:[post][view(==superview)]|", views: ["post": post])
+            child.view.addConstraintsWithVFL("V:|[view(==superview)]|")
+        }
+    }
+
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        let ratio = self.scrollView.contentOffsetRatio
+        coordinator.animate(alongsideTransition: { [weak self] _ in
+            ratio.x >= 0.5 ? self?.showHide() : self?.showPost()
+            self?.menu.reload()
+            }, completion: nil)
+    }
+
+    func showPost() {
+        scrollView.scrollRectToVisible(postGradesViewController.view.frame, animated: true)
+    }
+
+    func showHide() {
+        scrollView.scrollRectToVisible(hideGradesViewController.view.frame, animated: true)
+    }
+}
+
+extension PostSettingsViewController: UIScrollViewDelegate {
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        if hideGradesViewController.view.frame.contains(scrollView.contentOffset) {
+            menu.selectMenuItem(at: IndexPath(row: MenuItem.hide.rawValue, section: 0), animated: true)
+        } else {
+            menu.selectMenuItem(at: IndexPath(row: MenuItem.post.rawValue, section: 0), animated: true)
+        }
+    }
+}
+
+extension PostSettingsViewController: HorizontalMenuDelegate {
+    func accessibilityIdentifier(at: IndexPath) -> String {
+        guard let menuItem = MenuItem(rawValue: at.row) else { return "" }
+        var identifier: String
+        switch menuItem {
+        case .post: identifier = "post"
+        case .hide: identifier = "hide"
+        }
+        return "PostSettings.\(identifier)MenuItem"
+    }
+
+    var selectedColor: UIColor? {
+        return .named(.electric)
+    }
+
+    var maxItemWidth: CGFloat {
+        return 200
+    }
+
+    var measurementFont: UIFont {
+        return .scaledNamedFont(.semibold16)
+    }
+
+    func menuItemCount() -> Int {
+        return 2
+    }
+
+    func menuItemTitle(at: IndexPath) -> String {
+        guard let menuItem = MenuItem(rawValue: at.row) else { return "" }
+        switch menuItem {
+        case .post:
+            return NSLocalizedString("Post Grades", comment: "")
+        case .hide:
+            return NSLocalizedString("Hide Grades", comment: "")
+        }
+    }
+
+    func didSelectItem(at: IndexPath) {
+        guard let item = MenuItem(rawValue: at.row) else { return }
+        switch item {
+        case .post:
+            showPost()
+        case .hide:
+            showHide()
+        }
+    }
+}
