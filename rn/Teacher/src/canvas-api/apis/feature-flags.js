6d6b5f61c2f51a543b6222b653bdb087d30d5272
Nate Armstrong <natearmstrong2@gmail.com>
2019-08-19 08:57:07 -0600

    Update header

--- rn/Teacher/src/canvas-api/apis/feature-flags.js
+++ rn/Teacher/src/canvas-api/apis/feature-flags.js
@@ -1,4 +1,7 @@
 //
+// This file is part of Canvas.
+// Copyright (C) 2019-present  Instructure, Inc.
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
 // published by the Free Software Foundation, either version 3 of the
