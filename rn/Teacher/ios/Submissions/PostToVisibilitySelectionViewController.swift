12e71d2ef139c1e124195b55e7e299b2443657dc
G Richards <bluwave@users.noreply.github.com>
2019-08-23 11:08:25 -0600

    MBL-11702 - UI for manual post grades screen (#490)
    * MBL-11702 - UI for manual post grades screen
    refs: MBL-11702
    affects: Teacher
    release note: none
    
    * add visibility selector viewcontroller
    
    * add hide screen
    
    * add button actions
    
    * add combined settings screen
    
    * add extension method for register cell on tableview
    
    * localize header
    
    * remove unneeded removeTarget call
    
    * localize
    
    * remove VFL
    
    * fix typo
    
    * fixes

---
+++ rn/Teacher/ios/Submissions/PostToVisibilitySelectionViewController.swift
@@ -0,0 +1,65 @@
+//
+// This file is part of Canvas.
+// Copyright (C) 2019-present  Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import UIKit
+import Core
+
+protocol PostToVisibilitySelectionDelegate: class {
+    func visibilityDidChange(visibility: PostGradesViewController.PostToVisisbility)
+}
+
+class PostToVisibilitySelectionViewController: UITableViewController {
+
+    var selectedVisibility: PostGradesViewController.PostToVisisbility = .everyone
+    weak var delegate: PostToVisibilitySelectionDelegate?
+
+    static func create(visibility: PostGradesViewController.PostToVisisbility, delegate: PostToVisibilitySelectionDelegate?) -> PostToVisibilitySelectionViewController {
+        let controller = loadFromStoryboard()
+        controller.selectedVisibility = visibility
+        controller.delegate = delegate
+        return controller
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = NSLocalizedString("Post to...", comment: "")
+
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return PostGradesViewController.PostToVisisbility.allCases.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeue(for: indexPath)
+
+        let row = PostGradesViewController.PostToVisisbility.allCases[indexPath.row]
+        cell.detailTextLabel?.numberOfLines = 0
+        cell.textLabel?.text = row.title
+        cell.detailTextLabel?.text = row.subHeader
+        cell.accessoryType = row == selectedVisibility ? .checkmark : .none
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedVisibility = PostGradesViewController.PostToVisisbility.allCases[indexPath.row]
+        tableView.reloadData()
+        delegate?.visibilityDidChange(visibility: selectedVisibility)
+    }
+}
