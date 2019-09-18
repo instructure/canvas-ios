48072bbf08e6c336345923e66aa5e42f10946fda
G Richards <bluwave@users.noreply.github.com>
2019-09-18 16:22:04 -0600

    MBL-13195 - add multiple grading period API entity and requestable and Core Data model and UseCase (#631)
    * MBL-13195 - add multiple grading period API entity and requestable
    refs: MBL-13195
    affects: Parent
    release note: none
    
    * add Core.GradingPeriod entity

---
+++ Core/Core/Models/GradingPeriod.swift
@@ -0,0 +1,38 @@
+//
+// This file is part of Canvas.
+// Copyright (C) 2019-present  Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import Foundation
+import CoreData
+
+public final class GradingPeriod: NSManagedObject, WriteableModel {
+    public typealias JSON = APIGradingPeriod
+
+    @NSManaged public var id: String
+    @NSManaged public var title: String
+    @NSManaged public var courseID: String
+
+    @discardableResult
+    public static func save(_ item: APIGradingPeriod, in context: NSManagedObjectContext) -> GradingPeriod {
+        let predicate = NSPredicate(format: "%K == %@", #keyPath(GradingPeriod.id), item.id.value)
+        let model: GradingPeriod = context.fetch(predicate).first ?? context.insert()
+        model.id = item.id.value
+        model.title = item.title
+
+        return model
+    }
+}
