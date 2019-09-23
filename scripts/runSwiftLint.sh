e0cf0667d4f3d64f8663880db1a1dc189a88cc9d
Garrett Richards <garrettrichards@gmail.com>
2019-09-23 12:35:26 -0600

    remove swift lint in xcode build phases, add script to run on CI servers refs: none affects: none release note: none

---
+++ scripts/runSwiftLint.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+#
+# This file is part of Canvas.
+# Copyright (C) 2019-present  Instructure, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+# fail if any commands fails
+# set -e
+# debug log
+# set -x
+
+printHelp() {
+	echo "$0 <command>*"
+	echo "    fix - fix lint"
+	echo "    (* optional)"
+}
+
+LINT_CONFIG_FILE_PATH="$(pwd .swiftlint.yml)/.swiftlint.yml"
+
+FIX=""
+STRICT="--strict"
+COMMAND="LINTING"
+
+if [ "$#" -eq 1 ]; then
+
+	if [ $1 = "fix" ]; then
+		FIX="autocorrect"
+		STRICT=""
+		COMMAND="FIXING"
+	fi
+	
+fi
+
+
+declare -a names=("core" "student" "teacher" "parent")
+declare -a paths=("Core" "Student" "rn/Teacher/ios" "Parent")
+arraylength=${#paths[@]}
+
+echo "${COMMAND}"
+echo ""
+
+for (( i=0; i<${arraylength}; i++ ));
+do
+	echo "[${names[$i]}]"
+	pushd ${paths[$i]} > /dev/null 2>&1
+	swiftlint ${FIX} --config ${LINT_CONFIG_FILE_PATH} ${STRICT}  # > /dev/null 2>&1
+	popd > /dev/null 2>&1
+done
