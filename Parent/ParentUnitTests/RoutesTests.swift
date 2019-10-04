19e6ff8942c555e2b0641fc6810c427bf5a09ce6
Nate Armstrong <natearmstrong2@gmail.com>
2019-10-04 15:56:48 -0600

    Feature flag course grades route (#695)
    refs: MBL-13368
    affects: parent
    release note: none
    
    Test plan:
    
    - Disable experimental features
    - View a calendar event and/or an assignment with a link to course grades
      - /courses/:courseID/grades
      - narmstrong/o > CS 1400 > Week of Sept 1 > New Event!! > Grades link
    - Tapping grades link should show course calendar week view
    
    - Enable experimental features
    - Tap a course
    - You should be presented with new grades list
    - You can't get to a page with links so there's no way to test tapping on a grades link

---
+++ Parent/ParentUnitTests/RoutesTests.swift
@@ -0,0 +1,34 @@
+//
+// This file is part of Canvas.
+// Copyright (C) 2019-present  Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import Foundation
+@testable import Parent
+import XCTest
+@testable import Core
+
+class RoutesTests: ParentTestCase {
+    func testCourseGrades() {
+        ExperimentalFeature.allEnabled = false
+        XCTAssert(Parent.router.match(.parse("/courses/1/grades")) is CalendarEventWeekPageViewController)
+    }
+
+    func testCourseGradesParent3() {
+        ExperimentalFeature.allEnabled = true
+        XCTAssert(Parent.router.match(.parse("/courses/1/grades")) is GradesViewController)
+    }
+}
