792f6c22b90bfe57428fcb54f22c119f3f429b68
Garrett Richards <garrettrichards@gmail.com>
2019-10-16 16:00:08 -0600

    fix the twin bug where parents have millenials who are twins in the same class who take the same assignment refs: MBL-13334 affects: Parent release note: fixed bug with multiple students in the same class
    Test plan
    ------------
    see ticket

--- CanvasCore/CanvasCore/Assignments/Assignment/Assignment.swift
+++ CanvasCore/CanvasCore/Assignments/Assignment/Assignment.swift
@@ -341,21 +341,23 @@ extension Assignment: SynchronizedModel {
         
         assignmentGroup     = try assignmentGroupID.flatMap { try context.findOne(withValue: $0, forKey: "id") }
 
-        try updateSubmission(json, inContext: context)
+        let studentID: String? = try? json.stringID("studentID") // for parent when there are multiple submissions for different users
+        try updateSubmission(json, studentID: studentID, inContext: context)
         try updateURL(json)
         try updateRubric(json, inContext: context)
         try updateOverrides(json, inContext: context)
         try updateLockStatus(json)
     }
 
-    @objc func updateSubmission(_ json: JSONObject, inContext context: NSManagedObjectContext) throws {
+    @objc func updateSubmission(_ json: JSONObject, studentID: String? = nil, inContext context: NSManagedObjectContext) throws {
         var status: SubmissionStatus = []
         var submissionState = ""
 
         // "submission" is an array if we are an observer
         let submissions: [JSONObject]? = try? json <| "submission"
         let submission: JSONObject? = try? json <| "submission"
-        if let submissionJSON = submissions?.first ?? submission {
+        let submissionJSON: JSONObject? = try submissions?.first { try $0.stringID("user_id") == studentID } ?? submissions?.first
+        if let submissionJSON = (submissionJSON ?? submission) {
             let attempt: Int = (try submissionJSON <| "attempt") ?? 0
             hasSubmitted        = attempt > 0
 
