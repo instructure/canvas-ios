ed667277e845a97f005e6281a64182d1aa41ca15
Garrett Richards <garrettrichards@gmail.com>
2019-10-24 15:55:35 -0600

    sectioned list

--- Student/Student/Assignments/AssignmentList/AssignmentListPresenter.swift
+++ Student/Student/Assignments/AssignmentList/AssignmentListPresenter.swift
@@ -20,7 +20,7 @@ import Foundation
 import Core
 
 protocol AssignmentListViewProtocol: ErrorViewController, ColoredNavViewProtocol {
-    func update()
+    func update(loading: Bool)
 }
 
 class AssignmentListPresenter {
@@ -29,17 +29,25 @@ class AssignmentListPresenter {
     let courseID: String
     let env: AppEnvironment
     weak var view: AssignmentListViewProtocol?
+    var didGetAssignmentGroups = false
+    var assignmentGroups: Store<GetAssignmentGroups>?
+    var assignments: Store<GetAssignmentsForGrades>!
+    var currentGradingPeriodID: String?
 
     lazy var color = env.subscribe(GetCustomColors()) { [weak self] in
         self?.updateNavbar()
     }
 
-    lazy var course = env.subscribe(GetCourse(courseID: courseID)) { [weak self] in
+    lazy var courses = env.subscribe(GetCourse(courseID: courseID)) { [weak self] in
         self?.update()
         self?.updateNavbar()
     }
 
-    lazy var assignments = env.subscribe(GetAssignments(courseID: self.courseID, sort: sort)) { [weak self] in
+//    lazy var assignments = env.subscribe(GetAssignments(courseID: self.courseID, sort: sort)) { [weak self] in
+//        self?.update()
+//    }
+
+    lazy var gradingPeriods = env.subscribe(GetGradingPeriods(courseID: courseID)) { [weak self] in
         self?.update()
     }
 
@@ -51,22 +59,52 @@ class AssignmentListPresenter {
     }
 
     func viewIsReady() {
-        assignments.exhaust(while: { _ in true })
-        course.refresh()
-        color.refresh()
-        view?.update()
+//        assignments.exhaust(while: { _ in true })
+//        color.refresh()
+        refresh(force: true)
+    }
+
+    func refresh(force: Bool = false) {
+        didGetAssignmentGroups = false
+        color.refresh(force: force)
+        courses.refresh(force: force)
+        refreshAssignments(force: force)
+        gradingPeriods.refresh(force: force)
     }
 
     func update() {
-        view?.update()
+        guard
+            let assignmentGroups = assignmentGroups,
+            let assignments = assignments
+            else { return }
+        let loading = assignments.pending || assignmentGroups.pending || gradingPeriods.pending || courses.pending || color.pending
+        view?.update(loading: loading)
     }
 
     func updateNavbar() {
-        guard let course = course.first else { return }
+        guard let course = courses.first else { return }
         view?.updateNavBar(subtitle: course.name, color: course.color)
     }
 
     func select(_ assignment: Assignment, from: UIViewController) {
         env.router.route(to: assignment.htmlURL, from: from, options: nil)
+    }
+
+    func refreshAssignments(force: Bool) {
+        let useCase = GetAssignmentsForGrades(courseID: courseID, gradingPeriodID: currentGradingPeriodID, groupBy: .assignmentGroup, requestQuerySize: 99, clearsBeforeWrite: false, include: [.all_dates])
+        assignments = env.subscribe( useCase ) { [weak self] in
+            if !(self?.assignments.pending ?? false) && !(self?.didGetAssignmentGroups ?? false) {
+                self?.refreshAssignmentGroups(force: force)
+            }
+        }
+        assignments?.refresh(force: force)
+    }
+
+    func refreshAssignmentGroups(force: Bool) {
+        didGetAssignmentGroups = true
+        assignmentGroups = env.subscribe(  GetAssignmentGroups(courseID: courseID, gradingPeriodID: currentGradingPeriodID, include: [.assignments])  ) { [weak self] in
+            self?.update()
+        }
+        assignmentGroups?.refresh(force: force)
     }
 }
--- Student/Student/Assignments/AssignmentList/AssignmentListViewController.swift
+++ Student/Student/Assignments/AssignmentList/AssignmentListViewController.swift
@@ -19,21 +19,25 @@
 import UIKit
 import Core
 
-class AssignmentListViewController: UITableViewController {
+class AssignmentListViewController: UIViewController {
 
     var presenter: AssignmentListPresenter?
     var color: UIColor?
     var titleSubtitleView: TitleSubtitleView = TitleSubtitleView.create()
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
 
-    convenience init(env: AppEnvironment = .shared, courseID: String, sort: GetAssignments.Sort = .position) {
-        self.init(nibName: nil, bundle: nil)
-        presenter = AssignmentListPresenter(view: self, courseID: courseID, sort: sort)
+    static func create(env: AppEnvironment = .shared, courseID: String, sort: GetAssignments.Sort = .position) -> AssignmentListViewController {
+        let vc = loadFromStoryboard()
+        vc.presenter = AssignmentListPresenter(view: vc, courseID: courseID, sort: sort)
+        return vc
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTitleViewInNavbar(title: NSLocalizedString("Assignments", comment: ""))
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: String(describing: UITableViewCell.self))
+        tableView.registerCell(ListCell.self)
+        tableView.registerHeaderFooterView(SectionHeaderView.self)
         presenter?.viewIsReady()
     }
 
@@ -41,27 +45,62 @@ class AssignmentListViewController: UITableViewController {
         super.viewWillAppear(animated)
         navigationController?.navigationBar.useContextColor(color)
     }
+}
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return presenter?.assignments.count ?? 0
+extension AssignmentListViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return presenter?.assignments.numberOfSections ?? 0
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeue(UITableViewCell.self, for: indexPath)
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let sectionInfo = presenter?.assignments.sectionInfo(inSection: section) else { return 0 }
+        return sectionInfo.numberOfObjects
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: ListCell = tableView.dequeue(for: indexPath)
         cell.textLabel?.text = presenter?.assignments[indexPath]?.name
-        cell.imageView?.image = .icon(.assignment, .line)
+        cell.textLabel?.numberOfLines = 0
+        cell.textLabel?.lineBreakMode = .byWordWrapping
+        cell.detailTextLabel?.text = presenter?.assignments[indexPath]?.formattedDueDate()
+        cell.imageView?.image = presenter?.assignments[indexPath]?.icon
         cell.imageView?.tintColor = color
         return cell
     }
 
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard let assignment = presenter?.assignments[indexPath.row] else { return }
         presenter?.select(assignment, from: self)
+    }
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = tableView.dequeueHeaderFooter(SectionHeaderView.self)
+        view.titleLabel?.text = presenter?.assignmentGroups?[section]?.name
+        return view
+    }
+
+    class ListCell: UITableViewCell {
+        override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+            super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+            textLabel?.font = UIFont.scaledNamedFont(.semibold16)
+            detailTextLabel?.font = UIFont.scaledNamedFont(.medium14)
+            detailTextLabel?.textColor = UIColor.named(.textDark)
+            accessoryType = .disclosureIndicator
+        }
+
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
     }
 }
 
 extension AssignmentListViewController: AssignmentListViewProtocol {
-    func update() {
-        tableView.reloadData()
+    func update(loading: Bool) {
+        tableView.isHidden = loading
+        spinner.isHidden = !loading
+        if !loading {
+            tableView.reloadData()
+        }
+        view.layoutIfNeeded()
     }
 }
