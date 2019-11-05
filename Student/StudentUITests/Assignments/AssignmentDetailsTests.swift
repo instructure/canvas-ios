b656f1341677ac82240345a3cac0472dd591414f
Nate Armstrong <natearmstrong2@gmail.com>
2019-11-05 08:37:58 -0700

    Fix UI tests for assignment grades
    refs: MBL-13509
    affects: student
    release note: none

--- Student/StudentUITests/Assignments/AssignmentDetailsTests.swift
+++ Student/StudentUITests/Assignments/AssignmentDetailsTests.swift
@@ -241,7 +241,7 @@ class AssignmentDetailsTests: StudentUITestCase {
     func testGradeCellShowsSubmittedTextWhenNotGraded() {
         mockBaseRequests()
         let assignment = mock(assignment: .make(
-            submission: APISubmission.make()
+            submission: APISubmission.make(submission_type: .online_upload, workflow_state: .pending_review)
         ))
         show("/courses/\(course.id)/assignments/\(assignment.id)")
         XCTAssertTrue(AssignmentDetails.submittedText.waitToExist().isVisible)
@@ -254,7 +254,8 @@ class AssignmentDetailsTests: StudentUITestCase {
             points_possible: 100,
             submission: APISubmission.make(
                 grade: "90",
-                score: 90
+                score: 90,
+                workflow_state: .graded
             )
         ))
         show("/courses/\(course.id)/assignments/\(assignment.id)")
@@ -267,7 +268,8 @@ class AssignmentDetailsTests: StudentUITestCase {
             points_possible: 10,
             submission: APISubmission.make(
                 grade: "80%",
-                score: 8
+                score: 8,
+                workflow_state: .graded
             ),
             grading_type: .percent
         ))
@@ -285,6 +287,7 @@ class AssignmentDetailsTests: StudentUITestCase {
                 grade: "85",
                 score: 85,
                 late: true,
+                workflow_state: .graded,
                 late_policy_status: .late,
                 points_deducted: 5
             )
@@ -320,7 +323,7 @@ class AssignmentDetailsTests: StudentUITestCase {
         mockBaseRequests()
         let assignment = mock(assignment: .make(
             points_possible: 10,
-            submission: APISubmission.make()
+            submission: APISubmission.make(submission_type: .online_upload, workflow_state: .submitted)
         ))
         show("/courses/\(course.id)/assignments/\(assignment.id)")
         AssignmentDetails.viewSubmissionButton.tap()
@@ -333,7 +336,8 @@ class AssignmentDetailsTests: StudentUITestCase {
             points_possible: 10,
             submission: APISubmission.make(
                 grade: "80%",
-                score: 8
+                score: 8,
+                workflow_state: .graded
             ),
             grading_type: .percent
         ))
