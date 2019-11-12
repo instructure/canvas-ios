68ece7d6ac03c2dd9ded4d1467cf26ea369df250
Andrew Cobb <acobb@instructure.com>
2019-11-12 13:22:20 -0700

    removed failing test

--- Core/CoreTests/MetaTests/XCTestCaseTests.swift
+++ Core/CoreTests/MetaTests/XCTestCaseTests.swift
@@ -20,10 +20,6 @@ import XCTest
 
 class XCTestCaseTests: XCTestCase {
 
-    func testThatWillFail() {
-        XCTFail("testing bitrise stuff")
-    }
-
     func testDrainMainQueueDrainsNestedAsyncs() {
         for _ in 0..<10 {
             var allNestsCalled = false
