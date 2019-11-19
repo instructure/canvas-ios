6d1d4a9b3a21fa2986a0b94e45f9348461e528ee
Garrett Richards <garrettrichards@gmail.com>
2019-11-19 14:32:46 -0700

    fix lint

--- Core/Core/Use Cases/GetActivities.swift
+++ Core/Core/Use Cases/GetActivities.swift
@@ -34,7 +34,7 @@ public class GetActivities: CollectionUseCase {
                                #keyPath(Activity.typeRaw), ActivityType.conference.rawValue,
                                #keyPath(Activity.typeRaw), ActivityType.collaboration.rawValue,
                                #keyPath(Activity.typeRaw), ActivityType.assessmentRequest.rawValue)
-        let order = [ NSSortDescriptor(key: #keyPath(Activity.updatedAt), ascending: false),]
+        let order = [ NSSortDescriptor(key: #keyPath(Activity.updatedAt), ascending: false), ]
         return Scope(predicate: pred, order: order, sectionNameKeyPath: nil)
     }
 
