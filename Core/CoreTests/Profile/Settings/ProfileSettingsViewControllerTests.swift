344742a52d916a918cd80b193ca89dbca41f8cf4
G Richards <bluwave@users.noreply.github.com>
2019-12-16 09:06:20 -0700

    render tests for settings (#898)
    * render tests for settings
    refs: MBL-13543
    affects: Student
    release note: none
    
    * remove redundant test

--- Core/CoreTests/Profile/Settings/ProfileSettingsViewControllerTests.swift
+++ Core/CoreTests/Profile/Settings/ProfileSettingsViewControllerTests.swift
@@ -21,22 +21,84 @@ import XCTest
 
 class ProfileSettingsViewControllerTests: CoreTestCase {
     var vc: ProfileSettingsViewController!
+    var externalURLOpened: URL?
+
     override func setUp() {
         super.setUp()
         vc = ProfileSettingsViewController.create()
-        vc.view.frame = CGRect(x: 0, y: 0, width: 300, height: 800)
-        vc.view.layoutIfNeeded()
-        XCTAssertNotNil(vc.view)
     }
 
-    func testCanNavigateToExperimentalFeatures() {
-        vc.reloadData()
-        let cell = vc.tableView.cellForRow(at: IndexPath(row: 1, section: 0)) as? RightDetailTableViewCell
-        XCTAssertEqual(cell?.textLabel?.text, "Experimental Features")
+    func load() {
+        vc.view.frame = CGRect(x: 0, y: 0, width: 300, height: 800)
+        vc.view.layoutIfNeeded()
+        vc.viewDidLoad()
+        vc.viewWillAppear(false)
+    }
 
-        vc.tableView.delegate?.tableView?(vc.tableView, didSelectRowAt: IndexPath(row: 1, section: 0))
+    func testRender() {
+        let channels = [
+            APICommunicationChannel.make(address: "a", id: ID(1), position: 1, type: .email, workflow_state: .active),
+            APICommunicationChannel.make(address: "b", id: ID(2), position: 2, type: .push, workflow_state: .active),
+        ]
+        environment.mockStore = false
+        api.mock(vc.profile, value: APIProfile.make())
+        api.mock(vc.channels, value: channels)
+
+        load()
+
+        var cell = vc.tableView.cellForRow(at: IndexPath(row: 0, section: 0)) as? RightDetailTableViewCell
+        XCTAssertEqual( cell?.textLabel?.text, "Landing Page")
+        XCTAssertEqual( cell?.detailTextLabel?.text, "Dashboard")
+
+        cell = vc.tableView.cellForRow(at: IndexPath(row: 1, section: 0)) as? RightDetailTableViewCell
+        XCTAssertEqual( cell?.textLabel?.text, "Experimental Features")
+
+        cell = vc.tableView.cellForRow(at: IndexPath(row: 2, section: 0)) as? RightDetailTableViewCell
+        XCTAssertEqual( cell?.textLabel?.text, "Email Notifications")
+        vc.tableView(vc.tableView, didSelectRowAt: IndexPath(row: 2, section: 0))
         wait(for: [router.showExpectation], timeout: 1)
         let (routedVC, _, _) = router.viewControllerCalls.last!
-        XCTAssert(routedVC is ExperimentalFeaturesViewController)
+        XCTAssert(routedVC is NotificationCategoriesViewController)
+        if let catViewController = routedVC as? NotificationCategoriesViewController {
+            XCTAssertEqual(catViewController.channelType, CommunicationChannelType.email)
+        } else { XCTFail() }
+
+        cell = vc.tableView.cellForRow(at: IndexPath(row: 3, section: 0)) as? RightDetailTableViewCell
+        XCTAssertEqual( cell?.textLabel?.text, "Push Notifications")
+
+        let previousDelegate = environment.loginDelegate
+        environment.loginDelegate = self
+        cell = vc.tableView.cellForRow(at: IndexPath(row: 4, section: 0)) as? RightDetailTableViewCell
+        XCTAssertEqual( cell?.textLabel?.text, "Subscribe to Calendar Feed")
+        vc.tableView(vc.tableView, didSelectRowAt: IndexPath(row: 4, section: 0))
+        environment.loginDelegate = previousDelegate
+        XCTAssertEqual(externalURLOpened, URL(string: "https://calendar.url"))
+
+        cell = vc.tableView.cellForRow(at: IndexPath(row: 0, section: 1)) as? RightDetailTableViewCell
+        XCTAssertEqual( cell?.textLabel?.text, "Privacy Policy")
+        vc.tableView(vc.tableView, didSelectRowAt: IndexPath(row: 0, section: 1))
+        XCTAssert(router.lastRoutedTo(.parse("https://www.instructure.com/policies/privacy/")))
+
+        cell = vc.tableView.cellForRow(at: IndexPath(row: 1, section: 1)) as? RightDetailTableViewCell
+        XCTAssertEqual( cell?.textLabel?.text, "Terms of Use")
+        vc.tableView(vc.tableView, didSelectRowAt: IndexPath(row: 1, section: 1))
+        XCTAssert(router.lastRoutedTo(.termsOfService()))
+
+        cell = vc.tableView.cellForRow(at: IndexPath(row: 2, section: 1)) as? RightDetailTableViewCell
+        XCTAssertEqual( cell?.textLabel?.text, "Canvas on GitHub")
+        vc.tableView(vc.tableView, didSelectRowAt: IndexPath(row: 2, section: 1))
+        XCTAssert(router.lastRoutedTo(.parse("https://github.com/instructure/canvas-ios")))
+    }
+}
+
+extension ProfileSettingsViewControllerTests: LoginDelegate {
+    func userDidLogin(session: LoginSession) {
+    }
+
+    func userDidLogout(session: LoginSession) {
+    }
+
+    func openExternalURL(_ url: URL) {
+        externalURLOpened = url
     }
 }
