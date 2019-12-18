5f528ddba74ac7c71ea3375c19c5cddc09b0780e
Garrett Richards <garrettrichards@gmail.com>
2019-12-18 16:21:35 -0700

    add reply and reply all buttons to conversation details refs: MBL-13693 affects: Parent release note: none

--- Core/Core/Extensions/UIImageInstIconsExtensions.swift
+++ Core/Core/Extensions/UIImageInstIconsExtensions.swift
@@ -33,8 +33,8 @@ extension UIImage {
             gradebook, group, hamburger, highlighter, home, image, instructure, italic, link, lock,
             lti, marker, miniArrowDown, miniArrowUp, module, more, no, numberedList, outcomes,
             paint, paperclip, pause, pdf, play, prerequisite, publish, question, quiz, refresh,
-            reply, rubric, settings, star, strikethrough, text, textColor, trash, trouble, unlock,
-            user, video, warning, x
+            reply, replyAll, rubric, settings, star, strikethrough, text, textColor, trash, trouble,
+            unlock, user, video, warning, x
     }
 
     public static func icon(_ name: InstIconName, _ type: InstIconType = .line) -> UIImage {
