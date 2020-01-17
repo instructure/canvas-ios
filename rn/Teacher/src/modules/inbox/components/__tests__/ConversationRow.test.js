7cfba2f0db52dcd151413443ba3b931cb7df0924
Nate Armstrong <natearmstrong2@gmail.com>
2020-01-17 14:33:48 -0700

    Add pronouns to Inbox
    refs: MBL-13762
    affects: student, teacher
    release note: none
    
    Test plan:
    * Pronouns should show in:
      - Conversations list
      - Conversation details
      - Inbox compose
      - Address Book
    * Log in as `s` on narmstrong
    * Inbox > 2nd conversation in the list should display pronouns for Student One
    * Sent > Pronouns test should display pronouns for Student One recipient
    * Tap + to create a new conversation
    * Select Course > CS 1400
    * Tap + to add a person
    * Search for "Student One"
    * Pronouns should be displayed in result
    * Tap Student One
    * Pronouns should show in recipient token

--- rn/Teacher/src/modules/inbox/components/__tests__/ConversationRow.test.js
+++ rn/Teacher/src/modules/inbox/components/__tests__/ConversationRow.test.js
@@ -20,7 +20,7 @@
 import 'react-native'
 import React from 'react'
 import ConversationRow from '../ConversationRow'
-import explore from '../../../../../test/helpers/explore'
+import { shallow } from 'enzyme'
 
 jest.mock('TouchableHighlight', () => 'TouchableHighlight')
 
@@ -28,63 +28,100 @@ const template = {
   ...require('../../../../__templates__/conversations'),
 }
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer'
-
-it('renders correctly', () => {
-  const conversation = template.conversation()
-  const tree = renderer.create(
-    <ConversationRow conversation={conversation} />
-  ).toJSON()
-  expect(tree).toMatchSnapshot()
-})
-
-it('renders correctly with different data', () => {
-  const conversation = template.conversation({
-    participants: null,
-    subject: null,
-    workflow_state: 'unread',
-    starred: true,
+describe('ConversationRow', () => {
+  let props
+  beforeEach(() => {
+    jest.clearAllMocks()
+    props = {
+      conversation: template.conversation({ id: '1' }),
+      drawsTopLine: true,
+      onPress: jest.fn(),
+    }
   })
-  const tree = renderer.create(
-    <ConversationRow conversation={conversation} drawsTopLine={true} />
-  ).toJSON()
-  expect(tree).toMatchSnapshot()
-})
 
-it('renders correctly with a bunch of people in there', () => {
-  const conversation = template.conversation()
-  conversation.participants = Array(100).fill().map((e, i) => ({ id: i.toString(), name: i.toString() }))
-  const tree = renderer.create(
-    <ConversationRow conversation={conversation} drawsTopLine={true} />
-  ).toJSON()
-  expect(tree).toMatchSnapshot()
-})
-
-it('handles on press', () => {
-  const conversation = template.conversation()
-  const callback = jest.fn()
-  const tree = renderer.create(
-    <ConversationRow conversation={conversation} onPress={callback}/>
-  ).toJSON()
-  const button = explore(tree).selectByID(`inbox.conversation-${conversation.id}`) || {}
-  button.props.onPress()
-  expect(callback).toHaveBeenCalledWith(conversation.id)
-})
-
-it('extract date', () => {
-  const conversation = template.conversation({
-    last_message_at: 'last_message_at',
-    last_authored_message_at: 'last_authored_message_at',
+  it('hides unread indicator', () => {
+    props.conversation.workflow_state = 'read'
+    let tree = shallow(<ConversationRow {...props} />)
+    expect(tree.find('[testID="ConversationRow.1.unreadIndicator"]').exists()).toEqual(false)
   })
-  expect(ConversationRow.extractDate(conversation)).toEqual(conversation.last_authored_message_at)
-  conversation.last_authored_message_at = ''
-  expect(ConversationRow.extractDate(conversation)).toEqual(conversation.last_message_at)
-  conversation.last_authored_message_at = 'last_authored_message_at'
-  conversation.properties = ['last_author']
-  expect(ConversationRow.extractDate(conversation)).toEqual(conversation.last_authored_message_at)
-  conversation.properties = []
-  expect(ConversationRow.extractDate(conversation)).toEqual(conversation.last_message_at)
-  conversation.last_message_at = ''
-  expect(ConversationRow.extractDate(conversation)).toEqual(conversation.last_authored_message_at)
+
+  it('shows unread indicator', () => {
+    props.conversation.workflow_state = 'unread'
+    let tree = shallow(<ConversationRow {...props} />)
+    expect(tree.find('[testID="ConversationRow.1.unreadIndicator"]').exists()).toEqual(true)
+  })
+
+  it('shows starred', () => {
+    props.conversation.starred = true
+    let tree = shallow(<ConversationRow {...props} />)
+    expect(tree.find('[testID="ConversationRow.1.starredIndicator"]').exists()).toEqual(true)
+  })
+
+  it('hides starred', () => {
+    props.conversation.starred = false
+    let tree = shallow(<ConversationRow {...props} />)
+    expect(tree.find('[testID="ConversationRow.1.starredIndicator"]').exists()).toEqual(false)
+  })
+
+  it('renders with more than 6 participant names', () => {
+    props.conversation.participants = Array(8).fill().map((e, i) => ({ id: i.toString(), name: i.toString() }))
+    let tree = shallow(<ConversationRow {...props} />)
+    expect(tree.find('[testID="ConversationRow.1.names"]').prop('children')).toEqual('0, 2, 3, 4, 5 + 2 more')
+  })
+
+  it('renders participant names', () => {
+    props.conversation.participants = [
+      { id: '11', name: 'Graydon' },
+      { id: '12', name: 'James' },
+      { id: '13', name: 'Josh' },
+    ]
+    let tree = shallow(<ConversationRow {...props} />)
+    let expected = 'Graydon, James, Josh'
+    expect(tree.find('[testID="ConversationRow.1.names"]').prop('children')).toEqual(expected)
+  })
+
+  it('renders participant names with pronouns', () => {
+    props.conversation.participants = [
+      { id: '11', name: 'Graydon', pronouns: 'He/Him' },
+      { id: '12', name: 'James', pronouns: 'They/Them' },
+      { id: '13', name: 'Josh', pronouns: 'She/Her' },
+    ]
+    let tree = shallow(<ConversationRow {...props} />)
+    let expected = 'Graydon (He/Him), James (They/Them), Josh (She/Her)'
+    expect(tree.find('[testID="ConversationRow.1.names"]').prop('children')).toEqual(expected)
+  })
+
+  it('handles on press', () => {
+    const tree = shallow(<ConversationRow {...props} />)
+    tree.find('[testID="inbox.conversation-1"]').simulate('Press')
+    expect(props.onPress).toHaveBeenCalledWith('1')
+  })
+
+  it('renders the correct date', async () => {
+    let authored = new Date('2016-02-01T11:49:37-07:00')
+    let messaged = new Date('2017-02-01T11:49:37-07:00')
+    let authoredAt = '2/1/2016'
+    let messagedAt = '2/1/2017'
+    props.conversation.last_authored_message_at = authored
+    props.conversation.last_message_at = messaged
+    props.conversation.properties = ['last_author']
+    let tree = shallow(<ConversationRow {...props} />)
+    expect(tree.find('[testID="ConversationRow.1.date"]').prop('children')).toEqual(authoredAt)
+
+    props.conversation.last_authored_message_at = null
+    await tree.setProps(props)
+    expect(tree.find('[testID="ConversationRow.1.date"]').prop('children')).toEqual(messagedAt)
+
+    props.properties = null
+    props.conversation.last_message_at = null
+    props.conversation.last_authored_message_at = authored
+    await tree.setProps(props)
+    expect(tree.find('[testID="ConversationRow.1.date"]').prop('children')).toEqual(authoredAt)
+
+    props.conversation.properties = []
+    props.conversation.last_message_at = messaged
+    await tree.setProps(props)
+    expect(tree.find('[testID="ConversationRow.1.date"]').prop('children')).toEqual(messagedAt)
+  })
 })
+
