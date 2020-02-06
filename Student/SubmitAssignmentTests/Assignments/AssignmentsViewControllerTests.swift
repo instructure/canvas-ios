31ea6dfeaa271cbc334cc42308aaed96f2eca820
Nate Armstrong <natearmstrong2@gmail.com>
2020-02-05 17:13:12 -0700

    Fix submit share extension pagination
    refs: MBL-13879
    affects: student
    release note: Fixed a bug that could prevent assignments from being listed in the share extension
    
    Test plan: See [ticket](https://instructure.atlassian.net/browse/MBL-13879)

---
+++ Student/SubmitAssignmentTests/Assignments/AssignmentsViewControllerTests.swift
@@ -0,0 +1,107 @@
+//
+// This file is part of Canvas.
+// Copyright (C) 2020-present  Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import Foundation
+@testable import Core
+import TestsFoundation
+import XCTest
+@testable import SubmitAssignment
+
+class AssignmentsViewControllerTests: SubmitAssignmentTests {
+    var courseID = "1"
+    var selectedAssignmentID: String? = nil
+    var callback: (Assignment) -> Void = { _ in }
+
+    lazy var controller = AssignmentsViewController.create(
+        courseID: courseID,
+        selectedAssignmentID: selectedAssignmentID,
+        callback: callback
+    )
+
+    func testLayout() {
+        selectedAssignmentID = "1"
+        var selectedAssignment: Assignment?
+        callback = { selectedAssignment = $0 }
+        api.mock(
+            controller.assignments,
+            value: [
+                .make(
+                    id: "1",
+                    course_id: ID(courseID),
+                    name: "Assignment 1",
+                    submission_types: [.online_upload]
+                ),
+                .make(
+                    id: "2",
+                    course_id: ID(courseID),
+                    name: "Assignment 2",
+                    submission_types: [.online_upload]
+                ),
+            ]
+        )
+        controller.loadViewIfNeeded()
+        let tableView = controller.tableView
+        XCTAssertEqual(controller.tableView.dataSource?.tableView(tableView!, numberOfRowsInSection: 0), 2)
+        let firstCell = controller.tableView.dataSource?.tableView(tableView!, cellForRowAt: IndexPath(row: 0, section: 0))
+        XCTAssertEqual(firstCell?.textLabel?.text, "Assignment 1")
+        XCTAssertEqual(firstCell?.accessoryType, .checkmark)
+
+        let secondCell = controller.tableView.dataSource?.tableView(tableView!, cellForRowAt: IndexPath(row: 1, section: 0))
+        XCTAssertEqual(secondCell?.textLabel?.text, "Assignment 2")
+        XCTAssertEqual(secondCell?.accessoryType, UITableViewCell.AccessoryType.none)
+
+        controller.tableView.delegate?.tableView?(tableView!, didSelectRowAt: IndexPath(row: 1, section: 0))
+        XCTAssertEqual(selectedAssignment?.name, "Assignment 2")
+    }
+
+    func testPaginatedFiltering() {
+        let invalid = APIAssignment.make(
+            id: "1",
+            course_id: ID(courseID),
+            name: "invalid",
+            submission_types: [.online_text_entry]
+        )
+        let valid = APIAssignment.make(
+            id: "3",
+            course_id: ID(courseID),
+            name: "valid",
+            submission_types: [.online_upload]
+        )
+        api.mock(
+            controller.assignments,
+            value: [invalid],
+            response: HTTPURLResponse(next: "/page-2")
+        )
+        let task = api.mock(
+            GetNextRequest<[APIAssignment]>(path: "/page-2"),
+            value: [valid],
+            response: nil
+        )
+        task.paused = true
+        controller.loadViewIfNeeded()
+        let tableView = controller.tableView!
+        XCTAssertEqual(tableView.dataSource?.tableView(tableView, numberOfRowsInSection: 0), 1)
+        let loadingCell = tableView.dataSource?.tableView(tableView, cellForRowAt: IndexPath(row: 0, section: 0))
+        XCTAssert(loadingCell is LoadingCell)
+    
+        task.paused = false
+        XCTAssertEqual(tableView.dataSource?.tableView(tableView, numberOfRowsInSection: 0), 1)
+        let cell = tableView.dataSource?.tableView(tableView, cellForRowAt: IndexPath(row: 0, section: 0))
+        XCTAssertEqual(cell?.textLabel?.text, "valid")
+    }
+}
