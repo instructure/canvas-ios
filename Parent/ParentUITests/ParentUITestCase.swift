9821a6e977c34c7d93c1feb011c81ec22a4c200e
Andrew Cobb <acobb@instructure.com>
2020-02-13 14:52:43 -0700

    Fix nightly (#1072)

--- Parent/ParentUITests/ParentUITestCase.swift
+++ Parent/ParentUITests/ParentUITestCase.swift
@@ -32,6 +32,7 @@ class ParentUITestCase: CoreUITestCase {
     }
 
     override func mockBaseRequests() {
+        mockData(GetUserRequest(userID: "self"), value: APIUser.make())
         mockData(GetWebSessionRequest(to: URL(string: "https://canvas.instructure.com/users/self"))) // cookie keepalive
         for paginated in [true, false] {
             mockEncodableRequest([
