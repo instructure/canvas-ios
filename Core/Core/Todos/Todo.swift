17110e1fa5986ed465748e99b31cd38256cacd49
Nate Armstrong <natearmstrong2@gmail.com>
2020-02-14 13:31:28 -0700

    Show weekday in to-do list cells
    [run-nightly]
    
    refs: MBL-13968
    affects: student
    release note: Added weekday to To Do list due dates
    
    Test plan:
    * Due dates on To Do list should show the weekday
    * See [ticket](https://instructure.atlassian.net/browse/MBL-13968)

--- Core/Core/Todos/Todo.swift
+++ Core/Core/Todos/Todo.swift
@@ -22,6 +22,13 @@ import CoreData
 public final class Todo: NSManagedObject, WriteableModel {
     public typealias JSON = APITodo
 
+    static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .short
+        return formatter
+    }()
+
     @NSManaged public var assignment: Assignment
     @NSManaged var contextRaw: String
     @NSManaged public var id: String
@@ -73,9 +80,14 @@ public final class Todo: NSManagedObject, WriteableModel {
     public var subtitleText: String {
         switch type {
         case .submitting:
-            guard assignment.dueAt != nil else { return assignment.dueText }
-            let format = NSLocalizedString("Due: %@", bundle: .core, comment: "")
-            return String.localizedStringWithFormat(format, assignment.dueText)
+            guard let dueAt = assignment.dueAt else {
+                return NSLocalizedString("No Due Date", bundle: .core, comment: "")
+            }
+            let calendar = Calendar.current
+            let weekday = calendar.shortWeekdaySymbols[calendar.component(.weekday, from: dueAt)]
+            let dueText = Todo.dateFormatter.string(from: dueAt)
+            let format = NSLocalizedString("Due %@, %@", bundle: .core, comment: "")
+            return String.localizedStringWithFormat(format, weekday, dueText)
         case .grading:
             let format = NSLocalizedString("d_needs_grading", bundle: .core, comment: "")
             return String.localizedStringWithFormat(format, needsGradingCount)
