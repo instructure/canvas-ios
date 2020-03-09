8700f68ca58e055e5726613eb92179222d0dbe14
Andy VanWagoner <andy@instructure.com>
2020-03-09 10:26:49 -0600

    Add unit tests

--- Core/CoreTests/Views/PagesViewControllerTests.swift
+++ Core/CoreTests/Views/PagesViewControllerTests.swift
@@ -19,5 +19,142 @@
 import XCTest
 @testable import Core
 
-class PagesViewControllerTests: XCTestCase {
+class PagesViewControllerTests: CoreTestCase {
+    lazy var controller: PagesViewController = {
+        let controller = PagesViewController()
+        controller.currentPage.title = "0"
+        controller.dataSource = self
+        controller.delegate = self
+        return controller
+    }()
+
+    var currentPage: UIViewController?
+    var visiblePages: [UIViewController] = []
+    var visibleTitles: [Int] {
+        visiblePages.compactMap { $0.title.flatMap { Int($0) } }
+    }
+
+    func testScrolling() {
+        controller.view.frame = UIScreen.main.bounds
+        controller.view.layoutIfNeeded()
+        let scroll = controller.scrollView
+        let width = scroll.frame.width
+        scroll.delegate?.scrollViewWillBeginDragging?(scroll)
+        scroll.delegate?.scrollViewDidScroll?(scroll)
+        XCTAssertEqual(visibleTitles, [ 0 ])
+        scroll.contentOffset.x = width * 1.5
+        scroll.delegate?.scrollViewDidScroll?(scroll)
+        XCTAssertEqual(visibleTitles, [ 0, 1 ])
+
+        // same values won't trigger another isShowing delegate call
+        visiblePages = []
+        scroll.contentOffset.x = width * 1.75
+        scroll.delegate?.scrollViewDidScroll?(scroll)
+        XCTAssertEqual(visibleTitles, [])
+
+        var target = CGPoint(x: width * 2, y: 0)
+        scroll.delegate?.scrollViewWillEndDragging?(scroll, withVelocity: .zero, targetContentOffset: &target)
+        XCTAssertEqual(currentPage?.title, "1")
+
+        scroll.semanticContentAttribute = .forceRightToLeft
+        scroll.delegate?.scrollViewWillBeginDragging?(scroll)
+        scroll.contentOffset.x = width * 1.8
+        scroll.delegate?.scrollViewDidScroll?(scroll)
+        XCTAssertEqual(visibleTitles, [ 1, 0 ])
+
+        target = CGPoint(x: width * 2, y: 0)
+        scroll.delegate?.scrollViewWillEndDragging?(scroll, withVelocity: .zero, targetContentOffset: &target)
+        XCTAssertEqual(currentPage?.title, "0")
+
+        scroll.semanticContentAttribute = .unspecified
+        scroll.delegate?.scrollViewWillBeginDragging?(scroll)
+        scroll.contentOffset.x = width * 0.5
+        scroll.delegate?.scrollViewDidScroll?(scroll)
+        XCTAssertEqual(visibleTitles, [ -1, 0 ])
+
+        target = CGPoint(x: 0, y: 0)
+        scroll.delegate?.scrollViewWillEndDragging?(scroll, withVelocity: .zero, targetContentOffset: &target)
+        XCTAssertEqual(currentPage?.title, "-1")
+    }
+
+    func testAccessibilityScroll() {
+        controller.view.frame = UIScreen.main.bounds
+        controller.view.layoutIfNeeded()
+
+        XCTAssertTrue(controller.accessibilityScroll(.left))
+        XCTAssertEqual(currentPage?.title, "1")
+        XCTAssertTrue(controller.accessibilityScroll(.right))
+        XCTAssertEqual(currentPage?.title, "0")
+        XCTAssertTrue(controller.accessibilityScroll(.up))
+        XCTAssertEqual(currentPage?.title, "1")
+        XCTAssertTrue(controller.accessibilityScroll(.down))
+        XCTAssertEqual(currentPage?.title, "0")
+        XCTAssertTrue(controller.accessibilityScroll(.previous))
+        XCTAssertEqual(currentPage?.title, "-1")
+        XCTAssertTrue(controller.accessibilityScroll(.next))
+        XCTAssertEqual(currentPage?.title, "0")
+
+        controller.view.semanticContentAttribute = .forceRightToLeft
+        XCTAssertTrue(controller.accessibilityScroll(.left))
+        XCTAssertEqual(currentPage?.title, "-1")
+        XCTAssertTrue(controller.accessibilityScroll(.right))
+        XCTAssertEqual(currentPage?.title, "0")
+        XCTAssertTrue(controller.accessibilityScroll(.up))
+        XCTAssertEqual(currentPage?.title, "1")
+        XCTAssertTrue(controller.accessibilityScroll(.down))
+        XCTAssertEqual(currentPage?.title, "0")
+        XCTAssertTrue(controller.accessibilityScroll(.previous))
+        XCTAssertEqual(currentPage?.title, "-1")
+        XCTAssertTrue(controller.accessibilityScroll(.next))
+        XCTAssertEqual(currentPage?.title, "0")
+
+        controller.dataSource = nil
+        XCTAssertFalse(controller.accessibilityScroll(.left))
+    }
+
+    func testSetCurrentPage() {
+        controller.view.frame = UIScreen.main.bounds
+        controller.view.layoutIfNeeded()
+        let scroll = controller.scrollView
+
+        let one = UIViewController()
+        one.title = "1"
+        let two = UIViewController()
+        two.title = "2"
+
+        controller.setCurrentPage(one)
+        XCTAssertEqual(controller.currentPage, one)
+        XCTAssertEqual(controller.scrollView.subviews.count, 1)
+
+        scroll.delegate?.scrollViewWillBeginDragging?(scroll)
+        controller.setCurrentPage(two, direction: .forward)
+        XCTAssertEqual(controller.currentPage, two)
+        XCTAssertEqual(controller.scrollView.subviews.count, 2)
+
+        controller.setCurrentPage(one, direction: .reverse)
+        XCTAssertEqual(controller.currentPage, one)
+        XCTAssertEqual(controller.scrollView.subviews.count, 2)
+    }
+}
+
+extension PagesViewControllerTests: PagesViewControllerDataSource, PagesViewControllerDelegate {
+    func pagesViewController(_ pages: PagesViewController, pageBefore viewController: UIViewController) -> UIViewController? {
+        let controller = UIViewController()
+        controller.title = viewController.title.flatMap({ Int($0) }).map({ String($0 - 1) })
+        return controller
+    }
+
+    func pagesViewController(_ pages: PagesViewController, pageAfter viewController: UIViewController) -> UIViewController? {
+        let controller = UIViewController()
+        controller.title = viewController.title.flatMap({ Int($0) }).map({ String($0 + 1) })
+        return controller
+    }
+
+    func pagesViewController(_ pages: PagesViewController, isShowing list: [UIViewController]) {
+        visiblePages = list
+    }
+
+    func pagesViewController(_ pages: PagesViewController, didTransitionTo page: UIViewController) {
+        currentPage = page
+    }
 }
