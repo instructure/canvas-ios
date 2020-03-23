e0afa2c6c1e30745ebc5e4130e325deaf22f1c65
Andy VanWagoner <andy@instructure.com>
2020-03-23 11:04:55 -0600

    Remove ActAsUserPresenter (#1181)
    refs: none
    affects: none
    release note: none

--- Core/Core/ActAsUser/ActAsUserViewController.swift
+++ Core/Core/ActAsUser/ActAsUserViewController.swift
@@ -18,84 +18,133 @@
 
 import UIKit
 
-public class ActAsUserViewController: UITableViewController {
-    var env: AppEnvironment?
-    var presenter: ActAsUserPresenter?
+public class ActAsUserViewController: UIViewController {
+    @IBOutlet weak var actAsUserButton: UIButton!
+    @IBOutlet weak var actAsUserDescription: UILabel!
+    @IBOutlet weak var domainTextField: UITextField!
+    @IBOutlet weak var keyboardSpace: NSLayoutConstraint!
+    @IBOutlet weak var redPanda: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var userIDTextField: UITextField!
+
+    let env = AppEnvironment.shared
     var initialUserID: String?
+    var keyboard: KeyboardTransitioning?
+    weak var loginDelegate: LoginDelegate?
 
-    @IBOutlet var redPanda: UIImageView!
-    @IBOutlet var redPandaTopConstraint: NSLayoutConstraint!
-    @IBOutlet var redPandaTrailingConstraint: NSLayoutConstraint!
-    @IBOutlet var actAsUserDescription: UILabel!
-    @IBOutlet var domainTextField: UITextField!
-    @IBOutlet var userIDTextField: UITextField!
-    @IBOutlet var actAsUserButton: UIButton!
-
-    public static func create(env: AppEnvironment = .shared, loginDelegate: LoginDelegate, userID: String? = nil) -> ActAsUserViewController {
+    public static func create(loginDelegate: LoginDelegate, userID: String? = nil) -> ActAsUserViewController {
         let controller = loadFromStoryboard()
-        controller.env = env
         controller.initialUserID = userID
-        controller.presenter = ActAsUserPresenter(env: env, loginDelegate: loginDelegate)
+        controller.loginDelegate = loginDelegate
         return controller
     }
 
-    override public func viewDidLoad() {
-        tableView.backgroundColor = .named(.backgroundLightest)
-        navigationItem.rightBarButtonItems = [] // remove Done added by Helm
-        addCancelButton()
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .named(.backgroundLightest)
+        addCancelButton(side: .left)
         title = NSLocalizedString("Act as User", bundle: .core, comment: "")
+        navigationItem.rightBarButtonItem = nil // remove Done added by Helm
         // swiftlint:disable:next line_length
         actAsUserDescription.text = NSLocalizedString("\"Act as\" is essentially logging in as this user without a password. You will be able to take any action as if you were this user, and from other users' points of view, as if this user performed them. However, audit logs record that you were the one who performed the actions on behalf of this user.", bundle: .core, comment: "")
         domainTextField.placeholder = NSLocalizedString("Domain", bundle: .core, comment: "")
         userIDTextField.placeholder = NSLocalizedString("User ID", bundle: .core, comment: "")
         actAsUserButton.titleLabel?.text = NSLocalizedString("Act as User", bundle: .core, comment: "")
-        domainTextField.text = env?.currentSession?.baseURL.absoluteString
+        domainTextField.text = env.currentSession?.baseURL.absoluteString
         userIDTextField.text = initialUserID
+        updateActAsUserButtonDisabledStatus()
 
         domainTextField.addTarget(self, action: #selector(updateActAsUserButtonDisabledStatus), for: .editingChanged)
         userIDTextField.addTarget(self, action: #selector(updateActAsUserButtonDisabledStatus), for: .editingChanged)
-        actAsUserButton.isEnabled = false
 
-        domainTextField.addTarget(self, action: #selector(enterPressed), for: .editingDidEndOnExit)
-        userIDTextField.addTarget(self, action: #selector(enterPressed), for: .editingDidEndOnExit)
+        domainTextField.addTarget(self, action: #selector(actAsUserPressed), for: .editingDidEndOnExit)
+        userIDTextField.addTarget(self, action: #selector(actAsUserPressed), for: .editingDidEndOnExit)
 
-        redPanda.transform = redPanda.transform.rotated(by: .pi / -6).translatedBy(x: 0, y: 0)
-        actAsUserButton.layer.cornerRadius = 5
-        animatePanda()
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChangeFrame(_:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     }
 
     public override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        keyboard = KeyboardTransitioning(view: view, space: keyboardSpace)
         navigationController?.navigationBar.useModalStyle()
+        animatePanda()
     }
 
     func animatePanda() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) { [weak self] in
-            self?.redPandaTopConstraint.constant -= 30
-            self?.redPandaTrailingConstraint.constant -= 82
-            UIView.animate(withDuration: 1.5, animations: { [weak self] in
-                guard let redPanda = self?.redPanda else {
-                    return
-                }
+        let animation = CAKeyframeAnimation(keyPath: "transform")
+        let easeInEaseOut = CAMediaTimingFunction(name: .easeInEaseOut)
+        let cover = CATransform3DIdentity
+        let aside = CATransform3DMakeAffineTransform(redPanda.transform.translatedBy(x: -82, y: 30).rotated(by: .pi / -6))
+        animation.timingFunctions = [ easeInEaseOut, easeInEaseOut, easeInEaseOut, easeInEaseOut ]
+        animation.duration = 10
+        animation.keyTimes = [ 0, 0.2, 0.35, 0.85, 1 ]
+        animation.values = [ aside, aside, cover, cover, aside ]
+        animation.repeatCount = .infinity
+        redPanda.layer.add(animation, forKey: "masquerade")
+    }
 
-                redPanda.transform = redPanda.transform
-                    .rotated(by: .pi / 6)
-                self?.view.layoutIfNeeded()
-                }, completion: { [weak self] _ in
-                    self?.redPandaTopConstraint.constant += 30
-                    self?.redPandaTrailingConstraint.constant += 82
-                    UIView.animate(withDuration: 1.5, delay: 5, animations: { [weak self] in
-                        guard let redPanda = self?.redPanda else {
-                            return
-                        }
-                        redPanda.transform = redPanda.transform
-                            .rotated(by: .pi / -6)
-                        self?.view.layoutIfNeeded()
-                    }, completion: { [weak self] _ in
-                        self?.animatePanda()
-                    })
-            })
+    @objc func keyboardWillChangeFrame(_ notification: Notification) {
+        guard
+            let info = notification.userInfo as? [String: Any],
+            let keyboardFrame = info[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect,
+            let animationCurve = info[UIResponder.keyboardAnimationCurveUserInfoKey] as? UInt,
+            let animationDuration = info[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval
+        else { return }
+        let y = max(0, scrollView.contentSize.height - scrollView.safeAreaInsets.bottom - scrollView.convert(keyboardFrame, from: nil).origin.y)
+        UIView.animate(withDuration: animationDuration, delay: 0, options: .init(rawValue: animationCurve), animations: {
+            self.scrollView.contentOffset.y = y
+            self.scrollView.layoutIfNeeded()
+        }, completion: nil)
+    }
+
+    @objc func updateActAsUserButtonDisabledStatus() {
+        actAsUserButton.isEnabled = (
+            domainTextField.text?.isEmpty == false &&
+            userIDTextField.text?.isEmpty == false
+        )
+        actAsUserButton.alpha = actAsUserButton.isEnabled ? 1 : 0.5
+    }
+
+    @IBAction func actAsUserPressed() {
+        guard
+            var host = domainTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines), !host.isEmpty,
+            let userID = userIDTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines), !userID.isEmpty
+        else { return }
+        if !host.contains(".") {
+            host = "\(host).instructure.com"
         }
+        if URLComponents.parse(host).scheme == nil {
+            host = "https://\(host)"
+        }
+        guard let baseURL = URL(string: host), let session = env.currentSession else {
+            return showMasqueradingError()
+        }
+        view.endEditing(true)
+        let api = URLSessionAPI(loginSession: session, baseURL: baseURL)
+        api.makeRequest(GetUserRequest(userID: userID)) { [weak self] (user, _, error) in performUIUpdate {
+            guard let self = self else { return }
+            guard let user = user, error == nil else {
+                return self.showMasqueradingError()
+            }
+            self.env.router.dismiss(self)
+            self.loginDelegate?.startActing(as: LoginSession(
+                accessToken: session.accessToken,
+                baseURL: baseURL,
+                expiresAt: session.expiresAt,
+                lastUsedAt: Clock.now,
+                locale: user.locale ?? user.effective_locale,
+                masquerader: (session.originalBaseURL ?? session.baseURL)
+                    .appendingPathComponent("users")
+                    .appendingPathComponent(session.originalUserID ?? session.userID),
+                refreshToken: session.refreshToken,
+                userAvatarURL: user.avatar_url?.rawValue,
+                userID: user.id.value,
+                userName: user.short_name,
+                userEmail: user.email,
+                clientID: session.clientID,
+                clientSecret: session.clientSecret
+            ))
+        } }
     }
 
     func showMasqueradingError() {
@@ -104,35 +153,7 @@ public class ActAsUserViewController: UITableViewController {
             message: NSLocalizedString("There was an error with Act as User. Please try again later.", bundle: .core, comment: ""),
             preferredStyle: .alert
         )
-        alert.addAction(UIAlertAction(title: NSLocalizedString("Cancel", bundle: .core, comment: ""), style: .cancel, handler: nil))
-        self.present(alert, animated: true, completion: nil)
-    }
-
-    @IBAction func actAsUserPressed(_ sender: UIButton) {
-        guard let domain = domainTextField.text, let userID = userIDTextField.text else {
-            return
-        }
-        view.endEditing(true)
-        presenter?.didSubmit(domain: domain, userID: userID) { [weak self] err in
-            guard err == nil else {
-                self?.showMasqueradingError()
-                return
-            }
-            self?.dismiss(animated: true)
-        }
-    }
-}
-
-extension ActAsUserViewController {
-    @objc
-    func updateActAsUserButtonDisabledStatus() {
-        self.actAsUserButton.isEnabled = domainTextField.text?.isEmpty == false && userIDTextField.text?.isEmpty == false
-    }
-
-    @objc
-    func enterPressed() {
-        if self.actAsUserButton.isEnabled {
-            self.actAsUserPressed(self.actAsUserButton)
-        }
+        alert.addAction(AlertAction(NSLocalizedString("Cancel", bundle: .core, comment: ""), style: .cancel))
+        env.router.show(alert, from: self, options: .modal())
     }
 }
---
+++ Core/CoreTests/ActAsUser/ActAsUserViewControllerTests.swift
@@ -0,0 +1,83 @@
+//
+// This file is part of Canvas.
+// Copyright (C) 2019-present  Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import XCTest
+@testable import Core
+import TestsFoundation
+
+class ActAsUserViewControllerTests: CoreTestCase, LoginDelegate {
+    lazy var controller = ActAsUserViewController.create(loginDelegate: self)
+
+    var session: LoginSession?
+    func userDidLogin(session: LoginSession) {
+        self.session = session
+    }
+    func userDidLogout(session: LoginSession) {}
+    func openExternalURL(_ url: URL) {}
+
+    func testLayout() {
+        let nav = UINavigationController(rootViewController: controller)
+        controller.view.layoutIfNeeded()
+        controller.viewWillAppear(false)
+        router.show(controller, from: nav, options: .modal())
+
+        XCTAssertEqual(nav.navigationBar.barStyle, .default)
+        XCTAssertEqual(controller.title, "Act as User")
+
+        XCTAssertEqual(controller.actAsUserButton.isEnabled, false)
+        controller.userIDTextField.text = "1"
+        controller.userIDTextField.sendActions(for: .editingChanged)
+        XCTAssertEqual(controller.actAsUserButton.isEnabled, true)
+
+        controller.actAsUserButton.sendActions(for: .primaryActionTriggered)
+        XCTAssertNil(session)
+
+        NotificationCenter.default.post(name: UIResponder.keyboardWillChangeFrameNotification, object: nil, userInfo: [:])
+        XCTAssertEqual(controller.scrollView.contentOffset.y, 0)
+
+        NotificationCenter.default.post(name: UIResponder.keyboardWillChangeFrameNotification, object: nil, userInfo: [
+            UIResponder.keyboardFrameEndUserInfoKey: CGRect(x: 0, y: 100, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height - 100),
+            UIResponder.keyboardAnimationCurveUserInfoKey: UIView.AnimationOptions.curveEaseOut.rawValue,
+            UIResponder.keyboardAnimationDurationUserInfoKey: TimeInterval(2),
+        ])
+        XCTAssertGreaterThan(controller.scrollView.contentOffset.y, 0)
+
+        api.mock(GetUserRequest(userID: "1"), value: .make(), baseURL: URL(string: "https://cgnu.online")!)
+        controller.domainTextField.text = "cgnu.online/extra"
+        controller.actAsUserButton.sendActions(for: .primaryActionTriggered)
+        XCTAssertEqual(session?.userID, "1")
+        XCTAssertEqual(session?.baseURL, URL(string: "https://cgnu.online"))
+        XCTAssertEqual(session?.masquerader, URL(string: "https://canvas.instructure.com/users/1"))
+        session = nil
+
+        api.mock(GetUserRequest(userID: "1"), value: .make(), baseURL: URL(string: "https://cgnu.instructure.com")!)
+        controller.domainTextField.text = "cgnu"
+        controller.actAsUserButton.sendActions(for: .primaryActionTriggered)
+        XCTAssertNotNil(session)
+        session = nil
+
+        environment.currentSession = nil
+        controller.actAsUserButton.sendActions(for: .primaryActionTriggered)
+        XCTAssertEqual((router.presented as? UIAlertController)?.title, "Error")
+        router.viewControllerCalls.removeAll()
+
+        api.mock(GetUserRequest(userID: "1"), error: NSError.internalError(), baseURL: URL(string: "https://cgnu.instructure.com")!)
+        controller.actAsUserButton.sendActions(for: .primaryActionTriggered)
+        XCTAssertEqual((router.presented as? UIAlertController)?.title, "Error")
+    }
+}
