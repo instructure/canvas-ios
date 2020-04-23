a2b76fd946019e0e22aa9894585a878b9ae17178
Andrew Cobb <acobb@instructure.com>
2020-04-23 13:45:58 -0600

    fix nightly tests that use AttachmentView (#1270)
    [ignore-commit-lint]

--- Core/CoreUITests/Discussions/DiscussionEditTests.swift
+++ Core/CoreUITests/Discussions/DiscussionEditTests.swift
@@ -88,7 +88,7 @@ class DiscussionEditTests: CoreUITestCase {
         photo.tap()
 
         app.find(label: "Upload complete").waitToExist()
-        let img = app.images["AttachmentView.image"]
+        let img = app.find(id: "AttachmentView.image")
         app.find(label: "Upload complete").tapUntil { img.exists == true }
         NavBar.dismissButton.tap()
         app.find(id: "attachments.attachment-row.0.remove.btn").tap()
