11c517cb2451a30c8dee9a3352c4b047c4b8d262
Nate Armstrong <natearmstrong2@gmail.com>
2020-06-08 08:56:12 -0600

    Fix enhanced RCE media in Teacher app (#1354)
    refs: MBL-14443
    affects: teacher
    release note: Fixed a bug that could cause some media to not load in rich content
    
    Test plan: See ticket. There should be 3 playable media objects.

--- CanvasCore/CanvasCore/WhizzyWig/CanvasWebView.js
+++ CanvasCore/CanvasCore/WhizzyWig/CanvasWebView.js
@@ -29,3 +29,40 @@ var checkQuizzesNext = setInterval(function() {
      }
   })
 }, 100)
+
+// Replace all iframes with a button to launch in SFSafariViewController
+const iframes = document.querySelectorAll('iframe');
+// If there is only one iframe
+// and id="cnvs_content"
+// and the src is a canvas file
+// reload the webview with an authenticated version of the iframe's src
+if (iframes.length == 1 && /\/courses\/\d+\/files\/\d+\/download/.test(iframes[0].src) && iframes[0].id === "cnvs_content") {
+    window.webkit.messageHandlers.loadFrameSource.postMessage(iframes[0].src)
+} else {
+    // Replace all iframes with a button to launch in SFSafariViewController
+    iframes.forEach(iframe => {
+        const replace = iframe => {
+            const a = document.createElement('a')
+            a.textContent = '{$LTI_LAUNCH_TEXT$}'
+            a.classList.add('canvas-ios-lti-launch-button')
+            a.href = iframe.src
+            iframe.parentNode.replaceChild(a, iframe)
+        }
+        if (/\/courses\/\d+\/external_tools\/retrieve/.test(iframe.src)) {
+            replace(iframe)
+        } else if (/\/media_objects_iframe\/m-\w+/.test(iframe.src)) {
+            const match = iframe.src.match(/\/media_objects_iframe\/(m-\w+)/)
+            if (match.length == 2) {
+                const mediaID = match[1]
+                const video = document.createElement('video')
+                video.src = 'https://canvas.instructure.com/users/self/media_download?entryId='+mediaID+'&media_type=video&redirect=1'
+                video.setAttribute('poster', 'https://canvas.instructure.com/media_objects/'+mediaID+'/thumbnail?width=550&height=448')
+                video.setAttribute('controls', '')
+                video.setAttribute('preload', 'none')
+                iframe.parentNode.parentNode.replaceChild(video, iframe.parentNode)
+            }
+        } else {
+            iframe.addEventListener('error', event => replace(event.target))
+        }
+    })
+}
