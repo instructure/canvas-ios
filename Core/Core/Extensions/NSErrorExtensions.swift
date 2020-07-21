1759b1216702b7fa8009428c4b69af222bb0bcd0
Andy VanWagoner <andy@instructure.com>
2020-07-21 12:11:25 -0600

    fix lint

--- Core/Core/Extensions/NSErrorExtensions.swift
+++ Core/Core/Extensions/NSErrorExtensions.swift
@@ -57,24 +57,24 @@ extension NSError {
         let alert = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
         switch (domain, code) {
         case (NSCocoaErrorDomain, 13): // NSCocoaErrorDomain 13 NSUnderlyingException: error during SQL execution : database or disk is full
-            alert.title = NSLocalizedString("Disk Error", bundle: .core, comment: "Error title to alert user their device is out of storage space")
-            alert.message = NSLocalizedString("Your device is out of storage space. Please free up space and try again.", bundle: .core, comment: "Error description for file out of space")
+            alert.title = NSLocalizedString("Disk Error", bundle: .core, comment: "")
+            alert.message = NSLocalizedString("Your device is out of storage space. Please free up space and try again.", bundle: .core, comment: "")
             alert.addAction(dismiss)
 
         case (NSURLErrorDomain, _):
-            alert.title = NSLocalizedString("Network Error", bundle: .core, comment: "Error alert title for network error")
+            alert.title = NSLocalizedString("Network Error", bundle: .core, comment: "")
             alert.message = localizedDescription
             alert.addAction(report)
             alert.addAction(dismiss)
 
         case ("com.instructure.canvas", 90211): // push channel error. no idea where 90211 comes from.
-            alert.title = NSLocalizedString("Notification Error", bundle: .core, comment: "Error title for push notification registration error")
-            alert.message = NSLocalizedString("There was a problem registering your device for push notifications.", bundle: .core, comment: "Error description for Push Notifications registration error.")
+            alert.title = NSLocalizedString("Notification Error", bundle: .core, comment: "")
+            alert.message = NSLocalizedString("There was a problem registering your device for push notifications.", bundle: .core, comment: "")
             alert.addAction(report)
             alert.addAction(dismiss)
 
         default:
-            alert.title = NSLocalizedString("Unknown Error", bundle: .core, comment: "An unknown error title")
+            alert.title = NSLocalizedString("Unknown Error", bundle: .core, comment: "")
             alert.message = localizedFailureReason.flatMap {
                 "\(localizedDescription)\n\n\($0)"
             } ?? localizedDescription
