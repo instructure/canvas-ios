29246766801ec73ba905b6ca5c501b9b1f109a95
Andy VanWagoner <andy@instructure.com>
2020-08-18 09:46:57 -0600

    address feedback

--- Core/Core/Pages/PageEditor/PageEditorView.swift
+++ Core/Core/Pages/PageEditor/PageEditorView.swift
@@ -28,7 +28,7 @@ public struct PageEditorView: View {
 
     @State var title: String = ""
     @State var html: String = ""
-    @State var editingRoles: String = "members"
+    @State var editingRoles: RoleOption = .members
     @State var published: Bool = false
     @State var isFrontPage: Bool = false
 
@@ -37,8 +37,10 @@ public struct PageEditorView: View {
     @State var editingRolesPickerShown: Bool = false
     @State var rceHeight: CGFloat = 60
     @State var rceCanSubmit = false
-    @State var rceError: Error?
-    @State var saveError: Error?
+    @State var showError: Bool = false
+    @State var error: Error? {
+        didSet { showError = error != nil }
+    }
 
     public init(context: Context, url: String? = nil) {
         self.context = context
@@ -52,19 +54,22 @@ public struct PageEditorView: View {
                 CircleProgress().size()
             }
         }
+            .alert(isPresented: $showError) {
+                Alert(title: Text(verbatim: ""), message: Text(error!.localizedDescription))
+            }
             .avoidKeyboardArea()
             .background(Color.backgroundGrouped)
-            .navigationBarTitle(url == nil ? Text("New Page") : Text("Edit Page"), displayMode: .inline)
+            .navigationBarTitle(url == nil ? Text("New Page", bundle: .core) : Text("Edit Page", bundle: .core), displayMode: .inline)
             .navigationBarItems(
                 leading: Button(action: {
                     guard let controller = self.viewController() else { return }
                     self.env.router.dismiss(controller)
                 }, label: {
-                    Text("Cancel")
+                    Text("Cancel", bundle: .core)
                 })
                     .identifier("screen.dismiss"),
                 trailing: Button(action: save, label: {
-                    Text("Done").bold()
+                    Text("Done", bundle: .core).bold()
                 })
                     .disabled(isLoading || isSaving)
                     .identifier("PageEditor.doneButton")
@@ -76,25 +81,24 @@ public struct PageEditorView: View {
     var form: some View {
         ScrollView { VStack(alignment: .leading, spacing: 0) {
             if env.app == .teacher {
-                Text("Title")
+                Text("Title", bundle: .core)
                     .font(.semibold14).foregroundColor(.textDark)
                     .padding(EdgeInsets(top: 16, leading: 16, bottom: 8, trailing: 16))
                 Divider()
                 TextField("Add Title", text: $title)
-                    .foregroundColor(.textDarkest)
+                    .font(.regular16).foregroundColor(.textDarkest)
                     .padding(16)
                     .background(Color.backgroundLightest)
                     .identifier("PageEditor.titleField")
                 Divider()
             } else {
                 Text(title)
-                    .font(.bold20)
-                    .foregroundColor(.textDarkest)
+                    .font(.bold20).foregroundColor(.textDarkest)
                     .padding(EdgeInsets(top: 16, leading: 16, bottom: 0, trailing: 16))
                     .identifier("PageEditor.titleText")
             }
 
-            Text("Content")
+            Text("Content", bundle: .core)
                 .font(.semibold14).foregroundColor(.textDark)
                 .padding(EdgeInsets(top: 16, leading: 16, bottom: 8, trailing: 16))
             Divider()
@@ -105,46 +109,42 @@ public struct PageEditorView: View {
                 uploadTo: .context(context),
                 height: $rceHeight,
                 canSubmit: $rceCanSubmit,
-                error: $rceError
+                error: $error
             )
                 .background(Color.backgroundLightest)
                 .frame(minHeight: 200, idealHeight: max(200, rceHeight))
             Divider()
 
             if env.app == .teacher || context.contextType == .group {
-                Text("Details")
+                Text("Details", bundle: .core)
                     .font(.semibold14).foregroundColor(.textDark)
                     .padding(EdgeInsets(top: 16, leading: 16, bottom: 8, trailing: 16))
                 Divider()
-                if !isFrontPage && env.app == .teacher {
-                    Toggle("Publish", isOn: $published)
+                if url != "front_page" && env.app == .teacher {
+                    Toggle(isOn: $published) { Text("Publish", bundle: .core) }
                         .font(.semibold16).foregroundColor(.textDarkest)
                         .padding(16)
                         .background(Color.backgroundLightest)
+                        .disabled(isFrontPage)
                         .identifier("PageEditor.publishedToggle")
                     Divider()
                 }
-                if published && url != "front_page" && env.app == .teacher {
-                    Toggle("Set as Front Page", isOn: $isFrontPage)
+                if url != "front_page" && env.app == .teacher {
+                    Toggle(isOn: $isFrontPage) { Text("Set as Front Page", bundle: .core) }
                         .font(.semibold16).foregroundColor(.textDarkest)
                         .padding(16)
                         .background(Color.backgroundLightest)
+                        .disabled(!published)
                         .identifier("PageEditor.frontPageToggle")
                     Divider()
                 }
                 Button(action: { self.editingRolesPickerShown.toggle() }, label: {
-                    Text("Can Edit").font(.semibold16)
+                    Text("Can Edit", bundle: .core).font(.semibold16)
                     Spacer()
-                    if editingRoles == "members" {
-                        Text("Only members")
-                    } else if editingRoles == "teachers" {
-                        Text("Only teachers")
-                    } else if editingRoles == "students,teachers" {
-                        Text("Teachers and students")
-                    } else if editingRoles == "public" {
-                        Text("Anyone")
-                    }
-                    Image(systemName: "chevron.right").accentColor(.borderMedium)
+                    editingRoles.text
+                    Image(systemName: "chevron.right")
+                        .flipsForRightToLeftLayoutDirection(true)
+                        .accentColor(.borderMedium)
                 })
                     .padding(16)
                     .accentColor(.textDarkest)
@@ -152,15 +152,15 @@ public struct PageEditorView: View {
                     .identifier("PageEditor.editorsButton")
                 Divider()
                 if editingRolesPickerShown {
-                    Picker("Can Edit", selection: $editingRoles) {
+                    Picker(selection: $editingRoles, label: Text("Can Edit", bundle: .core), content: {
                         if context.contextType == .group {
-                            Text("Only members").tag("members")
+                            RoleOption.members.text
                         } else {
-                            Text("Only teachers").tag("teachers")
-                            Text("Teachers and students").tag("students,teachers")
+                            RoleOption.teachers.text
+                            RoleOption.teachersAndStudents.text
                         }
-                        Text("Anyone").tag("public")
-                    }
+                        RoleOption.public.text
+                    })
                         .labelsHidden()
                         .identifier("PageEditor.editorsPicker")
                 }
@@ -170,22 +170,24 @@ public struct PageEditorView: View {
 
     func load() {
         guard let url = url else {
+            editingRoles = context.contextType == .group ? .members : .teachers
             isLoading = false
             return
         }
         let useCase = GetPage(context: context, url: url)
-        useCase.fetch { _, _, _ in performUIUpdate {
+        useCase.fetch { _, _, error in performUIUpdate {
             let page: Page? = self.env.database.viewContext.fetch(scope: useCase.scope).first
-            var editingRoles = "public"
-            if page?.editingRoles.contains("teachers") == true { editingRoles = "teachers" }
-            if page?.editingRoles.contains("students") == true { editingRoles = "students,teachers" }
-            if page?.editingRoles.contains("members") == true { editingRoles = "members" }
+            var editingRoles = RoleOption.public
+            if page?.editingRoles.contains("teachers") == true { editingRoles = .teachers }
+            if page?.editingRoles.contains("students") == true { editingRoles = .teachersAndStudents }
+            if page?.editingRoles.contains("members") == true { editingRoles = .members }
             self.title = page?.title ?? ""
             self.html = page?.body ?? ""
             self.editingRoles = editingRoles
             self.published = page?.published ?? false
             self.isFrontPage = page?.isFrontPage ?? false
             self.isLoading = false
+            self.error = error
         } }
     }
 
@@ -196,16 +198,34 @@ public struct PageEditorView: View {
             url: url,
             title: title,
             body: html,
-            editing_roles: editingRoles,
+            editing_roles: editingRoles.rawValue,
             published: published || isFrontPage,
             front_page: isFrontPage
         ).fetch { result, _, error in performUIUpdate {
-            self.saveError = error
+            self.error = error
             self.isSaving = false
             if result != nil, let controller = self.viewController() {
                 self.env.router.dismiss(controller)
             }
         } }
+    }
+
+    enum RoleOption: String {
+        case `public`, members, teachers
+        case teachersAndStudents = "students,teachers"
+
+        var text: some View {
+            switch self {
+            case .public:
+                return Text("Anyone", bundle: .core).tag(self)
+            case .members:
+                return Text("Only members", bundle: .core).tag(self)
+            case .teachers:
+                return Text("Only teachers", bundle: .core).tag(self)
+            case .teachersAndStudents:
+                return Text("Teachers and students", bundle: .core).tag(self)
+            }
+        }
     }
 }
 
