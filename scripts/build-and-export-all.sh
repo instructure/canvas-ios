3cc543c998a05b8bfcd2f6a4a7f617520cb532c6
Andrew Cobb <acobb@instructure.com>
2020-09-04 14:33:39 -0600

    Try something else else

--- scripts/build-and-export-all.sh
+++ scripts/build-and-export-all.sh
@@ -1,4 +1,21 @@
 #!/bin/zsh
+#
+# This file is part of Canvas.
+# Copyright (C) 2020-present  Instructure, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 
 # `xcodebuild archive` always builds from scratch, which is fairly redundant.
 # Instead, we can archive a scheme that includes all 3 apps, then tweak the
