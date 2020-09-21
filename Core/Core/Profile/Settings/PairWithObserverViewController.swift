e666ab95438c0ba5ed97598adce2e79dcb454e28
Nate Armstrong <natearmstrong2@gmail.com>
2020-09-21 16:37:44 -0600

    Include account ID in pairing QR code
    refs: MBL-14752
    affects: student
    release note: none
    
    test plan: You would need to have an Android device to test this. I have
    one and tested it locally. I will test with the ad-hoc build as well.

--- Core/Core/Profile/Settings/PairWithObserverViewController.swift
+++ Core/Core/Profile/Settings/PairWithObserverViewController.swift
@@ -98,10 +98,11 @@ class PairWithObserverViewController: UIViewController, ErrorViewController {
     func displayQR(pairingCode: String?, accountID: String?, baseURL: URL?) {
         guard
             let code = pairingCode,
+            let accountID = accountID, // Android requires the account id
             let host = baseURL?.host
         else { return }
 
-        let comps = URLComponents(string: "canvas-parent://\(host)/pair?code=\(code)")
+        let comps = URLComponents(string: "canvas-parent://\(host)/pair?code=\(code)&account_id=\(accountID)")
 
         let input = comps?.url?.absoluteString ?? ""
         let data = input.data(using: String.Encoding.ascii)
