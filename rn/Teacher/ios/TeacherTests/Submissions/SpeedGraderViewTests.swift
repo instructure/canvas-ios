9299bb6c0a4841abfe37e69e0edcb3bc7c004a7d
Andy VanWagoner <andy@instructure.com>
2020-09-23 13:00:44 -0600

    don't worry about covering SwiftUI views

--- rn/Teacher/ios/TeacherTests/Submissions/SpeedGraderViewTests.swift
+++ rn/Teacher/ios/TeacherTests/Submissions/SpeedGraderViewTests.swift
@@ -26,7 +26,7 @@ class SpeedGraderViewTests: TeacherTestCase {
     lazy var controller: CoreHostingController<SpeedGraderView> = {
         api.mock(GetAssignment(courseID: "1", assignmentID: "1", include: [ .overrides ]), value: .make())
         api.mock(GetSubmissions(context: .course("1"), assignmentID: "1", filter: nil), value: [
-            .make(submission_history: []),
+            .make(submission_history: [], user: .make(avatar_url: URL(string: "data:text/plain,"))),
         ])
         return hostSwiftUIController(SpeedGraderView(context: .course("1"), assignmentID: "1", userID: "1", filter: nil))
     }()
