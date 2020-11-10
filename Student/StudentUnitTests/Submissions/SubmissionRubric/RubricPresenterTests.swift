8fa85b97a00681074b02ba99263aaaf059532fa2
Andy VanWagoner <andy@instructure.com>
2020-11-09 20:23:28 -0700

    fix tests

--- Student/StudentUnitTests/Submissions/SubmissionRubric/RubricPresenterTests.swift
+++ Student/StudentUnitTests/Submissions/SubmissionRubric/RubricPresenterTests.swift
@@ -51,13 +51,14 @@ class RubricPresenterTests: StudentTestCase {
     }
 
     func setupData() -> [RubricViewModel] {
-        Assignment.make()
+        let assignment = Assignment.make(from: .make(rubric: [
+            .make(id: "1", ratings: [
+                .make(id: "1", points: 10),
+                .make(id: "2", points: 25),
+            ]),
+        ]))
         Course.make()
         ContextColor.make()
-        let rubric = Rubric.make(from: .make(id: "1", ratings: [
-            .make(id: "1", points: 10, position: 1),
-            .make(id: "2", points: 25, position: 2),
-        ]))
         Submission.make(from: .make(rubric_assessment: [
             "1": .make(points: 10.0, rating_id: "1"),
             "2": .make(points: 25.0, rating_id: "2"),
@@ -72,7 +73,7 @@ class RubricPresenterTests: StudentTestCase {
                 ratings: [10.0, 25.0],
                 descriptions: ["Excellent", "Excellent"],
                 comment: "You failed at punctuation!",
-                rubricRatings: Array(rubric.ratings!).sorted { $0.points < $1.points },
+                rubricRatings: assignment.rubric![0].ratings!.sorted { $0.points < $1.points },
                 isCustomAssessment: false,
                 hideRubricPoints: false,
                 freeFormCriterionComments: false
@@ -84,7 +85,7 @@ class RubricPresenterTests: StudentTestCase {
         let expected = setupData()
 
         presenter.assignments.eventHandler()
-        XCTAssertEqual(presenter.rubrics.first?.ratings?.count, 2)
+        XCTAssertEqual(presenter.assignments.first?.rubric?.first?.ratings?.count, 2)
         XCTAssertEqual(models.count, expected.count)
         XCTAssertEqual(models.first, expected.first)
     }
@@ -93,16 +94,7 @@ class RubricPresenterTests: StudentTestCase {
         let expected = setupData()
 
         presenter.submissions.eventHandler()
-        XCTAssertEqual(presenter.rubrics.first?.ratings?.count, 2)
-        XCTAssertEqual(models.count, expected.count)
-        XCTAssertEqual(models.first, expected.first)
-    }
-
-    func testLoadRubrics() {
-        let expected = setupData()
-
-        presenter.rubrics.eventHandler()
-        XCTAssertEqual(presenter.rubrics.first?.ratings?.count, 2)
+        XCTAssertEqual(presenter.assignments.first?.rubric?.first?.ratings?.count, 2)
         XCTAssertEqual(models.count, expected.count)
         XCTAssertEqual(models.first, expected.first)
     }
@@ -111,7 +103,7 @@ class RubricPresenterTests: StudentTestCase {
         let expected = setupData()
 
         presenter.colors.eventHandler()
-        XCTAssertEqual(presenter.rubrics.first?.ratings?.count, 2)
+        XCTAssertEqual(presenter.assignments.first?.rubric?.first?.ratings?.count, 2)
         XCTAssertEqual(models.count, expected.count)
         XCTAssertEqual(models.first, expected.first)
     }
@@ -120,7 +112,7 @@ class RubricPresenterTests: StudentTestCase {
         let expected = setupData()
 
         presenter.courses.eventHandler()
-        XCTAssertEqual(presenter.rubrics.first?.ratings?.count, 2)
+        XCTAssertEqual(presenter.assignments.first?.rubric?.first?.ratings?.count, 2)
         XCTAssertEqual(models.count, expected.count)
         XCTAssertEqual(models.first, expected.first)
     }
@@ -129,14 +121,12 @@ class RubricPresenterTests: StudentTestCase {
         presenter.viewIsReady()
         let assignmentsStore = presenter.assignments as! TestStore
         let submissionsStore = presenter.submissions as! TestStore
-        let rubricsStore = presenter.rubrics as! TestStore
         let colorsStore = presenter.colors as! TestStore
         let coursesStore = presenter.courses as! TestStore
 
         wait(for: [
             assignmentsStore.refreshExpectation,
             submissionsStore.refreshExpectation,
-            rubricsStore.refreshExpectation,
             colorsStore.refreshExpectation,
             coursesStore.refreshExpectation,
         ], timeout: 0.1)
@@ -148,17 +138,16 @@ class RubricPresenterTests: StudentTestCase {
     }
 
     func testCustomGradeIsHandled() {
-        let ratings: [APIRubricRating] = [
-            .make(id: "1", points: 10, position: 1),
-            .make(id: "2", points: 25, position: 2),
-        ]
-
-        let rubric = Rubric.make(from: .make(id: "1", ratings: ratings))
         Submission.make(from: .make(rubric_assessment: [
-            "1": .make(points: 1.0, comments: "this is custom", rating_id: "3"),
+            "1": .make(comments: "this is custom", points: 1.0, rating_id: "3"),
         ]))
         Course.make()
-        Assignment.make()
+        let assignment = Assignment.make(from: .make(rubric: [
+            .make(id: "1", ratings: [
+                .make(id: "1", points: 10),
+                .make(id: "2", points: 25),
+            ]),
+        ]))
         ContextColor.make()
         let expected: [RubricViewModel] = [
             RubricViewModel(
@@ -170,7 +159,7 @@ class RubricPresenterTests: StudentTestCase {
                 ratings: [10.0, 25.0, 1.0],
                 descriptions: ["Excellent", "Excellent", "Custom Grade"],
                 comment: "this is custom",
-                rubricRatings: Array(rubric.ratings!).sorted { $0.points < $1.points },
+                rubricRatings: assignment.rubric![0].ratings!.sorted { $0.points < $1.points },
                 isCustomAssessment: true,
                 hideRubricPoints: false,
                 freeFormCriterionComments: false
@@ -179,23 +168,22 @@ class RubricPresenterTests: StudentTestCase {
 
         presenter.update()
 
-        XCTAssertEqual(presenter.rubrics.first?.ratings?.count, 2)
+        XCTAssertEqual(presenter.assignments.first?.rubric?.first?.ratings?.count, 2)
         XCTAssertEqual(models.count, expected.count)
         XCTAssertEqual(models.first, expected.first)
     }
 
     func testCustomGradeAndFreeFormCriterionCommentsTrue() {
-        let ratings: [APIRubricRating] = [
-            .make(id: "1", points: 10, position: 1),
-            .make(id: "2", points: 25, position: 2),
-        ]
-
-        let rubric = Rubric.make(from: .make(id: "1", ratings: ratings))
         Submission.make(from: .make(rubric_assessment: [
-            "1": .make(points: 1.0, comments: "this is custom", rating_id: "2"),
+            "1": .make(comments: "this is custom", points: 1.0, rating_id: "2"),
         ]))
         Course.make()
-        Assignment.make(from: .make(rubric_settings: .make(free_form_criterion_comments: true)))
+        let assignment = Assignment.make(from: .make(rubric: [
+            .make(id: "1", ratings:  [
+                .make(id: "1", points: 10),
+                .make(id: "2", points: 25),
+            ]),
+        ], rubric_settings: .make(free_form_criterion_comments: true)))
         ContextColor.make()
         let expected: [RubricViewModel] = [
             RubricViewModel(
@@ -207,7 +195,7 @@ class RubricPresenterTests: StudentTestCase {
                 ratings: [10.0, 25.0, 1.0],
                 descriptions: ["Excellent", "Excellent", "Custom Grade"],
                 comment: "this is custom",
-                rubricRatings: Array(rubric.ratings!).sorted { $0.points < $1.points },
+                rubricRatings: assignment.rubric![0].ratings!.sorted { $0.points < $1.points },
                 isCustomAssessment: true,
                 hideRubricPoints: false,
                 freeFormCriterionComments: true
@@ -216,7 +204,7 @@ class RubricPresenterTests: StudentTestCase {
 
         presenter.update()
 
-        XCTAssertEqual(presenter.rubrics.first?.ratings?.count, 2)
+        XCTAssertEqual(presenter.assignments.first?.rubric?.first?.ratings?.count, 2)
         XCTAssertEqual(models.count, expected.count)
         XCTAssertEqual(models.first, expected.first)
     }
@@ -224,18 +212,20 @@ class RubricPresenterTests: StudentTestCase {
     func testRubricViewModelRatingBlurb() {
         let r = Rubric.make()
         let rating = RubricRating.make()
-        let model = RubricViewModel(id: r.id,
-                                    title: r.desc,
-                                    longDescription: r.longDesc,
-                                    selectedDesc: "Custom Grade",
-                                    selectedIndex: 1,
-                                    ratings: [1.0, 2.0],
-                                    descriptions: ["Bad"],
-                                    comment: nil,
-                                    rubricRatings: [rating],
-                                    isCustomAssessment: true,
-                                    hideRubricPoints: false,
-                                    freeFormCriterionComments: false)
+        let model = RubricViewModel(
+            id: r.id,
+            title: r.desc,
+            longDescription: r.longDesc,
+            selectedDesc: "Custom Grade",
+            selectedIndex: 1,
+            ratings: [1.0, 2.0],
+            descriptions: ["Bad"],
+            comment: nil,
+            rubricRatings: [rating],
+            isCustomAssessment: true,
+            hideRubricPoints: false,
+            freeFormCriterionComments: false
+        )
 
         let a = model.ratingBlurb(0)
         let b = model.ratingBlurb(1)
