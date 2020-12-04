fcaa772f1d378e74f879fa6f67b5f4becf81621f
Balint Bartok <balint.bartok@instructure.com>
2020-12-04 14:40:30 +0100

    change access levels

--- rn/Teacher/ios/Teacher/SpeedGrader/SubmissionGrader.swift
+++ rn/Teacher/ios/Teacher/SpeedGrader/SubmissionGrader.swift
@@ -21,8 +21,10 @@ import Core
 
 struct SubmissionGrader: View {
     let index: Int
-    let assignment: Assignment
-    let submission: Submission
+    private let assignment: Assignment
+    private let submission: Submission
+    private var handleRefresh: (() -> Void)?
+
     @ObservedObject var attempts: Store<LocalUseCase<Submission>>
 
     @Binding var isPagingEnabled: Bool
@@ -34,10 +36,8 @@ struct SubmissionGrader: View {
     @State var tab: GraderTab = .grades
     @State var showRecorder: MediaCommentType?
 
-    var handleRefresh: (() -> Void)?
-
-    var selected: Submission { attempts.first { attempt == $0.attempt } ?? submission }
-    var file: File? {
+    private var selected: Submission { attempts.first { attempt == $0.attempt } ?? submission }
+    private var file: File? {
         selected.attachments?.first { fileID == $0.id } ??
         selected.attachments?.sorted(by: File.idCompare).first
     }
@@ -261,7 +261,7 @@ struct SubmissionGrader: View {
             .clipped()
     }
 
-    func snapDrawerTo(_ state: DrawerState) {
+    private func snapDrawerTo(_ state: DrawerState) {
         withTransaction(DrawerState.transaction) {
             drawerState = state
         }
