e7169630e16d77d0795b9be76ade8c8e17e413de
Istvan Devai <istvan.devai@instructure.com>
2021-03-18 09:10:53 +0100

    Extract grade slider, make it tappable along the Slider track, vertically align Slider min/max value Texts, only show bottom divider when the rubrics section is available.
    refs: MBL-15250
    affects: Teacher
    release note: Improved Grade Slider
    test plan: see ticket

---
+++ rn/Teacher/ios/Teacher/SpeedGrader/GradeSlider.swift
@@ -0,0 +1,74 @@
+//
+// This file is part of Canvas.
+// Copyright (C) 2021-present  Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import SwiftUI
+
+struct GradeSlider: View {
+    var value: Binding<Double>
+    var range: ClosedRange<Double>
+    var showTooltip: Bool
+    var tooltipText: Text
+    var score: Double
+    var possible: Double
+    var onEditingChanged: (Bool) -> Void = { _ in }
+
+    var body: some View {
+        GeometryReader { geometry in
+            Slider(value: value, in: range, onEditingChanged: onEditingChanged)
+                .gesture(DragGesture(minimumDistance: 0).onChanged { changeValue in
+                    let percent = min(max(0, Double(changeValue.location.x / geometry.size.width)), 1)
+                    let newValue = range.lowerBound + round(percent * (range.upperBound - range.lowerBound))
+                    value.wrappedValue = newValue
+                    onEditingChanged(true)
+                }.onEnded { _ in
+                    onEditingChanged(false)
+                })
+                .overlay(!showTooltip ? nil : GeometryReader { geometry in
+                    let x = CGFloat(score / max(possible, 0.01))
+                        * (geometry.size.width - 26) + 13 // center on slider thumb 26 wide
+                    tooltipText
+                        .foregroundColor(.textLightest)
+                        .padding(8)
+                        .background(TooltipBackground().fill(Color.backgroundDarkest))
+                        .position()
+                        .offset(x: x, y: -26)
+                }, alignment: .bottom)
+        }
+    }
+}
+
+struct TooltipBackground: Shape {
+    func path(in rect: CGRect) -> Path { Path { path in
+        let r: CGFloat = 5
+        let arrowHeight: CGFloat = 5
+        let arrowWidth: CGFloat = 10
+        path.move(to: CGPoint(x: r, y: 0)) // top left, almost
+        path.addLine(to: CGPoint(x: rect.maxX - r, y: 0))
+        path.addArc(tangent1End: CGPoint(x: rect.maxX, y: 0), tangent2End: CGPoint(x: rect.maxX, y: r), radius: r)
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY - r))
+        path.addArc(tangent1End: CGPoint(x: rect.maxX, y: rect.maxY), tangent2End: CGPoint(x: rect.maxX - r, y: rect.maxY), radius: r)
+        path.addLine(to: CGPoint(x: rect.midX + arrowWidth / 2, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY + arrowHeight))
+        path.addLine(to: CGPoint(x: rect.midX - arrowWidth / 2, y: rect.maxY))
+        path.addLine(to: CGPoint(x: r, y: rect.maxY))
+        path.addArc(tangent1End: CGPoint(x: 0, y: rect.maxY), tangent2End: CGPoint(x: 0, y: rect.maxY - r), radius: r)
+        path.addLine(to: CGPoint(x: 0, y: r))
+        path.addArc(tangent1End: CGPoint(x: 0, y: 0), tangent2End: CGPoint(x: r, y: 0), radius: r)
+        }
+    }
+}
