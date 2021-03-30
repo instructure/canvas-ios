749897c5dd0d6560de6a416d5a79ead324555069
Balint Bartok <balint.bartok@instructure.com>
2021-03-30 20:01:35 +0200

    fix layout test

--- Core/CoreTests/People/PeopleListViewControllerTests.swift
+++ Core/CoreTests/People/PeopleListViewControllerTests.swift
@@ -35,7 +35,7 @@ class PeopleListViewControllerTests: CoreTestCase {
                 name: "Jane",
                 sortable_name: "jane doe",
                 short_name: "jane",
-                enrollments: [ .make(id: "2", role: "StudentEnrollment"), .make(id: "3", role: "Custom"), .make(id: "4", role: "StudentEnrollment"), ],
+                enrollments: [ .make(id: "2", user_id: "2", role: "StudentEnrollment"), .make(id: "3", user_id: "2", role: "Custom"), .make(id: "4", user_id: "2", role: "StudentEnrollment"), ],
                 pronouns: "She/Her"
             ),
         ])
