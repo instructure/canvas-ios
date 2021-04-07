434675e10a67cc36b8a0d3aad0a32214b95b5ef6
balintbartok <72031065+balintbartok@users.noreply.github.com>
2021-04-07 09:55:01 +0200

    Addonly_active_users parameter (#1784)
    refs: MBL-13735
    affects: Student
    release note: Fixed activity stream showing unpublished courses.
    
    test plan: See ticket

--- Core/Core/Notifications/APIActivity.swift
+++ Core/Core/Notifications/APIActivity.swift
@@ -85,7 +85,8 @@ public struct GetActivitiesRequest: APIRequestable {
         return "\(context.pathComponent)/activity_stream"
     }
 
-    public var query: [APIQueryItem] {
-        [ .perPage(perPage) ]
-    }
+    public var query: [APIQueryItem] {[
+        .value("only_active_courses", "true"),
+        .perPage(perPage),
+    ]}
 }
