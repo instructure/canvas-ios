4765c7ba091f452606156534b1fcb16fa9843681
Balint Bartok <balint.bartok@instructure.com>
2021-05-05 11:06:15 +0200

    reinstate performuiupdate

--- Core/Core/Profile/SideMenu/SideMenuBottomSection.swift
+++ Core/Core/Profile/SideMenu/SideMenuBottomSection.swift
@@ -112,17 +112,19 @@ struct SideMenuBottomSection: View {
     func handleLogout() {
         UploadManager.shared.isUploading { isUploading in
             guard let session = self.env.currentSession else { return }
-            let logoutBlock = {
-                self.env.router.dismiss(controller) {
-                    self.env.loginDelegate?.userDidLogout(session: session)
+            performUIUpdate {
+                let logoutBlock = {
+                    self.env.router.dismiss(controller) {
+                        self.env.loginDelegate?.userDidLogout(session: session)
+                    }
                 }
-            }
-            guard isUploading else {
-                logoutBlock()
-                return
-            }
-            self.showUploadAlert {
-                logoutBlock()
+                guard isUploading else {
+                    logoutBlock()
+                    return
+                }
+                self.showUploadAlert {
+                    logoutBlock()
+                }
             }
         }
     }
