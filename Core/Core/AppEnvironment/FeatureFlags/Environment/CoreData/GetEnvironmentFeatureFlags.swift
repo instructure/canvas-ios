9f9ff803e75fc07f6b78d0c158eb086f4ab851e6
Attila Varga <attila.varga@instructure.com>
2021-05-12 16:09:05 +0200

    Add usecase for environment feature flag fetch.
    refs: MBL-15371
    affects: Student
    release note: none
    
    test plan: K5 dashboard should show for environments where canvas for elementary is enabled and the K5 experimental feature flag is turned on.

---
+++ Core/Core/AppEnvironment/FeatureFlags/Environment/CoreData/GetEnvironmentFeatureFlags.swift
@@ -0,0 +1,33 @@
+//
+// This file is part of Canvas.
+// Copyright (C) 2021-present  Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import CoreData
+
+public class GetEnvironmentFeatureFlags: APIUseCase {
+    public typealias Model = EnvironmentFeatureFlags
+
+    public var request: GetEnvironmentFeatureFlagsRequest { GetEnvironmentFeatureFlagsRequest() }
+    public var cacheKey: String? { "environment-feature-flags" }
+
+    public func write(response: APIEnvironmentFeatureFlags?, urlResponse: URLResponse?, to client: NSManagedObjectContext) {
+        guard let response = response else { return }
+
+        let flags: EnvironmentFeatureFlags = client.fetch().first ?? client.insert()
+        flags.isCanvasForElementaryEnabled = response.canvas_for_elementary ?? false
+    }
+}
