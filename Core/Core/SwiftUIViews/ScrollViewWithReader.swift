7776cd6ef5d759cbf539adbbbd9af9e67756498d
Attila Varga <72396990+vargaat@users.noreply.github.com>
2021-05-31 16:21:38 +0200

    Make K5 top navbar scroll to the selected item. (#1843)
    refs: MBL-15374
    affects: Student
    release note: none
    
    test plan: Items should scroll to screen on iOS 14 when selected if a part of them is off-screen.

---
+++ Core/Core/SwiftUIViews/ScrollViewWithReader.swift
@@ -0,0 +1,39 @@
+//
+// This file is part of Canvas.
+// Copyright (C) 2021-present  Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import SwiftUI
+
+public struct ScrollViewWithReader<Content>: View where Content: View {
+    private let contentBuilder: (CompatibleScrollViewProxy) -> Content
+    private let axes: Axis.Set
+    private let showsIndicators: Bool
+
+    public var body: some View {
+        CompatibleScrollViewReader { proxy in
+            ScrollView(axes, showsIndicators: showsIndicators) {
+                contentBuilder(proxy)
+            }
+        }
+    }
+
+    public init(_ axes: Axis.Set = .vertical, showsIndicators: Bool = true, @ViewBuilder contentBuilder: @escaping (CompatibleScrollViewProxy) -> Content) {
+        self.axes = axes
+        self.showsIndicators = showsIndicators
+        self.contentBuilder = contentBuilder
+    }
+}
