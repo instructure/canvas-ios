08cb0b4d0a042ae8c3007be351ff61c28636c73a
Attila Varga <attila.varga@instructure.com>
2021-06-03 11:19:44 +0200

    Add tests for announcement load in view model.

---
+++ Core/CoreTests/Dashboard/K5/ViewModel/K5HomeroomViewModelTests.swift
@@ -0,0 +1,73 @@
+//
+// This file is part of Canvas.
+// Copyright (C) 2021-present  Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import XCTest
+@testable import Core
+
+class K5HomeroomViewModelTests: CoreTestCase {
+
+    func testLoadsHomeroomAnnouncements() {
+        mockAnnouncements(message: "message 2")
+        mockDashboardCards()
+
+        let testee = K5HomeroomViewModel()
+
+        XCTAssertEqual(testee.announcements.count, 1)
+        guard let announcement = testee.announcements.first else { return }
+        XCTAssertEqual(announcement.courseName, "course2 name")
+        XCTAssertEqual(announcement.title, "title 2")
+        XCTAssertEqual(announcement.htmlContent, "message 2")
+        XCTAssertEqual(announcement.allAnnouncementsRoute, "/courses/2/announcements")
+    }
+
+    func testRefreshesAnnouncements() {
+        mockAnnouncements(message: "original message")
+        mockDashboardCards()
+        let testee = K5HomeroomViewModel()
+        let refrehCompleted = expectation(description: "Refresh completed")
+        refrehCompleted.assertForOverFulfill = true
+
+        mockAnnouncements(message: "updated message")
+        testee.refresh {
+            refrehCompleted.fulfill()
+        }
+
+        wait(for: [refrehCompleted], timeout: 1)
+        XCTAssertEqual(testee.announcements.count, 1)
+        guard let announcement = testee.announcements.first else { return }
+        XCTAssertEqual(announcement.htmlContent, "updated message")
+    }
+
+    private func mockAnnouncements(message: String) {
+        let mockRequest = GetAllAnnouncementsRequest(contextCodes: ["course_2", "course_1"], activeOnly: true, latestOnly: true)
+        let mockResponse = [
+            APIDiscussionTopic.make(context_code: "course_2", message: message, posted_at: Date(timeIntervalSince1970: 74874), title: "title 2"),
+        ]
+        api.mock(mockRequest, value: mockResponse)
+    }
+
+    private func mockDashboardCards() {
+        let mockRequest = GetDashboardCardsRequest()
+        let mockResponse = [
+            APIDashboardCard.make(id: "1", isHomeroom: false),
+            APIDashboardCard.make(id: "2", isHomeroom: true, shortName: "course2 name"),
+        ]
+        api.mock(mockRequest, value: mockResponse)
+
+    }
+}
