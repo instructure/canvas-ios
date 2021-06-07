57f71464f310f1d7ef377bcdc14cb117d68e8a23
Attila Varga <attila.varga@instructure.com>
2021-06-07 12:31:33 +0200

    Add info line UI to card.

--- Core/Core/Dashboard/K5/View/Homeroom/K5HomeroomSubjectCardView.swift
+++ Core/Core/Dashboard/K5/View/Homeroom/K5HomeroomSubjectCardView.swift
@@ -42,21 +42,24 @@ public struct K5HomeroomSubjectCardView: View {
                 image
                 viewModel.color.frame(width: imageSize.width, height: 2)
                 Text(viewModel.name)
-                    .font(.regular16)
+                    .font(.semibold16)
                     .foregroundColor(viewModel.color)
                     .lineLimit(2).fixedSize(horizontal: false, vertical: true)
-                    .padding(.horizontal, 10).padding(.top, 8)
+                    .padding(.horizontal, 8).padding(.top, 8)
                 viewModel.color
                     .frame(height: 2)
                     .padding(.horizontal, 8)
                     .padding(.vertical, 5)
+                infoLines
                 Spacer()
             }
                 .background(RoundedRectangle(cornerRadius: 4).stroke(Color(white: 0.89), lineWidth: 1 / UIScreen.main.scale))
                 .background(Color.white)
                 .cornerRadius(4)
                 .shadow(color: Color.black.opacity(0.2), radius: 1, x: 0, y: 1)
+
         })
+
         .buttonStyle(ScaleButtonStyle(scale: 1))
         .frame(width: cardSize.width, height: cardSize.height)
         .frame(minHeight: cardSize.height)
@@ -74,13 +77,55 @@ public struct K5HomeroomSubjectCardView: View {
         }
             .frame(width: imageSize.width, height: imageSize.height)
     }
+
+    private var infoLines: some View {
+        VStack(alignment: .leading, spacing: 5) {
+            ForEach(0..<viewModel.infoLines.count) { index in
+                infoLine(from: viewModel.infoLines[index])
+
+                if index != viewModel.infoLines.count - 1 {
+                    Divider()
+                }
+            }
+        }
+        .foregroundColor(.licorice)
+        .padding(.horizontal, 8)
+        .environment(\.font, Font.regular14)
+        .environment(\.lineLimit, 2)
+    }
+
+    private func infoLine(from model: K5HomeroomSubjectCardViewModel.InfoLine) -> some View {
+        HStack(alignment: .top, spacing: 5) {
+            model.icon
+                .resizable()
+                .foregroundColor(viewModel.color)
+                .scaledToFill()
+                .frame(width: 18, height: 18)
+            Text(model.text)
+            +
+            Text(model.highlightedText)
+                .foregroundColor(.red)
+        }
+    }
 }
 
 struct K5HomeroomSubjectCardView_Previews: PreviewProvider {
-    static var previews: some View {
-        let model = K5HomeroomSubjectCardViewModel(courseId: "1", imageURL: URL(string: "https://inst.prod.acquia-sites.com/sites/default/files/image/2021-01/Instructure%20Office.jpg")!, name: "SOCIAL STUDIES", color: .oxford, infoLines: [])
-        K5HomeroomSubjectCardView(viewModel: model, width: 193).previewLayout(.sizeThatFits)
-        K5HomeroomSubjectCardView(viewModel: model, width: 400).previewLayout(.sizeThatFits)
+    private static let env = PreviewEnvironment()
+    private static let context = env.globalDatabase.viewContext
 
+    static var previews: some View {
+        let announcement = LatestAnnouncement.save(.make(message: "I will be out on Thursday. Mrs. Robinson will be substituting."), in: context)
+        let models = [
+            K5HomeroomSubjectCardViewModel(courseId: "1", imageURL: URL(string: "https://inst.prod.acquia-sites.com/sites/default/files/image/2021-01/Instructure%20Office.jpg")!, name: "SOCIAL STUDIES", color: .electric, infoLines: [.make(dueToday: 0, missing: 0)]),
+            K5HomeroomSubjectCardViewModel(courseId: "1", imageURL: URL(string: "https://inst.prod.acquia-sites.com/sites/default/files/image/2021-01/Instructure%20Office.jpg")!, name: "SOCIAL STUDIES", color: .electric, infoLines: [
+                                            .make(dueToday: 3, missing: 1),
+                                            .make(from: announcement)!
+                                            ]),
+        ]
+
+        ForEach(0..<models.count) { index in
+            K5HomeroomSubjectCardView(viewModel: models[index], width: 193).previewLayout(.sizeThatFits)
+            K5HomeroomSubjectCardView(viewModel: models[index], width: 400).previewLayout(.sizeThatFits)
+        }
     }
 }
