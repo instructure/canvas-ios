474f03401f105d38d3da632bcd34b0ad27b1d797
David Varga <david.varga@instructure.com>
2021-06-11 08:59:11 +0200

    Add AssignmentsE2ETest for Teacher
    refs: MBL-15472
    affects: Teacher
    release note: none
    
    test plan:

---
+++ rn/Teacher/ios/TeacherE2ETests/AssignmentsE2ETests.swift
@@ -0,0 +1,38 @@
+//
+// This file is part of Canvas.
+// Copyright (C) 2021-present  Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import XCTest
+import TestsFoundation
+
+class AssignmentsE2ETests: CoreUITestCase {
+    func testAssignmentsE2E() {
+        Dashboard.courseCard(id: "263").waitToExist()
+        Dashboard.courseCard(id: "263").tap()
+        CourseNavigation.assignments.tap()
+        AssignmentsList.assignment(id: "1831").tap()
+        AssignmentDetails.description("This is assignment one.").waitToExist()
+        NavBar.backButton.tap()
+        AssignmentsList.assignment(id: "261986").tap()
+        app.find(labelContaining: "10 pts").waitToExist()
+        app.find(labelContaining: "Needs Grading").waitToExist()
+        NavBar.backButton.tap()
+        NavBar.backButton.tap()
+        NavBar.backButton.tap()
+        Dashboard.courseCard(id: "263").waitToExist()
+    }
+}
