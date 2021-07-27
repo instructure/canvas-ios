62ed447b2c70725e2a96cc5c14143702fd3fd270
Istvan Devai <istvan.devai@instructure.com>
2021-07-27 09:10:06 +0200

    Reenable Schedule and Grades bar menu items, add Grades page base UI.
    refs: MBL-15157
    affects: Student
    release note: Canvas for Elementary grades page.
    test plan: none

--- Core/Core/Dashboard/K5/View/Grades/K5GradesView.swift
+++ Core/Core/Dashboard/K5/View/Grades/K5GradesView.swift
@@ -19,7 +19,95 @@
 import SwiftUI
 
 struct K5GradesView: View {
+
+    @State var gradeSelectorOpen = false
+
     var body: some View {
-        Text("Grades Details", bundle: .core)
+        VStack(spacing: 0) {
+            VStack(alignment: .leading, spacing: 0) {
+                Text("Select").font(.regular13)
+                HStack {
+                    Text("Current Grading Period").font(.bold24)
+                    Image.arrowOpenDownSolid.rotationEffect(.degrees(gradeSelectorOpen ? -180 : 0)).animation(.easeOut)
+                    Spacer()
+                }.onTapGesture {
+                    gradeSelectorOpen.toggle()
+                }
+                if gradeSelectorOpen {
+                    Text("Grading periods go here")
+                }
+            }.padding(.top, 15).padding(.bottom, 13)
+            Divider()
+            ScrollView {
+                VStack {
+                    Text("Grade cells go here")
+                    Spacer()
+                }
+            }
+        }
+    }
+}
+
+struct K5GradesView_Previews: PreviewProvider {
+    static var previews: some View {
+        K5GradesView()
+    }
+}
+
+struct K5GradeCell: View {
+
+    @State var color: Color
+    @State var title: String
+    @State var gradeValue: Int
+
+    var body: some View {
+        GeometryReader { geometry in
+            HStack(spacing: 13) {
+                if geometry.size.width > 396 {
+                    Image.eyeSolid.frame(width: 72, height: 72).cornerRadius(4).foregroundColor(color)
+                }
+                VStack(alignment: .leading, spacing: 6) {
+                    Text(title)
+                        .font(.bold17)
+                    GradeProgressBar(value: gradeValue, color: color).frame(height: 16)
+                    Text("\(gradeValue)" + "%").font(.regular17)
+                }
+            }
+        }.frame(minHeight: 72).padding(.top, 13.5).padding(.bottom, 13.5)
+    }
+}
+
+struct K5GradeCell_Previews: PreviewProvider {
+    static var previews: some View {
+        K5GradeCell(color: .yellow, title: "ART", gradeValue: 50)
+    }
+}
+
+struct GradeProgressBar: View {
+    @State var value: Int
+    @State var color: Color
+    @State private var animate: Bool = false
+
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle().frame(width: geometry.size.width, height: geometry.size.height)
+                    .foregroundColor(.clear)
+                    .border(color, width: 1)
+
+                Rectangle().frame(width: min(CGFloat(animate ? value : 0)/100.0*geometry.size.width, geometry.size.width),
+                                  height: geometry.size.height)
+                    .foregroundColor(color)
+                    .animation(.spring(response: 0.5, dampingFraction: 0.5, blendDuration: 0.5))
+            }
+        }.onAppear {
+            animate.toggle()
+        }
+    }
+}
+
+struct GradeProgressBar_Previews: PreviewProvider {
+    static var previews: some View {
+        GradeProgressBar(value: 50, color: .red).frame(height: 16)
     }
 }
