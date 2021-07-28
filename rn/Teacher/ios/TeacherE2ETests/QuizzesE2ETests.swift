9fcf9594f24bf46b49f98c5b58f71433f2b4659f
David Varga <david.varga@instructure.com>
2021-07-28 09:20:27 +0200

    Added E2E Test for classic quiz
    refs: MBL-15528
    affects: Teacher
    release note: none
    
    test plan:

---
+++ rn/Teacher/ios/TeacherE2ETests/QuizzesE2ETests.swift
@@ -0,0 +1,42 @@
+//
+// This file is part of Canvas.
+// Copyright (C) 2021-present  Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import XCTest
+import TestsFoundation
+
+class QuizzesE2ETests: CoreUITestCase {
+    func testQuizzesE2E() {
+        Dashboard.courseCard(id: "263").waitToExist()
+        Dashboard.courseCard(id: "263").tap()
+        CourseNavigation.quizzes.tap()
+        app.find(label: "Quiz One").tap()
+        QuizDetails.previewQuiz.waitToExist()
+        app.find(label: "This is the first quiz.").waitToExist()
+        QuizDetails.previewQuiz.tap()
+        app.find(label: "Quiz Preview").waitToExist()
+        app.find(label: "Quiz Instructions").waitToExist()
+        app.find(label: "This is question A").waitToExist()
+        XCTAssertTrue(app.find(labelContaining: "This is a preview of the published version of the quiz").exists())
+        app.find(label: "Done").tap()
+        app.find(label: "Edit").tap()
+        XCTAssertTrue(app.find(labelContaining: "Edit Quiz Details").exists())
+        XCTAssertTrue(app.find(labelContaining: "Shuffle Answers").exists())
+        app.find(label: "Done").tap()
+        XCTAssertTrue(QuizDetails.previewQuiz.exists())
+    }
+}
