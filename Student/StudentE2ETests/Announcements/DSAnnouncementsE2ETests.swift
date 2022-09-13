b25b21d095cc4c175ed00112f776e2cc33c40b4f
David Varga <david.varga@instructure.com>
2022-09-13 09:05:00 +0200

    Added Global Announcements DataSeeding
    refs: MBL-16223
    affects: Student, Teacher
    release note: none
    
    test plan:

--- Student/StudentE2ETests/Announcements/DSAnnouncementsE2ETests.swift
+++ Student/StudentE2ETests/Announcements/DSAnnouncementsE2ETests.swift
@@ -41,4 +41,38 @@ class DSAnnouncementsE2ETests: E2ETestCase {
         XCTAssertEqual(DiscussionDetails.title.label(), announcementTitle)
         XCTAssertTrue(app.find(label: announcementMessage).exists())
     }
+
+    @available(iOS 15, *)
+    func testAnnouncementToggleE2E() {
+        let student = seeder.createUser()
+        let teacher = seeder.createUser()
+        let course = seeder.createCourse()
+        seeder.enrollTeacher(teacher, in: course)
+        seeder.enrollStudent(student, in: course)
+
+        let dateFormatter = ISO8601DateFormatter()
+        let globalAnnouncementSubject = "This is a GA"
+        let globalAnnouncementMessage = "This will disappear in 4 minutes"
+        let globalAnnouncementStartAt = dateFormatter.string(from: Date.now.addMinutes(-1))
+        let globalAnnouncementEndAt = dateFormatter.string(from: Date.now.addMinutes(3))
+
+        let globalAnnouncement = seeder.postAccountNotifications(requestBody:
+                .init(subject: globalAnnouncementSubject, message: globalAnnouncementMessage,
+                      start_at: globalAnnouncementStartAt, end_at: globalAnnouncementEndAt)
+        )
+
+        logInDSUser(student)
+        Dashboard.courseCard(id: course.id).waitToExist()
+        app.find(label: globalAnnouncement.subject).waitToExist()
+
+        AccountNotifications.toggleButton(id: globalAnnouncement.id).waitToExist()
+        XCTAssertFalse(AccountNotifications.dismissButton(id: globalAnnouncement.id).isVisible)
+
+        AccountNotifications.toggleButton(id: globalAnnouncement.id).tap()
+        AccountNotifications.dismissButton(id: globalAnnouncement.id).waitToExist()
+        app.find(label: globalAnnouncement.message).waitToExist()
+
+        AccountNotifications.dismissButton(id: globalAnnouncement.id).tap()
+        AccountNotifications.dismissButton(id: globalAnnouncement.id).waitToVanish()
+    }
 }
