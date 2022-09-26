ed2af6f07b2717f4e127d0f75bb0ff1f0eaf6aea
Attila Varga <attila.varga@instructure.com>
2022-09-26 09:27:24 +0200

    Remove unused variable.

--- Core/Core/Store/Store.swift
+++ Core/Core/Store/Store.swift
@@ -75,7 +75,6 @@ public class Store<U: UseCase>: NSObject, NSFetchedResultsControllerDelegate {
     public private(set) var pending: Bool = false
     public private(set) var requested: Bool = false
     public private(set) var error: Error?
-    private var externalStoreChangeListener: AnyCancellable?
 
     public var state: StoreState {
         error != nil ? .error :
