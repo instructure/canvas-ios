8d2159827d0ec4392e864a17de5f5cecfaf7f9c4
Attila Varga <attila.varga@instructure.com>
2022-11-02 16:10:12 +0100

    Update unit tests.

--- Core/CoreTests/Dashboard/ViewModel/DashboardLayoutViewModelTests.swift
+++ Core/CoreTests/Dashboard/ViewModel/DashboardLayoutViewModelTests.swift
@@ -21,57 +21,9 @@ import XCTest
 
 class DashboardLayoutViewModelTests: CoreTestCase {
 
-    func testInitialButtonStateForGridLayout() {
-        environment.userDefaults?.isDashboardLayoutGrid = true
-        let testee = DashboardLayoutViewModel()
-        XCTAssertEqual(testee.buttonImage, .dashboardLayoutList)
-        XCTAssertEqual(testee.buttonA11yLabel, NSLocalizedString("Change dashboard layout to list", comment: ""))
-    }
-
-    func testInitialButtonStateForListLayout() {
-        environment.userDefaults?.isDashboardLayoutGrid = false
-        let testee = DashboardLayoutViewModel()
-        XCTAssertEqual(testee.buttonImage, .dashboardLayoutGrid)
-        XCTAssertEqual(testee.buttonA11yLabel, NSLocalizedString("Change dashboard layout to grid", comment: ""))
-    }
-
-    func testToggleChangesButtonImage() {
-        let buttonChangeEvent = expectation(description: "UI update received")
-        buttonChangeEvent.expectedFulfillmentCount = 2 // initial state, change
-        var listButtonImageReceived = false
-        var gridButtonImageReceived = false
-        let testee = DashboardLayoutViewModel()
-        let subscription = testee.$buttonImage.sink { image in
-            buttonChangeEvent.fulfill()
-
-            if image == .dashboardLayoutList {
-                listButtonImageReceived = true
-            } else if image == .dashboardLayoutGrid {
-                gridButtonImageReceived = true
-            }
-        }
-
-        testee.toggle()
-        wait(for: [buttonChangeEvent], timeout: 0.1)
-        XCTAssertTrue(listButtonImageReceived)
-        XCTAssertTrue(gridButtonImageReceived)
-
-        subscription.cancel()
-    }
-
-    func testTogglePersistsState() {
-        let initialState = environment.userDefaults!.isDashboardLayoutGrid
-        let testee = DashboardLayoutViewModel()
-
-        testee.toggle()
-
-        let newState = environment.userDefaults!.isDashboardLayoutGrid
-        XCTAssertNotEqual(initialState, newState)
-    }
-
     func testGridLayoutCalculation() {
         environment.userDefaults?.isDashboardLayoutGrid = true
-        let testee = DashboardLayoutViewModel()
+        let testee = DashboardLayoutViewModel(interactor: DashboardSettingsInteractorPreview())
 
         let smallLayout = testee.layoutInfo(for: 600)
         XCTAssertEqual(smallLayout.columns, 2)
@@ -80,46 +32,13 @@ class DashboardLayoutViewModelTests: CoreTestCase {
     }
 
     func testListLayoutCalculation() {
-        environment.userDefaults?.isDashboardLayoutGrid = false
-        let testee = DashboardLayoutViewModel()
+        let interactor = DashboardSettingsInteractorPreview()
+        interactor.layout.send(.list)
+        let testee = DashboardLayoutViewModel(interactor: interactor)
 
         let smallLayout = testee.layoutInfo(for: 600)
         XCTAssertEqual(smallLayout.columns, 1)
         let largeLayout = testee.layoutInfo(for: 650)
         XCTAssertEqual(largeLayout.columns, 1)
-    }
-
-    func testAnalyticsReportforGridLayout() {
-        environment.userDefaults?.isDashboardLayoutGrid = true
-        _ = DashboardLayoutViewModel()
-
-        guard
-              analytics.events.count == 1,
-              let eventName = analytics.events.last?.name,
-              let params = analytics.events.last?.parameters
-        else {
-            XCTFail("Unexpected event or parameter count")
-            return
-        }
-
-        XCTAssertEqual(eventName, "dashboard_layout")
-        XCTAssertEqual(params as? [String: String], ["type": "grid"])
-    }
-
-    func testAnalyticsReportforListLayout() {
-        environment.userDefaults?.isDashboardLayoutGrid = false
-        _ = DashboardLayoutViewModel()
-
-        guard
-              analytics.events.count == 1,
-              let eventName = analytics.events.last?.name,
-              let params = analytics.events.last?.parameters
-        else {
-            XCTFail("Unexpected event or parameter count")
-            return
-        }
-
-        XCTAssertEqual(eventName, "dashboard_layout")
-        XCTAssertEqual(params as? [String: String], ["type": "list"])
     }
 }
