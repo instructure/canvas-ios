89eee798c9970a5363522a7139196baf4289fef2
Attila Varga <attila.varga@instructure.com>
2023-01-05 14:05:50 +0100

    refs: MBL-16451 affects: Student, Teacher, Parent release note: none
    test plan:
    - Setup production secrets for crashlytics.
    - Switch scheme's Run action to build Production config, uncheck debug executable.
    - Run the app.
    - Switch screens.
    - Go to developer menu and tap Force Native Crash.
    - Restart the app.
    - Locate the app.DSYM file in derived data, zip it.
    - Upload it to crashlytics.
    - Wait for the crash to be symbolicated.
    - Check if crash logs include screen routes.

--- rn/Teacher/ios/Teacher/TeacherAppDelegate.swift
+++ rn/Teacher/ios/Teacher/TeacherAppDelegate.swift
@@ -204,7 +204,6 @@ class TeacherAppDelegate: UIResponder, UIApplicationDelegate, UNUserNotification
                     let value = remoteConfig.configValue(forKey: key).boolValue
                     feature.isEnabled = value
                     Firebase.Crashlytics.crashlytics().setCustomValue(value, forKey: feature.userDefaultsKey)
-//                    Analytics.setUserProperty(value ? "YES" : "NO", forName: feature.rawValue)
                 }
             }
         }
@@ -213,8 +212,14 @@ class TeacherAppDelegate: UIResponder, UIApplicationDelegate, UNUserNotification
 
 extension TeacherAppDelegate: AnalyticsHandler {
     func handleEvent(_ name: String, parameters: [String: Any]?) {
-        // Google Analytics needs to be disabled for now
-//        Analytics.logEvent(name, parameters: parameters)
+        guard FirebaseOptions.defaultOptions()?.apiKey != nil else {
+            return
+        }
+
+        if let screenName = parameters?["screen_name"] as? String,
+           let screenClass = parameters?["screen_class"] as? String {
+            Firebase.Crashlytics.crashlytics().log("\(screenName) (\(screenClass))")
+        }
     }
 
     private func initializeTracking() {
