1961eb94c596854994a189dd1c49c8a4e1fd90a5
Istvan Devai <istvan.devai@instructure.com>
2023-01-19 15:57:04 +0100

    Add LoginUsePolicy viewmodel and view.
    refs: MBL-16319
    affects: Student, Teacher
    release note: Added acceptable use policy screen on login.
    test plan: See ticket.

---
+++ Core/Core/Login/LoginUsePolicy/LoginUsePolicyViewModel.swift
@@ -0,0 +1,57 @@
+//
+// This file is part of Canvas.
+// Copyright (C) 2023-present  Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import Foundation
+
+public class LoginUsePolicyViewModel: ObservableObject {
+
+    @Published var isAccepted: Bool = false
+    @Published var requestError: Error?
+
+    private let accepted: (() -> Void)
+    private let cancelled: (() -> Void)
+
+    init(accepted: @escaping (() -> Void), cancelled: @escaping (() -> Void)) {
+        self.accepted = accepted
+        self.cancelled = cancelled
+    }
+
+    public func submitAcceptance() {
+        acceptUsePolicy { result in
+            switch result {
+            case let .failure(error):
+                self.requestError = error
+            case .success:
+                self.accepted()
+            }
+        }
+    }
+
+    public func cancelAcceptance() {
+        cancelled()
+    }
+
+    private func acceptUsePolicy(_ callback: @escaping (Result<Void, Error>) -> Void) {
+        AppEnvironment.shared.api.makeRequest(PutUserAcceptedTermsRequest(hasAccepted: true)) { _, _, error in
+            guard error == nil else {
+                return callback(.failure(error ?? NSError.internalError()))
+            }
+            callback(.success(()))
+        }
+    }
+}
