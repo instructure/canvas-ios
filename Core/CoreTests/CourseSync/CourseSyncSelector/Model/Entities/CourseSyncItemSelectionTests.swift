d5f81e962bc29926e6f2538c89dd5209bc20c845
szabinst <szabolcs.fuzi@instructure.com>
2023-06-21 11:06:21 +0200

    chore: update selection tests

--- Core/CoreTests/CourseSync/CourseSyncSelector/Model/Entities/CourseSyncItemSelectionTests.swift
+++ Core/CoreTests/CourseSync/CourseSyncSelector/Model/Entities/CourseSyncItemSelectionTests.swift
@@ -61,57 +61,57 @@ class CourseSyncItemSelectionTests: XCTestCase {
 
     func testMapsSelectedCourseButNotTabsOrFiles() {
         let course = CourseSyncEntry(name: "",
-                                     id: "1",
+                                     id: "course-1",
                                      tabs: [
-                                        .init(id: "2", name: "", type: .pages, selectionState: .selected)
+                                        .init(id: "tab-pages", name: "", type: .pages, selectionState: .selected)
                                      ],
                                      files: [
-                                        .init(id: "3", displayName: "", fileName: "",
+                                        .init(id: "file-1", displayName: "", fileName: "",
                                               url: URL(string: "/")!, mimeClass: "", selectionState: .selected, bytesToDownload: 0),
                                      ],
                                      selectionState: .selected)
-        XCTAssertEqual(CourseSyncItemSelection.make(from: [course]), ["1"])
+        XCTAssertEqual(CourseSyncItemSelection.make(from: [course]), ["course-1"])
     }
 
     func testMapsSelectedFilesTabButNotFiles() {
         let course = CourseSyncEntry(name: "",
-                                     id: "1",
+                                     id: "course-1",
                                      tabs: [
-                                        .init(id: "2", name: "", type: .files, selectionState: .selected),
+                                        .init(id: "tab-files", name: "", type: .files, selectionState: .selected),
                                      ],
                                      files: [
-                                        .init(id: "3", displayName: "", fileName: "",
+                                        .init(id: "file-1", displayName: "", fileName: "",
                                               url: URL(string: "/")!, mimeClass: "", selectionState: .selected, bytesToDownload: 0),
                                      ],
                                      selectionState: .partiallySelected)
-        XCTAssertEqual(CourseSyncItemSelection.make(from: [course]), ["1"])
+        XCTAssertEqual(CourseSyncItemSelection.make(from: [course]), ["tab-files"])
     }
 
     func testMapsSelectedTabs() {
         let course = CourseSyncEntry(name: "",
-                                     id: "1",
+                                     id: "course-1",
                                      tabs: [
-                                        .init(id: "2", name: "", type: .files, selectionState: .selected),
-                                        .init(id: "3", name: "", type: .files, selectionState: .deselected),
+                                        .init(id: "tab-files-1", name: "", type: .files, selectionState: .selected),
+                                        .init(id: "tab-files-2", name: "", type: .files, selectionState: .deselected),
                                      ],
                                      files: [],
                                      selectionState: .partiallySelected)
-        XCTAssertEqual(CourseSyncItemSelection.make(from: [course]), ["1"])
+        XCTAssertEqual(CourseSyncItemSelection.make(from: [course]), ["tab-files-1"])
     }
 
     func testMapsSelectedFiles() {
         let course = CourseSyncEntry(name: "",
-                                     id: "1",
+                                     id: "course-1",
                                      tabs: [
-                                        .init(id: "2", name: "", type: .files, selectionState: .partiallySelected),
+                                        .init(id: "tab-files", name: "", type: .files, selectionState: .partiallySelected),
                                      ],
                                      files: [
-                                        .init(id: "3", displayName: "", fileName: "",
+                                        .init(id: "file-1", displayName: "", fileName: "",
                                               url: URL(string: "/")!, mimeClass: "", selectionState: .deselected, bytesToDownload: 0),
-                                        .init(id: "4", displayName: "", fileName: "",
+                                        .init(id: "file-2", displayName: "", fileName: "",
                                               url: URL(string: "/")!, mimeClass: "", selectionState: .selected, bytesToDownload: 0),
                                      ],
                                      selectionState: .partiallySelected)
-        XCTAssertEqual(CourseSyncItemSelection.make(from: [course]), ["1"])
+        XCTAssertEqual(CourseSyncItemSelection.make(from: [course]), ["file-2"])
     }
 }
--- Core/CoreTests/CourseSync/CourseSyncSelector/ViewModel/CourseSyncSelectorViewModelItemTests.swift
+++ Core/CoreTests/CourseSync/CourseSyncSelector/ViewModel/CourseSyncSelectorViewModelItemTests.swift
@@ -163,7 +163,7 @@ class CourseSyncSelectorViewModelItemTests: XCTestCase {
 
         XCTAssertEqual(testee.id, "1")
         XCTAssertEqual(testee.title, "testFile")
-        XCTAssertEqual(testee.subtitle, nil)
+        XCTAssertEqual(testee.subtitle, "Zero KB")
         XCTAssertEqual(testee.isCollapsed, nil)
         XCTAssertTrue(testee.cellStyle == .listItem)
 
@@ -201,13 +201,13 @@ class CourseSyncSelectorViewModelItemTests: XCTestCase {
         }
 
         item0.selectionDidToggle?()
-        XCTAssertEqual(mockInteractor.lastSelected?.selection, CourseEntrySelection.course("0"))
+        XCTAssertEqual(mockInteractor.lastSelected?.selection, CourseEntrySelection.course("testID"))
         XCTAssertEqual(mockInteractor.lastSelected?.isSelected, true)
         item2.selectionDidToggle?()
-        XCTAssertEqual(mockInteractor.lastSelected?.selection, CourseEntrySelection.tab("0", "1"))
+        XCTAssertEqual(mockInteractor.lastSelected?.selection, CourseEntrySelection.tab("testID", "0"))
         XCTAssertEqual(mockInteractor.lastSelected?.isSelected, true)
         item3.selectionDidToggle?()
-        XCTAssertEqual(mockInteractor.lastSelected?.selection, CourseEntrySelection.file("0", "0"))
+        XCTAssertEqual(mockInteractor.lastSelected?.selection, CourseEntrySelection.file("testID", "0"))
         XCTAssertEqual(mockInteractor.lastSelected?.isSelected, true)
     }
 
@@ -229,7 +229,7 @@ class CourseSyncSelectorViewModelItemTests: XCTestCase {
         }
 
         item.collapseDidToggle?()
-        XCTAssertEqual(mockInteractor.lastCollapsed?.selection, CourseEntrySelection.course("0"))
+        XCTAssertEqual(mockInteractor.lastCollapsed?.selection, CourseEntrySelection.course("testID"))
         XCTAssertEqual(mockInteractor.lastCollapsed?.isCollapsed, true)
     }
 
@@ -251,7 +251,7 @@ class CourseSyncSelectorViewModelItemTests: XCTestCase {
         }
 
         item.collapseDidToggle?()
-        XCTAssertEqual(mockInteractor.lastCollapsed?.selection, CourseEntrySelection.tab("0", "0"))
+        XCTAssertEqual(mockInteractor.lastCollapsed?.selection, CourseEntrySelection.tab("testID", "0"))
         XCTAssertEqual(mockInteractor.lastCollapsed?.isCollapsed, true)
     }
 }
