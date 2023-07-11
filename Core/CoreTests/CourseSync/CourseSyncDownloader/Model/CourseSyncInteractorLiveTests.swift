cd21245ad276a191f7b2fae72823c14d11996be2
Attila Varga <attila.varga@instructure.com>
2023-07-11 10:53:45 +0200

    Add test to check if syllabus selection correctly translates to syllabus interactor invocation.

--- Core/CoreTests/CourseSync/CourseSyncDownloader/Model/CourseSyncInteractorLiveTests.swift
+++ Core/CoreTests/CourseSync/CourseSyncDownloader/Model/CourseSyncInteractorLiveTests.swift
@@ -48,6 +48,7 @@ class CourseSyncInteractorLiveTests: CoreTestCase {
                     .init(id: "tab-assignments", name: "Assignments", type: .assignments),
                     .init(id: "tab-pages", name: "Pages", type: .pages),
                     .init(id: "tab-files", name: "Files", type: .files),
+                    .init(id: "tab-syllabus", name: "Syllabus", type: .syllabus),
                 ],
                 files: [
                     .make(id: "file-1", displayName: "1", url: URL(string: "1.jpg")!, bytesToDownload: 1000),
@@ -411,6 +412,39 @@ class CourseSyncInteractorLiveTests: CoreTestCase {
         XCTAssertEqual(entries[0].tabs[2].state, .error)
 
         subscription.cancel()
+    }
+
+    func testStartsSyllabusDownload() {
+        let syllbusDownloadStarted = expectation(description: "Syllabus download started")
+        let mockSyllabusInteractor = CourseSyncSyllabusInteractorMock(expectation: syllbusDownloadStarted)
+        let testee = CourseSyncInteractorLive(
+            pagesInteractor: pagesInteractor,
+            assignmentsInteractor: assignmentsInteractor,
+            filesInteractor: filesInteractor,
+            syllabusInteractor: mockSyllabusInteractor,
+            scheduler: .immediate
+        )
+        entries[0].tabs[3].selectionState = .selected
+
+        let subscription = testee.downloadContent(for: entries).sink()
+
+        wait(for: [syllbusDownloadStarted], timeout: 1)
+        subscription.cancel()
+    }
+}
+
+private class CourseSyncSyllabusInteractorMock: CourseSyncSyllabusInteractor {
+    let expectation: XCTestExpectation
+
+    init(expectation: XCTestExpectation) {
+        self.expectation = expectation
+    }
+
+    func getContent(courseId _: String) -> AnyPublisher<Void, Error> {
+        expectation.fulfill()
+        return Just(())
+            .setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
     }
 }
 
