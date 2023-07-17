c89b894c956fd47a09b12a3c997abf24717ecee9
Andras Szasz <andras.szasz@instructure.com>
2023-07-17 15:26:27 +0200

    testCalendarLayout

--- Student/StudentE2ETests/Calendar/CalendarHelper.swift
+++ Student/StudentE2ETests/Calendar/CalendarHelper.swift
@@ -19,14 +19,111 @@
 import TestsFoundation
 
 public class CalendarHelper: BaseHelper {
+    // MARK: UI Elements
+    public static var navBar: Element { app.find(id: "Core.PlannerView") }
+    public static var todayButton: Element { app.find(id: "PlannerCalendar.todayButton") }
+    public static var addNoteButton: Element { app.find(id: "PlannerCalendar.addNoteButton") }
+    public static var yearLabel: Element { app.find(id: "PlannerCalendar.yearLabel") }
+    public static var monthButton: Element { app.find(id: "PlannerCalendar.monthButton") }
+    public static var filterButton: Element { app.find(id: "PlannerCalendar.filterButton") }
+
+    public static func dayButton(date: String) -> Element {
+        // Date in "2023-7-16" format
+        return app.find(id: "PlannerCalendar.dayButton.\(date)")
+    }
+
+    public static func formatDateForDayButton(date: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-M-dd"
+        let formattedDate = dateFormatter.date(from: date)!
+        return dateFormatter.string(from: formattedDate)
+    }
+
+    public static func eventCell(event: DSCalendarEvent) -> Element {
+        return app.find(id: "PlannerList.event.\(event.id)")
+    }
+
+    public static func titleLabelOfEvent(eventCell: Element) -> Element {
+        return eventCell.rawElement.findAll(type: .staticText)[0]
+    }
+
+    public static func dateLabelOfEvent(eventCell: Element) -> Element {
+        return eventCell.rawElement.findAll(type: .staticText)[1]
+    }
+
+    public static func courseLabelOfEvent(eventCell: Element) -> Element {
+        return eventCell.rawElement.findAll(type: .staticText)[2]
+    }
+
+    // MARK: DataSeeding
+    public static func formatDate(addDays: Int = 0, addHours: Int = 0) -> String {
+        let date = Date().addDays(addDays).addMinutes(addHours*60)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        let formattedDate = dateFormatter.string(from: date)
+        return formattedDate
+    }
+
+    public static func createSampleCalendarEvents(course: DSCourse) -> SampleEvents {
+        let yesterdays = createCalendarEvent(course: course,
+                                             title: "Yesterdays Event",
+                                             startDate: formatDate(addDays: -1, addHours: 1),
+                                             endDate: formatDate(addDays: -1, addHours: 2))
+        let todays = createCalendarEvent(course: course,
+                                         title: "Todays Event",
+                                         startDate: formatDate(addHours: 1),
+                                         endDate: formatDate(addHours: 2))
+        let tomorrows = createCalendarEvent(course: course,
+                                            title: "Tomorrows Event",
+                                            startDate: formatDate(addDays: 1, addHours: 1),
+                                            endDate: formatDate(addDays: 1, addHours: 2))
+        let recurring = createCalendarEvent(course: course,
+                                            title: "Recurring Event",
+                                            allDay: true,
+                                            rRule: "FREQ=WEEKLY;COUNT=10")
+        let blackout = createCalendarEvent(course: course,
+                                           title: "Blackout Event",
+                                           startDate: formatDate(addDays: 2),
+                                           endDate: formatDate(addDays: 2),
+                                           allDay: true,
+                                           blackoutDate: true)
+        return SampleEvents(yesterdays: yesterdays,
+                            todays: todays,
+                            tomorrows: tomorrows,
+                            recurring: recurring,
+                            blackout: blackout)
+    }
+
     @discardableResult
     public static func createCalendarEvent(course: DSCourse,
                                            title: String = "Sample Calendar Event",
-                                           description: String = "Be there or be square!") -> DSCalendarEvent {
+                                           description: String = "Be there or be square!",
+                                           startDate: String? = nil,
+                                           endDate: String? = nil,
+                                           locationName: String = "Best Location",
+                                           locationAddress: String = "Right there under that old chestnut tree",
+                                           allDay: Bool? = nil,
+                                           rRule: String? = nil,
+                                           blackoutDate: Bool? = nil) -> DSCalendarEvent {
         let calendarEvent = CreateDSCalendarEventRequest.RequestedDSCalendarEvent(courseId: course.id,
                                                                                   title: title,
-                                                                                  description: description)
+                                                                                  description: description,
+                                                                                  start_at: startDate,
+                                                                                  end_at: endDate,
+                                                                                  location_name: locationName,
+                                                                                  location_address: locationAddress,
+                                                                                  all_day: allDay,
+                                                                                  rrule: rRule,
+                                                                                  blackout_date: blackoutDate)
         let requestBody = CreateDSCalendarEventRequest.Body(calendar_event: calendarEvent)
         return seeder.createCalendarEvent(requestBody: requestBody)
+    }
+
+    public struct SampleEvents {
+        let yesterdays: DSCalendarEvent
+        let todays: DSCalendarEvent
+        let tomorrows: DSCalendarEvent
+        let recurring: DSCalendarEvent
+        let blackout: DSCalendarEvent
     }
 }
--- Student/StudentE2ETests/Calendar/CalendarTests.swift
+++ Student/StudentE2ETests/Calendar/CalendarTests.swift
@@ -16,4 +16,51 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
-import Foundation
+import TestsFoundation
+
+class CalendarTests: E2ETestCase {
+    typealias Helper = CalendarHelper
+
+    func testCalendarLayout() {
+        // MARK: Seed the usual stuff with some calendar events
+        let student = seeder.createUser()
+        let course = seeder.createCourse()
+        seeder.enrollStudent(student, in: course)
+
+        let event = Helper.createCalendarEvent(course: course)
+
+        // MARK: Get the user logged in
+        logInDSUser(student)
+        let calendarTab = TabBar.calendarTab.waitToExist()
+        XCTAssertTrue(calendarTab.isVisible)
+
+        calendarTab.tap()
+
+        let navBar = Helper.navBar.waitToExist()
+        XCTAssertTrue(navBar.isVisible)
+
+        let todayButton = Helper.todayButton.waitToExist()
+        XCTAssertTrue(todayButton.isVisible)
+
+        let addNoteButton = Helper.addNoteButton.waitToExist()
+        XCTAssertTrue(addNoteButton.isVisible)
+
+        let todayDateButton = Helper.dayButton(date: Helper.formatDateForDayButton(date: event.start_at!)).waitToExist()
+        XCTAssertTrue(todayDateButton.isVisible)
+        XCTAssertTrue(todayDateButton.isSelected)
+
+        let eventItem = Helper.eventCell(event: event).waitToExist()
+        XCTAssertTrue(eventItem.isVisible)
+
+        let eventTitleLabel = Helper.titleLabelOfEvent(eventCell: eventItem).waitToExist()
+        XCTAssertTrue(eventTitleLabel.isVisible)
+        XCTAssertEqual(eventTitleLabel.label(), event.title)
+
+        let eventDateLabel = Helper.dateLabelOfEvent(eventCell: eventItem).waitToExist()
+        XCTAssertTrue(eventDateLabel.isVisible)
+
+        let eventCourseLabel = Helper.courseLabelOfEvent(eventCell: eventItem).waitToExist()
+        XCTAssertTrue(eventCourseLabel.isVisible)
+        XCTAssertEqual(eventCourseLabel.label(), course.name)
+    }
+}
