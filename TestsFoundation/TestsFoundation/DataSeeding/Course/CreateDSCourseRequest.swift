aaf83b88ab1f9996eb8d5bbafb896f8f860cd0c3
Andras Szasz <128469459+ndrsszsz@users.noreply.github.com>
2023-07-20 10:44:26 +0200

    Syllabus tests (#2516)
    * SyllabusTests
    
    * CalendarHelper
    
    * SyllabusTests
    refs: MBL-16897
    affects: Student
    release note: None
    test plan: Tests to pass

--- TestsFoundation/TestsFoundation/DataSeeding/Course/CreateDSCourseRequest.swift
+++ TestsFoundation/TestsFoundation/DataSeeding/Course/CreateDSCourseRequest.swift
@@ -35,6 +35,12 @@ struct CreateDSCourseRequest: APIRequestable {
 extension CreateDSCourseRequest {
     public struct RequestedDSCourse: Encodable {
         let name: String
+        let syllabus_body: String?
+
+        public init(name: String, syllabus_body: String? = nil) {
+            self.name = name
+            self.syllabus_body = syllabus_body
+        }
     }
 
     public struct Body: Encodable {
