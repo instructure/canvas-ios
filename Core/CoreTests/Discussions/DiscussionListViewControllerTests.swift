88e12d97c3176422068a8901bdba6e207792f39b
szabinst <szabolcs.fuzi@instructure.com>
2023-08-15 11:51:40 +0200

    test: DiscussionDetails test, refine list tests

--- Core/CoreTests/Discussions/DiscussionListViewControllerTests.swift
+++ Core/CoreTests/Discussions/DiscussionListViewControllerTests.swift
@@ -136,75 +136,55 @@ class DiscussionListViewControllerTests: CoreTestCase {
     }
 
     func testPointsLabelWhenQuantitativeDataEnabled() {
-        api.mock(
-            GetCourse(courseID: "1"),
-            value: .make(
-                settings: APICourseSettings(
-                    usage_rights_required: nil,
-                    syllabus_course_summary: nil,
-                    restrict_quantitative_data: true
-                )
-            )
-        )
+        // Given
+        mockCourseAndAssignmentWith(restrict_quantitative_data: true)
 
-        api.mock(controller.topics, value: [
-            .make(
-                assignment: .make(has_overrides: true, points_possible: 21),
-                assignment_id: "1",
-                html_url: URL(string: "/courses/1/discussion_topics/2"),
-                id: "2",
-                posted_at: DateComponents(calendar: .current, year: 2020, month: 11, day: 3).date,
-                title: "Overrides"
-            ),
-        ])
-
+        // When
         controller.view.layoutIfNeeded()
         controller.viewWillAppear(false)
 
+        // Then
         let cell = controller.tableView.cellForRow(at: IndexPath(row: 0, section: 0)) as? Core.DiscussionListCell
         XCTAssertEqual(cell?.pointsLabel.isHidden, true)
         XCTAssertEqual(cell?.pointsLabel.text, "21 pts")
     }
 
     func testPointsLabelWhenQuantitativeDataDisabled() {
-        api.mock(
-            GetCourse(courseID: "1"),
-            value: .make(
-                settings: APICourseSettings(
-                    usage_rights_required: nil,
-                    syllabus_course_summary: nil,
-                    restrict_quantitative_data: false
-                )
-            )
-        )
+        // Given
+        mockCourseAndAssignmentWith(restrict_quantitative_data: false)
 
-        api.mock(controller.topics, value: [
-            .make(
-                assignment: .make(has_overrides: true, points_possible: 21),
-                assignment_id: "1",
-                html_url: URL(string: "/courses/1/discussion_topics/2"),
-                id: "2",
-                posted_at: DateComponents(calendar: .current, year: 2020, month: 11, day: 3).date,
-                title: "Overrides"
-            ),
-        ])
-
+        // When
         controller.view.layoutIfNeeded()
         controller.viewWillAppear(false)
 
+        // Then
         let cell = controller.tableView.cellForRow(at: IndexPath(row: 0, section: 0)) as? Core.DiscussionListCell
         XCTAssertEqual(cell?.pointsLabel.isHidden, false)
         XCTAssertEqual(cell?.pointsLabel.text, "21 pts")
     }
 
     func testPointsLabelWhenQuantitativeDataNotSpecified() {
+        // Given
+        mockCourseAndAssignmentWith(restrict_quantitative_data: nil)
+
+        // When
+        controller.view.layoutIfNeeded()
+        controller.viewWillAppear(false)
+
+        // Then
+        let cell = controller.tableView.cellForRow(at: IndexPath(row: 0, section: 0)) as? Core.DiscussionListCell
+        XCTAssertEqual(cell?.pointsLabel.isHidden, false)
+        XCTAssertEqual(cell?.pointsLabel.text, "21 pts")
+    }
+
+    private func mockCourseAndAssignmentWith(restrict_quantitative_data: Bool?) {
         api.mock(
             GetCourse(courseID: "1"),
             value: .make(
                 settings: APICourseSettings(
                     usage_rights_required: nil,
                     syllabus_course_summary: nil,
-                    restrict_quantitative_data: nil
+                    restrict_quantitative_data: restrict_quantitative_data
                 )
             )
         )
@@ -219,13 +199,6 @@ class DiscussionListViewControllerTests: CoreTestCase {
                 title: "Overrides"
             ),
         ])
-
-        controller.view.layoutIfNeeded()
-        controller.viewWillAppear(false)
-
-        let cell = controller.tableView.cellForRow(at: IndexPath(row: 0, section: 0)) as? Core.DiscussionListCell
-        XCTAssertEqual(cell?.pointsLabel.isHidden, false)
-        XCTAssertEqual(cell?.pointsLabel.text, "21 pts")
     }
 
     func testGroupDiscussions() {
