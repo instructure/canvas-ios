018e76347a1da7878e9e95cfd5cf586fcf9e35a0
Attila Varga <attila.varga@instructure.com>
2023-08-24 12:21:16 +0200

    Add scheduler skeleton.

---
+++ Core/Core/BackgroundProcessing/BackgroundProcessingInteractor.swift
@@ -0,0 +1,39 @@
+//
+// This file is part of Canvas.
+// Copyright (C) 2023-present  Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import BackgroundTasks
+
+struct BackgroundProcessingInteractor {
+    private let scheduler: BGTaskSchedulerWrapper
+
+    public init(scheduler: BGTaskSchedulerWrapper) {
+        self.scheduler = scheduler
+    }
+}
+
+/**
+ Core is an extension API only project that forbids calling various methods on `BGTaskScheduler`
+ so we have to abstract it away and inject it from a higher level project.
+ */
+public protocol BGTaskSchedulerWrapper {
+
+    func register(forTaskWithIdentifier identifier: String,
+                  using queue: DispatchQueue?,
+                  launchHandler: @escaping (BGTask) -> Void) -> Bool
+    func cancel(taskRequestWithIdentifier identifier: String)
+}
