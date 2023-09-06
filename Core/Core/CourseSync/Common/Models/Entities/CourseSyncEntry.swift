f6902acff0f3a5c752826c96a5aaacbfafa7f005
szabinst <szabolcs.fuzi@instructure.com>
2023-09-06 13:04:29 +0200

    fix: review findings

--- Core/Core/CourseSync/Common/Models/Entities/CourseSyncEntry.swift
+++ Core/Core/CourseSync/Common/Models/Entities/CourseSyncEntry.swift
@@ -182,10 +182,18 @@ public struct CourseSyncEntry: Equatable {
             files.indices.forEach { files[$0].selectionState = selectionState }
         }
 
+        // Selecting modules or grades will select every other tab because they are needed to compose modules/grades. 
         if selectionState == .selected {
             if tabs[id: id]?.type == .modules || tabs[id: id]?.type == .grades {
                 for (index, tab) in tabs.enumerated() where tab.type != .modules && tab.type != .grades {
                     tabs[index].selectionState = .selected
+                }
+            }
+        // Deselecting a tab other than modules or grades will deselect modules and grades.
+        } else if selectionState == .deselected {
+            if tabs[id: id]?.type != .modules && tabs[id: id]?.type != .grades {
+                for (index, tab) in tabs.enumerated() where tab.type == .modules || tab.type == .grades {
+                    tabs[index].selectionState = .deselected
                 }
             }
         }
