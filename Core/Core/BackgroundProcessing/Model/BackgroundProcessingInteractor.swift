31f2f387590828db0909312c2956d563deb2d6bb
Attila Varga <72396990+vargaat@users.noreply.github.com>
2023-09-12 13:11:35 +0200

    Add background update service (#2574)
    [ignore-commit-lint]

---
+++ Core/Core/BackgroundProcessing/Model/BackgroundProcessingInteractor.swift
@@ -0,0 +1,64 @@
+//
+// This file is part of Canvas.
+// Copyright (C) 2023-present  Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import BackgroundTasks
+
+public struct BackgroundProcessingInteractor {
+    private let scheduler: CoreBGTaskScheduler
+
+    public init(scheduler: CoreBGTaskScheduler) {
+        self.scheduler = scheduler
+    }
+
+    public func register(taskID: String) {
+        let isRegistered = scheduler.register(forTaskWithIdentifier: taskID,
+                                              using: nil) { backgroundTask in
+            guard let task = BackgroundProcessingAssembly.resolveTask(for: backgroundTask.identifier) else {
+                Logger.shared.error("BackgroundProcessingInteractor: Background task ID \(taskID) couldn't be resolved to a task.")
+                backgroundTask.setTaskCompleted(success: true)
+                return
+            }
+
+            backgroundTask.expirationHandler = {
+                Logger.shared.error("BackgroundProcessingInteractor: Background task \(taskID) will be cancelled.")
+                task.cancel()
+                backgroundTask.setTaskCompleted(success: false)
+            }
+
+            task.start {
+                backgroundTask.setTaskCompleted(success: true)
+            }
+        }
+
+        if !isRegistered {
+            Logger.shared.error("BackgroundProcessingInteractor: Failed to register background task \(taskID).")
+        }
+    }
+
+    public func schedule(task: BGTaskRequest) {
+        do {
+            try scheduler.submit(task)
+        } catch(let error) {
+            Logger.shared.error("BackgroundProcessingInteractor: Error scheduling task \(task.identifier): \(error.localizedDescription)")
+        }
+    }
+
+    public func cancel(taskID: String) {
+        scheduler.cancel(taskRequestWithIdentifier: taskID)
+    }
+}
