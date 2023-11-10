1be7031aa8df9639906e61523a6d543fd44114de
szabinst <szabolcs.fuzi@instructure.com>
2023-11-10 10:43:37 +0100

    fix: tests

--- Core/CoreTests/CourseSync/CourseSyncSelector/Model/CourseSyncSelectorInteractorLiveTests.swift
+++ Core/CoreTests/CourseSync/CourseSyncSelector/Model/CourseSyncSelectorInteractorLiveTests.swift
@@ -32,6 +32,7 @@ class CourseSyncSelectorInteractorLiveTests: CoreTestCase {
 
     override func tearDown() {
         defaults.reset()
+        API.resetMocks()
         super.tearDown()
     }
 
@@ -51,6 +52,7 @@ class CourseSyncSelectorInteractorLiveTests: CoreTestCase {
                 }
             )
 
+        drainMainQueue()
         waitForExpectations(timeout: 0.1)
         XCTAssertEqual(entries.count, 1)
         subscription.cancel()
@@ -75,6 +77,7 @@ class CourseSyncSelectorInteractorLiveTests: CoreTestCase {
                 }
             )
 
+        drainMainQueue()
         waitForExpectations(timeout: 0.1)
         XCTAssertEqual(entries.count, 1)
         XCTAssertEqual(entries.first?.id, "courses/2")
@@ -84,6 +87,16 @@ class CourseSyncSelectorInteractorLiveTests: CoreTestCase {
     func testTabList() {
         let testee = CourseSyncSelectorInteractorLive(sessionDefaults: defaults)
         let expectation = expectation(description: "Publisher sends value")
+
+        let rootFolder = APIFolder.make(context_type: "Course", context_id: 1, files_count: 1, id: 0)
+        let rootFolderFile = APIFile.make(id: 0, folder_id: 0, display_name: "root-file-1")
+
+        let folder1 = APIFolder.make(id: 1, parent_folder_id: 0)
+        let folder1File = APIFile.make(id: 1, folder_id: 1, display_name: "folder-1-file")
+
+        mockRootFolders(folders: [rootFolder])
+        mockFolderItems(for: "0", folders: [folder1], files: [rootFolderFile])
+        mockFolderItems(for: "1", folders: [], files: [folder1File])
 
         mockCourseList(
             courseList: [
@@ -109,6 +122,7 @@ class CourseSyncSelectorInteractorLiveTests: CoreTestCase {
                 }
             )
 
+        drainMainQueue()
         waitForExpectations(timeout: 0.1)
         XCTAssertEqual(entries.count, 1)
         XCTAssertEqual(entries[0].tabs.count, 4)
@@ -143,6 +157,7 @@ class CourseSyncSelectorInteractorLiveTests: CoreTestCase {
                 }
             )
 
+        drainMainQueue()
         waitForExpectations(timeout: 0.1)
         XCTAssertEqual(entries.count, 1)
         XCTAssertEqual(entries[0].files.count, 2)
@@ -172,6 +187,7 @@ class CourseSyncSelectorInteractorLiveTests: CoreTestCase {
                 }
             )
 
+        drainMainQueue()
         waitForExpectations(timeout: 0.1)
         XCTAssertEqual(entries.count, 1)
         XCTAssertEqual(entries[0].tabs.count, 0)
@@ -300,6 +316,7 @@ class CourseSyncSelectorInteractorLiveTests: CoreTestCase {
                 }
             )
 
+        drainMainQueue()
         waitForExpectations(timeout: 0.1)
         XCTAssertEqual(selectedSize, 2048)
         subscription.cancel()
@@ -328,6 +345,7 @@ class CourseSyncSelectorInteractorLiveTests: CoreTestCase {
             .sink()
             .store(in: &subscriptions)
 
+        drainMainQueue()
         waitForExpectations(timeout: 0.1)
         subscriptions.removeAll()
     }
@@ -354,6 +372,7 @@ class CourseSyncSelectorInteractorLiveTests: CoreTestCase {
             .sink()
             .store(in: &subscriptions)
 
+        drainMainQueue()
         waitForExpectations(timeout: 0.1)
         subscriptions.removeAll()
     }
@@ -388,6 +407,7 @@ class CourseSyncSelectorInteractorLiveTests: CoreTestCase {
 
         // MARK: - THEN
 
+        drainMainQueue()
         XCTAssertCompletableSingleOutputEquals(selectedItemID, "courses/2")
         session.reset()
     }
@@ -410,6 +430,7 @@ class CourseSyncSelectorInteractorLiveTests: CoreTestCase {
 
         // MARK: - THEN
 
+        drainMainQueue()
         XCTAssertEqual(defaults.offlineSyncSelections, ["courses/2"])
     }
 
@@ -432,6 +453,7 @@ class CourseSyncSelectorInteractorLiveTests: CoreTestCase {
 
         // MARK: - THEN
 
+        drainMainQueue()
         XCTAssertEqual(defaults.offlineSyncSelections, ["courses/2", "courses/1"])
     }
 
