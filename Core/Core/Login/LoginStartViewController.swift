d5fcf1abfa691955d0d00032ff5bf4b6b8fea633
szabinst <szabolcs.fuzi@instructure.com>
2023-11-20 14:58:46 +0100

    Login screen logo color improvements
    refs: MBL-17163
    affects: Student, Teacher, Parent
    release note: Updated the login screen logo colors to improve accessibility
    test plan: See ticket

--- Core/Core/Login/LoginStartViewController.swift
+++ Core/Core/Login/LoginStartViewController.swift
@@ -85,7 +85,7 @@ class LoginStartViewController: UIViewController {
         }
         authenticationMethodLabel.isHidden = true
         logoView.tintColor = .currentLogoColor()
-        wordmark.tintColor = .currentLogoColor()
+        wordmark.tintColor = .ash
         animatableLogo.tintColor = logoView.tintColor
         previousLoginsView.isHidden = true
         self.lastLoginAccount = nil
@@ -98,7 +98,7 @@ class LoginStartViewController: UIViewController {
             : Bundle.main.isTeacherApp ? "TEACHER"
             : "STUDENT"
         ), attributes: [.kern: 2])
-        wordmarkLabel.textColor = .textDarkest
+        wordmarkLabel.textColor = .currentLogoColor()
         logoView.superview?.accessibilityLabel = "Canvas " + (wordmarkLabel.text ?? "")
         let loginText = NSLocalizedString("Log In", bundle: .core, comment: "")
         if MDMManager.shared.host != nil {
