b84e03c62dfc936572c9164e1e742cc24c600e11
szabinst <110813321+szabinst@users.noreply.github.com>
2024-01-09 17:18:12 +0100

    Account level group discussion (#2673)
    refs: MBL-16420
    affects: Student, Teacher
    release note: Added support for Redesigned Discussions
    test plan: Test account level group discussions

--- Core/Core/EmbeddedWebPage/ViewModel/EmbeddedWebPageViewModelLive.swift
+++ Core/Core/EmbeddedWebPage/ViewModel/EmbeddedWebPageViewModelLive.swift
@@ -35,15 +35,23 @@ public class EmbeddedWebPageViewModelLive: EmbeddedWebPageViewModel {
      This method assumes that feature flags for the given context are already in CoreData.
      */
     public static func isRedesignEnabled(in context: Context) -> Bool {
-        var featureFlagContext = context
-
         if context.contextType == .group {
             let group = AppEnvironment.shared.subscribe(GetGroup(groupID: context.id))
-            if let courseID = group.first?.courseID {
-                featureFlagContext = Context.course(courseID)
+            if let courseID = group.first?.courseID {   // If it's group inside a course
+                return AppEnvironment.shared.subscribe( // return the course's feature flag value
+                    GetEnabledFeatureFlags(context: .course(courseID))
+                ).isFeatureFlagEnabled(.discussionRedesign)
+            } else {
+                return AppEnvironment.shared.subscribe( // If it's not a course group, it must be an account level group
+                    GetEnvironmentFeatureFlags(context: .currentUser) // return the environment flag value
+                ).isFeatureEnabled(.react_discussions_post)
             }
+        } else {
+            return AppEnvironment.shared.subscribe(
+                GetEnabledFeatureFlags(context: context)
+            ).isFeatureFlagEnabled(.discussionRedesign)
         }
-        return AppEnvironment.shared.subscribe(GetEnabledFeatureFlags(context: featureFlagContext)).isFeatureFlagEnabled(.discussionRedesign)
+
     }
     @Published public private(set) var subTitle: String?
     @Published public private(set) var contextColor: UIColor?
