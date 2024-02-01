03ef3fd0b0928dbd2bee32e18a4e2455eab8cca4
szabinst <110813321+szabinst@users.noreply.github.com>
2024-02-01 13:47:56 +0100

    Disable Discussions tab in offline mode if discussionRedesign is enabled (#2711)

--- Core/Core/Courses/CourseDetails/ViewModel/CourseDetailsViewModel.swift
+++ Core/Core/Courses/CourseDetails/ViewModel/CourseDetailsViewModel.swift
@@ -64,6 +64,12 @@ public class CourseDetailsViewModel: ObservableObject {
         self?.updateTabs()
     }
 
+    // At this point we probably don't have feature flags saved in Core Data so we need to fetch them.
+    // If discussionRedesign is enabled and we are in offline mode, we need to disable the Discussions tab.
+    private lazy var featureFlags = env.subscribe(GetEnabledFeatureFlags(context: .course(courseID))) { [weak self] in
+        self?.updateTabs()
+    }
+
     private var subscriptions = Set<AnyCancellable>()
     private let offlineModeInteractor: OfflineModeInteractor
 
@@ -98,6 +104,7 @@ public class CourseDetailsViewModel: ObservableObject {
         permissions.refresh()
         course.refresh()
         colors.refresh()
+        featureFlags.refresh()
     }
 
     public func retryAfterError() {
@@ -144,13 +151,25 @@ public class CourseDetailsViewModel: ObservableObject {
     }
 
     private func updateCellOfflineSupport(on cells: [CourseDetailsCellViewModel]) {
-        guard let offlineSelectionsForCourse = env.userDefaults?.offlineSyncSelections else {
+        guard var offlineSelectionsForCourse = env.userDefaults?.offlineSyncSelections else {
             return
         }
+
+        if isDiscussionRedesignEnabled() {
+            offlineSelectionsForCourse.removeAll { selection in
+                selection.contains("courses/\(courseID)/tabs/\(TabName.discussions.rawValue)") ||
+                    selection.contains("courses/\(courseID)/tabs/\(TabName.announcements.rawValue)")
+            }
+        }
+
         let wholeCourseSelected = offlineSelectionsForCourse.contains("courses/\(courseID)")
 
         if wholeCourseSelected {
-            let offlineTabs = TabName.OfflineSyncableTabs.map { $0.rawValue }
+            var offlineTabs = TabName.OfflineSyncableTabs.map { $0.rawValue }
+
+            if isDiscussionRedesignEnabled() {
+                offlineTabs = offlineTabs.filter { $0 != TabName.discussions.rawValue && $0 != TabName.announcements.rawValue }
+            }
 
             cells.forEach {
                 $0.isSupportedOffline = offlineTabs.contains($0.tabID)
@@ -224,6 +243,10 @@ public class CourseDetailsViewModel: ObservableObject {
         updateCellSelectionStates(on: cellViewModels, selectedIndex: selectionViewModel.selectedIndex)
         updateCellOfflineSupport(on: cellViewModels)
         state = .data(cellViewModels)
+    }
+
+    private func isDiscussionRedesignEnabled() -> Bool {
+        EmbeddedWebPageViewModelLive.isRedesignEnabled(in: context)
     }
 
     // MARK: Attendance Tool
