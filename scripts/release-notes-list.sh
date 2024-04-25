7fc2850274c54f25055e46c5bcb29b7198721c06
Attila Varga <attila.varga@instructure.com>
2024-04-25 15:55:06 +0200

    Add script to list changes between a tag and master.
    [ignore-commit-lint]

---
+++ scripts/release-notes-list.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+	echo "This script lists commits between the version's tag and the master branch affecting the given app."
+    echo "Usage: $0 <Student|Teacher|Parent> <version>"
+    exit 1
+fi
+
+tag_name="$1-$2"
+
+if ! git rev-parse -q --verify "refs/tags/$tag_name" >/dev/null; then
+    echo "Tag '$tag_name' not found."
+    exit 1
+fi
+
+# -i to do a case insensitive search
+# --grep to list commits containing the affects string and the app's name in one line
+# --oneline to print commits in a short format
+git log -i --grep="affects:.*$1" --oneline master...$tag_name
