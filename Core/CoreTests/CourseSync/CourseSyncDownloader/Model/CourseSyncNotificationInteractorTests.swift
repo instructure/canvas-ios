e7758169abd7372f4ff7008d475bba88c03d4b74
szabinst <szabolcs.fuzi@instructure.com>
2024-05-23 17:10:16 +0200

    fix: tests

--- Core/CoreTests/CourseSync/CourseSyncDownloader/Model/CourseSyncNotificationInteractorTests.swift
+++ Core/CoreTests/CourseSync/CourseSyncDownloader/Model/CourseSyncNotificationInteractorTests.swift
@@ -64,7 +64,7 @@ class CourseSyncNotificationInteractorTests: CoreTestCase {
         }
 
         XCTAssertEqual(notification.content.title, "Offline Content Sync Failed")
-        XCTAssertEqual(notification.content.body, "One or more items failed to sync.")
+        XCTAssertEqual(notification.content.body, "One or more items failed to sync. Please check your internet connection and retry syncing.")
     }
 
     func testNotSendsFailedNotificationWhenSyncProgressIsOnScreen() {
