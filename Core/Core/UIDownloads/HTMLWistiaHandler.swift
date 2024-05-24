8314ea0139cbb712eb62269696d8709fcbeed50f
forgotvas <vadim@touchappmedia.com>
2024-05-24 14:02:23 +0300

    fix: transcription overlap

--- Core/Core/UIDownloads/HTMLWistiaHandler.swift
+++ Core/Core/UIDownloads/HTMLWistiaHandler.swift
@@ -19,24 +19,24 @@
 import SwiftSoup
 struct HTMLWistiaHandler {
     func updateWistia(in html: String) -> String? {
-        var result: String = html
         do {
             let document = try SwiftSoup.parse(html)
             let iframes = try document.getElementsByTag("iframe")
             for iframe in iframes {
                 let src = try iframe.attr("src")
-                if src.contains("wistia") {
+                if isWistiaLink(src) {
                     let styleValue = try? iframe.attr("style")
 
                     if let id = getWistiaId(from: src) {
                         var stringToReplace: String = ""
+                        let currentHTML = try document.html()
                         let v1Link = "https://fast.wistia.com/assets/external/E-v1.js"
-                        if !result.contains(v1Link) {
+                        if !currentHTML.contains(v1Link) {
                             stringToReplace += "<script src=\"\(v1Link)\" async></script>"
                         }
 
                         let transcriptLink: String = "https://fast.wistia.net/assets/external/transcript.js"
-                        if !result.contains(transcriptLink) {
+                        if !currentHTML.contains(transcriptLink) {
                             stringToReplace += "<script src=\"\(transcriptLink)\" async></script>"
                         }
 
@@ -44,21 +44,43 @@ struct HTMLWistiaHandler {
                         <script src="https://fast.wistia.com/embed/medias/\(id).jsonp" async></script>
                         <div class="wistia_embed wistia_async_\(id)"
                         style = \"width:100%; height:100%; \(styleValue ?? "")\">&nbsp;</div>
-                        <wistia-transcript media-id="\(id)" style="margin-top: 20px;height:200px;"></wistia-transcript>
                         """
-                        result = result.replacingOccurrences(of: try iframe.outerHtml(), with: stringToReplace)
+
+                        let wistiaTranscriptionTag = "<wistia-transcript media-id=\"\(id)\" style=\"margin-top: 20px;height:200px;\"></wistia-transcript>"
+                        if let parent = iframe.parent(),
+                            try parent.attr("class") == "wistia_responsive_wrapper",
+                            let parentOfParent = parent.parent(),
+                            try parentOfParent.attr("class") == "wistia_responsive_padding" {
+                            try parentOfParent.after(wistiaTranscriptionTag)
+                        } else {
+                            stringToReplace += wistiaTranscriptionTag
+                        }
+
+                        let div = try document.createElement("div")
+                        try div.attr("style", "width:100%;height:100%")
+                        try div.append(stringToReplace)
+                        try iframe.replaceWith(div)
                     }
                 }
             }
-            return result
+            let html = try document.html()
+            return html
         } catch {
 
         }
         return nil
     }
 
+    func isWistiaLink(_ link: String) -> Bool {
+        link.contains("wistia") &&
+        link.contains("iframe") &&
+        link.contains("embed")
+    }
+
     func getWistiaId(from link: String) -> String? {
-        if link.contains("wistia"), let url = URL(string: link) {
+        if isWistiaLink(link),
+            let url = URL(string: link) {
+
             let components = url.pathComponents
 
             var isNextIsId = false
