56dd665c28e32857b6028e053a1376c5570de0d1
Attila Varga <attila.varga@instructure.com>
2024-05-27 15:03:09 +0200

    Remove unused mock implementation. Rename file.

--- Core/TestsFoundation/Notifications/MockUserNotificationCenter.swift
+++ Core/TestsFoundation/Notifications/MockUserNotificationCenter.swift
@@ -20,15 +20,6 @@ import Foundation
 @testable import Core
 import UserNotifications
 
-public class MockNotificationManager: PushNotificationsInteractor {
-    public var mock: MockUserNotificationCenter
-
-    public init() {
-        mock = MockUserNotificationCenter()
-        super.init(notificationCenter: mock, logger: TestLogger())
-    }
-}
-
 public class MockUserNotificationCenter: UserNotificationCenterProtocol {
     public var requests: [UNNotificationRequest] = []
     public var error: Error?
