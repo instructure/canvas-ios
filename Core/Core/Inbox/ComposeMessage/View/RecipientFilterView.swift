f303ade5e78fd79e5879b82b3275fec67eb97292
Ahmed-Naguib93 <Ahmed.Naguib@instructure.com>
2024-08-26 12:02:55 +0300

    Add RecipientFilterView UI
    refs: MBL-17813
    affects: Student and teacher
    release note: none
    
    test plan:

---
+++ Core/Core/Inbox/ComposeMessage/View/RecipientFilterView.swift
@@ -0,0 +1,60 @@
+//
+// This file is part of Canvas.
+// Copyright (C) 2024-present  Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import SwiftUI
+
+struct RecipientFilterView: View {
+    // MARK: - Properties
+    let recipient: [Recipient]
+    var didSelectRecipient: ((Recipient) -> Void)
+
+    // MARK: - Body
+    var body: some View {
+        ScrollView {
+            VStack {
+                ForEach(recipient, id: \.self) { recipient in
+                    RecipientRow(recipient: recipient)
+                        .frame(height: 50)
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            didSelectRecipient(recipient)
+                        }
+                }
+            }
+            .background(Color.backgroundLightest)
+        }
+        .frame(maxHeight: 160)
+        .clipShape(RoundedRectangle(cornerRadius: 4))
+        .shadow(color: Color.textDark.opacity(0.2), radius: 5, x: 0, y: 0)
+        .padding(5)
+    }
+}
+
+#if DEBUG
+#Preview {
+    let imageUrl = URL(string: "https://png.pngtree.com/thumb_back/fh260/background/20230614/pngtree-cartoon-image-of-a-bearded-man-with-glasses-image_2876117.jpg")
+    return RecipientFilterView(recipient: [
+        .init(id: "1", name: "Canvas IOS 1", avatarURL: imageUrl),
+        .init(id: "2", name: "Canvas", avatarURL: imageUrl),
+        .init(id: "3", name: "Canvas Test ", avatarURL: imageUrl),
+        .init(id: "4", name: "Canvas 3", avatarURL: imageUrl),
+        .init(id: "5", name: "Canvas IOS16", avatarURL: imageUrl)
+    ]) { _ in }
+}
+
+#endif
---
+++ Core/Core/Inbox/ComposeMessage/View/RecipientRow.swift
@@ -0,0 +1,58 @@
+//
+// This file is part of Canvas.
+// Copyright (C) 2024-present  Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import SwiftUI
+
+struct RecipientRow: View {
+    // MARK: - Properties
+    let recipient: Recipient
+
+    // MARK: - Body
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            HStack(alignment: .center, spacing: 13) {
+                if let imageUrl = recipient.avatarURL {
+                    RemoteImage(imageUrl, width: 36, height: 36)
+                        .clipShape(Circle())
+                }
+                Text(recipient.displayName)
+                    .font(.regular16)
+                    .foregroundStyle(Color.textDark)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+
+            }
+            .background(Color.backgroundLightest)
+            .padding(.horizontal, 15)
+            .padding(.vertical, 7)
+            InstUI.Divider()
+        }
+    }
+}
+
+#if DEBUG
+#Preview {
+    RecipientRow(
+        recipient: .init(
+            id: "1",
+            name: "Canvas IOS",
+            avatarURL: URL(string: "https://png.pngtree.com/thumb_back/fh260/background/20230614/pngtree-cartoon-image-of-a-bearded-man-with-glasses-image_2876117.jpg")
+        )
+    )
+}
+
+#endif
