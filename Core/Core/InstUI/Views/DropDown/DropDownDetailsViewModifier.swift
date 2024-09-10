94128e89dca7c22ebefdfae0ef3c79465dd3ea69
Suhaib Al-Absi <suhaib.alabsi@instructure.com>
2024-09-10 13:38:33 +0300

    Accessibility improvements

--- Core/Core/InstUI/Views/DropDown/DropDownDetailsViewModifier.swift
+++ Core/Core/InstUI/Views/DropDown/DropDownDetailsViewModifier.swift
@@ -32,6 +32,7 @@ struct DropDownDetailsContainerViewModifier<ListContent: View>: ViewModifier {
 
     func body(content: Content) -> some View {
         content
+            .accessibilityHidden(state.isDetailsShown)
             .allowsHitTesting(state.isDetailsShown == false)
             .background(content: {
                 GeometryReader(content: { geometry in
