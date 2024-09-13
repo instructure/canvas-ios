0af6eec7b3596044055f2b176d661dae266fb588
szabinst <szabolcs.fuzi@instructure.com>
2024-09-13 11:46:00 +0200

    feat: dashboard layout

--- Horizon/Horizon/Sources/Dashboard/View/DashboardView.swift
+++ Horizon/Horizon/Sources/Dashboard/View/DashboardView.swift
@@ -30,9 +30,153 @@ struct DashboardView: View {
         InstUI.BaseScreen(
             state: viewModel.state,
             config: .init(refreshable: false)
-        ) { _ in
-            Text(viewModel.title)
+        ) { proxy in
+            VStack(alignment: .leading, spacing: 0) {
+                Text(viewModel.title)
+                    .font(.bold28)
+                    .foregroundColor(.textDarkest)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.top, 16)
+                Text("BIOLOGY CERTIFICATE #17491")
+                    .font(.regular12)
+                    .foregroundColor(.textDark)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.top, 24)
+                progressBar(proxy: proxy)
+                currentModuleView
+                whatsNextModuleView(proxy: proxy)
+            }
+            .frame(maxWidth: .infinity)
         }
+        .padding(.horizontal, 16)
+        .background(Color.backgroundLightest)
+    }
+
+    @ViewBuilder
+    private func progressBar(proxy: GeometryProxy) -> some View {
+        Rectangle()
+            .fill(Color.backgroundMedium)
+            .frame(width: proxy.size.width, height: 25)
+            .overlay(alignment: .leading) {
+                Rectangle()
+                    .fill(Color.backgroundDarkest)
+                    .frame(
+                        width: viewModel.progression * proxy.size.width,
+                        height: 25
+                    )
+                    .overlay(alignment: .trailing) {
+                        Text(viewModel.progressionString)
+                            .font(.regular12)
+                            .foregroundStyle(Color.textLightest)
+                            .padding(.trailing, 6)
+                    }
+            }
+            .padding(.top, 16)
+    }
+
+    private var currentModuleView: some View {
+        ZStack {
+            VStack {
+                Rectangle()
+                    .fill(Color.backgroundLightest)
+                    .frame(height: 200)
+                    .padding(16)
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Short form text")
+                            .font(.bold16)
+                            .foregroundStyle(Color.textDarkest)
+                        Text("20 MINS")
+                            .font(.regular12)
+                            .foregroundStyle(Color.textDark)
+                    }
+                    Spacer()
+                    Button {
+                        print("tapped")
+                    } label: {
+                        Text("Start")
+                            .font(.regular16)
+                            .padding(.all, 8)
+                            .background(Color.backgroundDarkest)
+                            .foregroundColor(Color.textLightest)
+                            .cornerRadius(3)
+                    }
+                }
+                .padding(.horizontal, 16)
+                .padding(.bottom, 16)
+            }
+        }
+        .background(Color.backgroundLight)
+        .padding(.top, 16)
+    }
+
+    @ViewBuilder
+    private func whatsNextModuleView(proxy: GeometryProxy) -> some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text("WHAT'S NEXT")
+                .font(.regular12)
+                .foregroundColor(.textDark)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHStack(spacing: 8) {
+                    VStack(alignment: .leading, spacing: 4) {
+                        Image(systemName: "doc")
+                            .frame(width: 18, height: 18)
+                        Text("Practice Quiz")
+                            .font(.regular16)
+                            .foregroundStyle(Color.textDarkest)
+                            .padding(.top, 4)
+                        Text("60 MINS")
+                            .font(.regular12)
+                            .foregroundStyle(Color.textDark)
+                        Text("BIOLOGY CERTIFICATE")
+                            .font(.regular12)
+                            .foregroundStyle(Color.textDarkest)
+                    }
+                    .padding(.all, 8)
+                    .frame(width: (proxy.size.width - 16 - 8) / 2 - 4)
+                    .frame(minHeight: 138, maxHeight: 138)
+                    .background(Color.backgroundLight)
+                    VStack(alignment: .leading, spacing: 4) {
+                        Image(systemName: "doc")
+                            .frame(width: 18, height: 18)
+                        Text("Video")
+                            .font(.regular16)
+                            .foregroundStyle(Color.textDarkest)
+                            .padding(.top, 4)
+                        Text("33 MINS")
+                            .font(.regular12)
+                            .foregroundStyle(Color.textDark)
+                        Text("HISTORY CERTIFICATE")
+                            .font(.regular12)
+                            .foregroundStyle(Color.textDarkest)
+                    }
+                    .padding(.all, 8)
+                    .frame(width: (proxy.size.width - 16 - 8) / 2 - 4)
+                    .frame(minHeight: 138, maxHeight: 138)
+                    .background(Color.backgroundLight)
+                    VStack(alignment: .leading, spacing: 4) {
+                        Image(systemName: "doc")
+                            .frame(width: 18, height: 18)
+                        Text("Video")
+                            .font(.regular16)
+                            .foregroundStyle(Color.textDarkest)
+                            .padding(.top, 4)
+                        Text("33 MINS")
+                            .font(.regular12)
+                            .foregroundStyle(Color.textDark)
+                        Text("HISTORY CERTIFICATE")
+                            .font(.regular12)
+                            .foregroundStyle(Color.textDarkest)
+                    }
+                    .padding(.all, 8)
+                    .frame(width: (proxy.size.width - 16 - 8) / 2 - 4)
+                    .frame(minHeight: 138, maxHeight: 138)
+                    .background(Color.backgroundLight)
+                }
+            }
+        }
+        .padding(.top, 16)
     }
 }
 
