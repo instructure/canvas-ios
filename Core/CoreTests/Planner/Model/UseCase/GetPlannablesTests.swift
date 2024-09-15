0abe5d0ee341b221210024383abaa3b9db531285
Suhaib Al-Absi <suhaib.alabsi@instructure.com>
2024-09-15 16:52:15 +0300

    Fixing unit tests

--- Core/CoreTests/Planner/Model/UseCase/GetPlannablesTests.swift
+++ Core/CoreTests/Planner/Model/UseCase/GetPlannablesTests.swift
@@ -20,11 +20,17 @@ import XCTest
 @testable import Core
 
 class GetPlannablesTests: CoreTestCase {
+
     var start = Clock.now
     var end = Clock.now.addDays(1)
     var userID: String?
     var contextCodes: [String]?
-    lazy var useCase = GetPlannables(userID: userID, startDate: start, endDate: end, contextCodes: contextCodes)
+
+    func makeUseCase() -> GetPlannables {
+        GetPlannables(userID: userID, startDate: start, endDate: end, contextCodes: contextCodes)
+    }
+
+    lazy var useCase: GetPlannables = makeUseCase()
 
     func testCacheKey() {
         XCTAssertEqual(useCase.cacheKey, "get-plannables--\(start)-\(end)--")
@@ -135,8 +141,83 @@ class GetPlannablesTests: CoreTestCase {
         wait(for: [expectation], timeout: 1)
     }
 
+    func testMakeRequestTeacherApp() {
+        // Given - Teacher
+        environment.app = .teacher
+        contextCodes = ["course_1"]
+
+        /// Courses
+        api.mock(GetCoursesRequest(
+            enrollmentState: .active,
+            enrollmentType: .observer,
+            state: [.available],
+            perPage: 100
+        ), value: [.make(id: "1", enrollments: [.make(id: "1", associated_user_id: "1")])])
+
+        /// Calendar Events
+        api.mock(GetCalendarEventsRequest(
+            contexts: [Context(.course, id: "1")],
+            startDate: start,
+            endDate: end,
+            type: .event,
+            include: [.submission],
+            allEvents: false,
+            userID: userID
+        ), value: [.make(id: "1", type: .event)])
+
+        api.mock(GetCalendarEventsRequest(
+            contexts: [Context(.course, id: "1")],
+            startDate: start,
+            endDate: end,
+            type: .assignment,
+            include: [.submission],
+            allEvents: false,
+            userID: userID
+        ), value: [.make(id: "2", type: .assignment)])
+
+        /// Planner Notes
+        api.mock(GetPlannerNotesRequest(
+            contexts: [Context(.course, id: "1")],
+            startDate: start,
+            endDate: end
+        ), value: [.make(id: "33", title: "Planner Note", details: "Details")])
+
+        let expectation = XCTestExpectation(description: "callback")
+        useCase.makeRequest(environment: environment) { response, _, _ in
+            XCTAssertEqual(response?.calendarEvents?.count, 2)
+            XCTAssertEqual(response?.plannerNotes?.count, 1)
+
+            XCTAssertEqual(response?.calendarEvents?[0].type, .event)
+            XCTAssertEqual(response?.calendarEvents?[1].type, .assignment)
+
+            XCTAssertEqual(response?.plannerNotes?[0].id, "33")
+            XCTAssertEqual(response?.plannerNotes?[0].title, "Planner Note")
+            XCTAssertEqual(response?.plannerNotes?[0].details, "Details")
+
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
+
+        // Given - Parent
+        environment.app = .parent
+        contextCodes = ["course_1"]
+
+        // Then
+        let expectation2 = XCTestExpectation(description: "parent-callback")
+        useCase.makeRequest(environment: environment) { response, _, _ in
+            XCTAssertEqual(response?.calendarEvents?.count, 2)
+            XCTAssertEqual(response?.plannerNotes?.count, 0)
+
+            XCTAssertEqual(response?.calendarEvents?[0].type, .event)
+            XCTAssertEqual(response?.calendarEvents?[1].type, .assignment)
+
+            expectation2.fulfill()
+        }
+        wait(for: [expectation2], timeout: 1)
+    }
+
     func testWriteCalendarEvents() {
-        let response = GetPlannables.Response(plannables: nil, calendarEvents: [
+        let response = GetPlannables.Response.make(calendarEvents: [
             .make(id: "1", start_at: start, hidden: false),
             .make(id: "2", start_at: start, hidden: true)
         ])
@@ -144,5 +225,19 @@ class GetPlannablesTests: CoreTestCase {
         let plannables = databaseClient.fetch(scope: useCase.scope) as [Plannable]
         XCTAssertEqual(plannables.count, 1)
         XCTAssertEqual(plannables.first?.id, "1")
+    }
+
+    func testWritePlannerNotes() {
+        let response = GetPlannables.Response.make(plannerNotes: [
+            .make(id: "44", todo_date: start),
+            .make(id: "76", todo_date: start.addHours(3))
+        ])
+
+        useCase.write(response: response, urlResponse: nil, to: databaseClient)
+        let plannables = databaseClient.fetch(scope: useCase.scope) as [Plannable]
+
+        XCTAssertEqual(plannables.count, 2)
+        XCTAssertEqual(plannables[safeIndex: 0]?.id, "44")
+        XCTAssertEqual(plannables[safeIndex: 1]?.id, "76")
     }
 }
