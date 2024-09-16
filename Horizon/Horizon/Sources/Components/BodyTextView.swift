23a373a394e659739b287aeb2413c46351817da9
szabinst <szabolcs.fuzi@instructure.com>
2024-09-16 10:05:14 +0200

    feat: body text component

---
+++ Horizon/Horizon/Sources/Components/BodyTextView.swift
@@ -0,0 +1,34 @@
+//
+// This file is part of Canvas.
+// Copyright (C) 2024-present  Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import Core
+import SwiftUI
+
+struct BodyTextView: View {
+    let title: String
+
+    var body: some View {
+        Text(title)
+            .font(.regular16)
+            .foregroundStyle(Color.textDarkest)
+    }
+}
+
+#Preview {
+    BodyTextView(title: "Biology certificate #17531")
+}
