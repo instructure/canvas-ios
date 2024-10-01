dd1b1445c7a6a9a97b8c8222626932614e3c4918
Richard Harangozo <richard.harangozo@instructure.com>
2024-10-01 14:36:11 +0200

    Fix wrong CalendarEventDetails screen showing in Parent app
    refs: MBL-17946
    affects: Parent
    release note: Fixed issues on calendar event screen.
    
    test plan: See ticket

--- Core/Core/Planner/CalendarMain/PlannerListViewController.swift
+++ Core/Core/Planner/CalendarMain/PlannerListViewController.swift
@@ -133,26 +133,35 @@ extension PlannerListViewController: UITableViewDataSource, UITableViewDelegate 
 
         selectedPlannableId = plannable.id
 
-        switch plannable.plannableType {
-        case .planner_note:
-            let vc = PlannerAssembly.makeToDoDetailsViewController(plannable: plannable)
-            env.router.show(vc, from: self, options: .detail)
-        case .calendar_event:
-            let vc = PlannerAssembly.makeEventDetailsViewController(eventId: plannable.id) { [delegate] output in
-                switch output {
-                case .didUpdate, .didDelete:
-                    delegate?.plannerListWillRefresh()
-                case .didCancel:
-                    break
+        switch env.app {
+        case .student, .teacher:
+            switch plannable.plannableType {
+            case .planner_note:
+                let vc = PlannerAssembly.makeToDoDetailsViewController(plannable: plannable)
+                env.router.show(vc, from: self, options: .detail)
+            case .calendar_event:
+                let vc = PlannerAssembly.makeEventDetailsViewController(eventId: plannable.id) { [delegate] output in
+                    switch output {
+                    case .didUpdate, .didDelete:
+                        delegate?.plannerListWillRefresh()
+                    case .didCancel:
+                        break
+                    }
                 }
+                env.router.show(vc, from: self, options: .detail)
+            default:
+                routeToPlannableDetailsAtUrl(plannable.htmlURL)
             }
-            env.router.show(vc, from: self, options: .detail)
-        default:
-            if let url = plannable.htmlURL {
-                let to = url.appendingQueryItems(URLQueryItem(name: "origin", value: "calendar"))
-                env.router.route(to: to, from: self, options: .detail)
-            }
+        case .parent, .none:
+            routeToPlannableDetailsAtUrl(plannable.htmlURL)
         }
+    }
+
+    private func routeToPlannableDetailsAtUrl(_ url: URL?) {
+        guard let url else { return }
+
+        let to = url.appendingQueryItems(URLQueryItem(name: "origin", value: "calendar"))
+        env.router.route(to: to, from: self, options: .detail)
     }
 
     public func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
