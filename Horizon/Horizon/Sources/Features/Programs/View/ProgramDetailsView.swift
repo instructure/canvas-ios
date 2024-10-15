302043e057f6da786ca3c4a962a59543f27d8a53
szabinst <szabolcs.fuzi@instructure.com>
2024-10-15 14:50:32 +0200

    feat: disable drag inside course selector scrollview, cleanup

--- Horizon/Horizon/Sources/Features/Programs/View/ProgramDetailsView.swift
+++ Horizon/Horizon/Sources/Features/Programs/View/ProgramDetailsView.swift
@@ -92,12 +92,21 @@ struct ProgramDetailsViewView: View {
                     .id(index)
                 }
             }
+            .scrollTargetLayout()
+            .scrollTargetBehavior(.viewAligned)
         }
-        .scrollTargetLayout()
-        .scrollTargetBehavior(.viewAligned)
+        .simultaneousGesture(DragGesture(minimumDistance: 0), including: .all)
         .scrollBounceBehavior(.basedOnSize)
         .scrollPosition(id: $selectedCourseIndex, anchor: .center)
         .animation(.smooth, value: selectedCourseIndex)
+        // Re-enable if ScrollView drag gesture is needed.
+        /*
+        .onChange(of: selectedCourseIndex) {
+            if let selectedCourseIndex, selectedCourseIndex != selectedCourseDetailsIndex {
+                selectedCourseDetailsIndex = selectedCourseIndex
+            }
+        }
+        */
     }
 
     @ViewBuilder
@@ -123,33 +132,7 @@ struct ProgramDetailsViewView: View {
         .tabViewStyle(.page(indexDisplayMode: .never))
         .onChange(of: selectedCourseDetailsIndex) {
             selectedCourseIndex = selectedCourseDetailsIndex
-            print("🟪 ", selectedCourseDetailsIndex)
         }
         .animation(.smooth, value: selectedCourseDetailsIndex)
-    }
-}
-
-// MARK: Extensions
-struct SizePreferenceKey: PreferenceKey {
-    static var defaultValue: CGSize = .zero
-    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
-        value = nextValue()
-    }
-}
-
-extension View {
-    func getSizeOfView(_ getSize: @escaping ((CGSize) -> Void)) -> some View {
-        return self
-            .background {
-                GeometryReader { geometry in
-                    Color.clear.preference(
-                        key: SizePreferenceKey.self,
-                        value: geometry.size
-                    )
-                    .onPreferenceChange(SizePreferenceKey.self) { value in
-                        getSize(value)
-                    }
-                }
-            }
     }
 }
