7298ae7638af56687aeeaedf88a3dc08c3a6370a
Andras Szasz <andras.szasz@instructure.com>
2024-11-20 18:03:36 +0100

    fix unit tests

--- Core/CoreTests/Assignments/AssignmentList/ViewModel/AssignmentListPreferencesViewModelTests.swift
+++ Core/CoreTests/Assignments/AssignmentList/ViewModel/AssignmentListPreferencesViewModelTests.swift
@@ -49,12 +49,12 @@ final class AssignmentListPreferencesViewModelTests: CoreTestCase {
 
     func testInitialState() {
         XCTAssertEqual(testee.courseName, "Test Course")
-        XCTAssertEqual(testee.gradingPeriods, gradingPeriods)
+        XCTAssertEqual(testee.gradingPeriods.filter { $0.gradingPeriod != nil }.map { $0.gradingPeriod }, gradingPeriods)
         XCTAssertEqual(testee.sortingOptions, AssignmentListViewModel.AssignmentArrangementOptions.allCases)
         XCTAssertTrue(testee.isFilterSectionVisible)
         XCTAssertTrue(testee.isGradingPeriodsSectionVisible)
         XCTAssertEqual(testee.selectedSortingOption, AssignmentListViewModel.AssignmentArrangementOptions.dueDate)
-        XCTAssertEqual(testee.selectedGradingPeriod, nil)
+        XCTAssertEqual(testee.selectedGradingPeriod!.gradingPeriod, nil)
         XCTAssertEqual(testee.selectedAssignmentFilterOptions, AssignmentFilterOption.allCases)
     }
 
