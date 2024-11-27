fa740e293d3c047c6cf42de7f1235d92753714e5
Suhaib Al-Absi <suhaib.alabsi@instructure.com>
2024-11-28 00:05:51 +0300

    Update AssignmentDetailsPresenter.swift

--- Student/Student/Assignments/AssignmentDetails/AssignmentDetailsPresenter.swift
+++ Student/Student/Assignments/AssignmentDetails/AssignmentDetailsPresenter.swift
@@ -139,7 +139,7 @@ class AssignmentDetailsPresenter {
         self.fragment = fragment
         self.submissionButtonPresenter = SubmissionButtonPresenter(env: env, view: view, assignmentID: assignmentID)
         if let session = env.currentSession {
-            self.userID = session.userID
+            self.userID = session.userID.localID
         }
         subscribeToSuccessfulSubmissionNotification(assignmentID: assignmentID)
     }
