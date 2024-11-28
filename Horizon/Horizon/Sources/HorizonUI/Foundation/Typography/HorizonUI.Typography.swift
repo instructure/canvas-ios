6f73b28ae612869ca8af5dcfb66ee79363bb2aff
szabinst <szabolcs.fuzi@instructure.com>
2024-11-28 11:48:57 +0100

    wip foundations
    [ignore-commit-lint]

---
+++ Horizon/Horizon/Sources/HorizonUI/Foundation/Typography/HorizonUI.Typography.swift
@@ -0,0 +1,58 @@
+//
+// This file is part of Canvas.
+// Copyright (C) 2024-present  Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import SwiftUI
+
+extension HorizonUI {
+    struct Typography: View {
+        enum Name {
+            case h1
+            case h2
+            case h3
+
+            var font: Font {
+                switch self {
+                case .h1: return HorizonUI.fonts.regular10
+                case .h2: return HorizonUI.fonts.regular10
+                case .h3: return HorizonUI.fonts.regular10
+                }
+            }
+        }
+
+        private let text: String
+        private let name: Typography.Name
+        private let color: Color
+
+        init(
+            text: String,
+            name: Typography.Name,
+            color: Color = HorizonUI.colors.primitives.blue57
+        ) {
+            self.text = text
+            self.name = name
+            self.color = color
+        }
+
+        var body: some View {
+            Text(text)
+                .font(name.font)
+                .padding(.huiSpaces.primitives.xSmall)
+                .foregroundStyle(color)
+        }
+    }
+}
