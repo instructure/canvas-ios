ab088860aef8d36b916d17114c9be11e7054338d
Ahmed-Naguib93 <Ahmed.Naguib@instructure.com>
2024-12-03 22:27:12 +0200

    Move text submission to core module
    [ignore-commit-lint]

---
+++ Core/Core/TextSubmission/TextSubmissionViewController.swift
@@ -0,0 +1,133 @@
+//
+// This file is part of Canvas.
+// Copyright (C) 2019-present  Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import UIKit
+
+public final class TextSubmissionViewController: UIViewController, ErrorViewController, RichContentEditorDelegate {
+    // MARK: - Outlets
+
+    @IBOutlet private weak var contentView: UIView!
+    @IBOutlet private weak var keyboardSpace: NSLayoutConstraint!
+
+    // MARK: - Properties
+
+    private var assignmentID: String!
+    private var courseID: String!
+    private var editor: RichContentEditorViewController!
+    private let env = AppEnvironment.shared
+    private var keyboard: KeyboardTransitioning?
+    private var userID: String!
+    private var htmlContent: String?
+    private let isHorizonApp = AppEnvironment.shared.app == .horizon
+
+    // MARK: - Public Properties
+    public var didSentHtmlContent: ((String?) -> Void) = { _ in}
+
+    public static func create(courseID: String, assignmentID: String, userID: String) -> TextSubmissionViewController {
+        let controller = loadFromStoryboard()
+        controller.assignmentID = assignmentID
+        controller.courseID = courseID
+        controller.userID = userID
+        controller.editor = RichContentEditorViewController.create(context: .course(courseID), uploadTo: .myFiles)
+        return controller
+    }
+
+    public static func create(htmlContent: String? = nil, courseID: String) -> TextSubmissionViewController {
+        let controller = loadFromStoryboard()
+        controller.editor = RichContentEditorViewController.create(context: .course(courseID), uploadTo: .myFiles)
+        controller.htmlContent = htmlContent
+        return controller
+    }
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .backgroundLightest
+        title = String(localized: "Text Entry", bundle: .core)
+
+        navigationController?.navigationBar.useModalStyle()
+        if isHorizonApp {
+            navigationItem.rightBarButtonItem = UIBarButtonItem(
+                title: String(localized: "Done", bundle: .core),
+                style: .plain,
+                target: self,
+                action: #selector(sendHTMLContent)
+            )
+        } else {
+            navigationItem.rightBarButtonItem = UIBarButtonItem(
+                title: String(localized: "Submit", bundle: .core),
+                style: .plain,
+                target: self,
+                action: #selector(submit)
+            )
+        }
+        navigationItem.rightBarButtonItem?.accessibilityIdentifier = "TextSubmission.submitButton"
+        navigationItem.rightBarButtonItem?.isEnabled = false
+        addCancelButton(side: .left)
+
+        editor.delegate = self
+        editor.placeholder = String(localized: "Write...", bundle: .core, comment: "Text submission editor placeholder")
+        editor.a11yLabel = String(localized: "Submission text", bundle: .core, comment: "Text submission editor accessibility label")
+        editor.webView.scrollView.layer.masksToBounds = false
+        embed(editor, in: contentView)
+
+        if let htmlContent, isHorizonApp {
+            editor.setHTML(htmlContent)
+        }
+    }
+
+    public override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        keyboard = KeyboardTransitioning(view: view, space: keyboardSpace)
+    }
+
+    public func rce(_ editor: RichContentEditorViewController, canSubmit: Bool) {
+        navigationItem.rightBarButtonItem?.isEnabled = canSubmit
+    }
+
+    public func rce(_ editor: RichContentEditorViewController, didError error: Error) {
+        showError(error)
+    }
+
+    @objc private func sendHTMLContent() {
+        editor.getHTML { [weak self] html in
+            guard let self else {
+                return
+            }
+            didSentHtmlContent(html)
+            env.router.dismiss(self)
+        }
+    }
+
+    @objc private func submit() {
+        editor.getHTML { (html: String) in
+            CreateSubmission(
+                context: .course(self.courseID),
+                assignmentID: self.assignmentID,
+                userID: self.userID,
+                submissionType: .online_text_entry,
+                body: html
+            ).fetch { (_, _, error) in performUIUpdate {
+                if let error = error {
+                    self.showError(error)
+                } else {
+                    self.dismiss(animated: true, completion: nil)
+                }
+            } }
+        }
+    }
+}
