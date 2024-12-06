14355ef066ac0ff905bc1d4639a56d06291ecc6f
Ahmed-Naguib93 <Ahmed.Naguib@instructure.com>
2024-12-06 17:51:46 +0200

    Add UI Primitives & Additional Primitives Colors
    [ignore-commit-lint]

--- packages/HorizonUI/Sources/HorizonUI/Sources/Foundation/Colors/HorizonUI.Colors.Primitives.swift
+++ packages/HorizonUI/Sources/HorizonUI/Sources/Foundation/Colors/HorizonUI.Colors.Primitives.swift
@@ -20,20 +20,240 @@ import SwiftUI
 
 public extension HorizonUI.Colors {
     struct Primitives: Sendable {
-        // TODO: Add other variants
-        let blue12 = Color(hexString: "#E0EBF5")
-        let blue45 = Color(hexString: "#2B7ABC")
-        let blue57 = Color(hexString: "#0E68B3")
+
+        // MARK: - Blue
+
+        let blue12 = Color(.blue12)
+        let blue45 = Color(.blue45)
+        let blue57 = Color(.blue57)
+        let blue70 = Color(.blue70)
+        let blue82 = Color(.blue82)
+        let blueGradient = [Color(.blue82), Color(.green70)]
+
+        // MARK: - Green
+
+        let green12 = Color(.green12)
+        let green45 = Color(.green45)
+        let green57 = Color(.green57)
+        let green70 = Color(.green70)
+        let green82 = Color(.green82)
+
+        // MARK: - Orange
+
+        let orange12 = Color(.orange12)
+        let orange30 = Color(.orange30)
+        let orange45 = Color(.orange45)
+        let orange57 = Color(.orange57)
+        let orange70 = Color(.orange70)
+        let orange82 = Color(.orange82)
+
+        // MARK: - Red
+
+        let red12 = Color(.red12)
+        let red45 = Color(.red45)
+        let red57 = Color(.red57)
+        let red70 = Color(.red70)
+        let red82 = Color(.red82)
+
+        // MARK: - White
+
+        let white10 = Color(.white10)
+
+        // MARK: - Grey
+
+        let grey11 = Color(.grey11)
+        let grey12 = Color(.grey12)
+        let grey14 = Color(.grey14)
+        let grey24 = Color(.grey24)
+        let grey45 = Color(.grey45)
+        let grey57 = Color(.grey57)
+        let grey70 = Color(.grey70)
+        let grey82 = Color(.grey82)
+        let grey100 = Color(.grey100)
+        let grey125 = Color(.grey125)
+
+        // MARK: - Black
+
+        let black174 = Color(.black174)
+
+        // MARK: - Beige
+
+        let beige10 = Color(.beige10)
+        let beige11 = Color(.beige11)
+        let beige12 = Color(.beige12)
+        let beige15 = Color(.beige15)
+        let beige19 = Color(.beige19)
+        let beige26 = Color(.beige26)
+        let beige35 = Color(.beige35)
+        let beige49 = Color(.beige49)
+        let beige69 = Color(.beige69)
+        let beige100 = Color(.beige100)
+        let beige147 = Color(.beige147)
+        let beigeGradient = [Color(.beige10), Color(.beige11)]
+
+        // MARK: - Additional Primitives
+        // MARK: - Rose
+
+        let rose30 = Color(.rose30)
+        let rose35 = Color(.rose35)
+        let rose40 = Color(.rose40)
+        let rose45 = Color(.rose45)
+        let rose50 = Color(.rose50)
+        let rose57 = Color(.rose57)
+        let rose70 = Color(.rose70)
+        let rose90 = Color(.rose90)
+        let rose110 = Color(.rose110)
+
+        // MARK: - Copper
+
+        let copper30 = Color(.copper30)
+        let copper35 = Color(.copper35)
+        let copper40 = Color(.copper40)
+        let copper45 = Color(.copper45)
+        let copper50 = Color(.copper50)
+        let copper57 = Color(.copper57)
+        let copper70 = Color(.copper70)
+        let copper90 = Color(.copper90)
+        let copper110 = Color(.copper110)
+
+        // MARK: - Honey
+
+        let honey30 = Color(.honey30)
+        let honey35 = Color(.honey35)
+        let honey40 = Color(.honey40)
+        let honey45 = Color(.honey45)
+        let honey50 = Color(.honey50)
+        let honey57 = Color(.honey57)
+        let honey70 = Color(.honey70)
+        let honey90 = Color(.honey90)
+        let honey110 = Color(.honey110)
+
+        // MARK: - Forest
+
+        let forest30 = Color(.forest30)
+        let forest35 = Color(.forest35)
+        let forest40 = Color(.forest40)
+        let forest45 = Color(.forest45)
+        let forest50 = Color(.forest50)
+        let forest57 = Color(.forest57)
+        let forest70 = Color(.forest70)
+        let forest90 = Color(.forest90)
+        let forest110 = Color(.forest110)
+
+        // MARK: - Aurora
+
+        let aurora30 = Color(.aurora30)
+        let aurora35 = Color(.aurora35)
+        let aurora40 = Color(.aurora40)
+        let aurora45 = Color(.aurora45)
+        let aurora50 = Color(.aurora50)
+        let aurora57 = Color(.aurora57)
+        let aurora70 = Color(.aurora70)
+        let aurora90 = Color(.aurora90)
+        let aurora110 = Color(.aurora110)
+
+        // MARK: - Sea
+
+        let sea30 = Color(.sea30)
+        let sea35 = Color(.sea35)
+        let sea40 = Color(.sea40)
+        let sea45 = Color(.sea45)
+        let sea50 = Color(.sea50)
+        let sea57 = Color(.sea57)
+        let sea70 = Color(.sea70)
+        let sea90 = Color(.sea90)
+        let sea110 = Color(.sea110)
+
+        // MARK: - Sky
+        let sky30 = Color(.sky30)
+        let sky35 = Color(.sky35)
+        let sky40 = Color(.sky40)
+        let sky45 = Color(.sky45)
+        let sky50 = Color(.sky50)
+        let sky57 = Color(.sky57)
+        let sky70 = Color(.sky70)
+        let sky90 = Color(.sky90)
+        let sky110 = Color(.sky110)
+
+        // MARK: - Ocean
+
+        let ocean30 = Color(.ocean30)
+        let ocean35 = Color(.ocean35)
+        let ocean40 = Color(.ocean40)
+        let ocean45 = Color(.ocean45)
+        let ocean50 = Color(.ocean50)
+        let ocean57 = Color(.ocean57)
+        let ocean70 = Color(.ocean70)
+        let ocean90 = Color(.ocean90)
+        let ocean110 = Color(.ocean110)
+
+        // MARK: - Violet
+
+        let violet30 = Color(.violet30)
+        let violet35 = Color(.violet35)
+        let violet40 = Color(.violet40)
+        let violet45 = Color(.violet45)
+        let violet50 = Color(.violet50)
+        let violet57 = Color(.violet57)
+        let violet70 = Color(.violet70)
+        let violet90 = Color(.violet90)
+        let violet110 = Color(.violet110)
+
+        // MARK: - Plum
+
+        let plum30 = Color(.plum30)
+        let plum35 = Color(.plum35)
+        let plum40 = Color(.plum40)
+        let plum45 = Color(.plum45)
+        let plum50 = Color(.plum50)
+        let plum57 = Color(.plum57)
+        let plum70 = Color(.plum70)
+        let plum90 = Color(.plum90)
+        let plum110 = Color(.plum110)
+
+        // MARK: - Stone
+
+        let stone30 = Color(.stone30)
+        let stone35 = Color(.stone35)
+        let stone40 = Color(.stone40)
+        let stone45 = Color(.stone45)
+        let stone50 = Color(.stone50)
+        let stone57 = Color(.stone57)
+        let stone70 = Color(.stone70)
+        let stone90 = Color(.stone90)
+        let stone110 = Color(.stone110)
+
 
         // TODO: Make it #if DEBUG later
-        let allColors: [ColorWithID]
+        var allColors: [ColorWithID] = []
 
         init() {
-            self.allColors = [
-                ColorWithID("blue12", blue12),
-                ColorWithID("blue45", blue45),
-                ColorWithID("blue57", blue57)
-            ]
+            //            self.allColors = [
+            //                ColorWithID("blue12", blue12),
+            //                ColorWithID("blue45", blue45),
+            //                ColorWithID("blue57", blue57)
+            //            ]
+
+            self.allColors = allColorWithIDs()
+        }
+
+        func allColorWithIDs() -> [ColorWithID] {
+            var colorList: [ColorWithID] = []
+            let mirror = Mirror(reflecting: self)
+
+            for child in mirror.children {
+                if let name = child.label {
+                    if let color = child.value as? Color {
+                        colorList.append(ColorWithID(name, color))
+                    } else if let gradient = child.value as? [Color] {
+                        for (index, gradientColor) in gradient.enumerated() {
+                            colorList.append(ColorWithID("\(name)\(index)", gradientColor))
+                        }
+                    }
+                }
+            }
+
+            return colorList
         }
     }
 }
