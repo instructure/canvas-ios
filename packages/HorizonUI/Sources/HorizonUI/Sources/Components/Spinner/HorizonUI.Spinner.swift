7e06a8892476aa99b0bc4edbf32b0a18c9deaca3
reabbotted <reabbotted@gmail.com>
2024-12-11 15:52:56 -0700

    Adding the copyright

--- packages/HorizonUI/Sources/HorizonUI/Sources/Components/Spinner/HorizonUI.Spinner.swift
+++ packages/HorizonUI/Sources/HorizonUI/Sources/Components/Spinner/HorizonUI.Spinner.swift
@@ -1,3 +1,21 @@
+//
+// This file is part of Canvas.
+// Copyright (C) 2024-present  Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
 import SwiftUI
 
 extension HorizonUI {
@@ -44,8 +62,7 @@ extension HorizonUI {
     }
 }
 
-fileprivate struct SpinnerCircle: View {
-
+private struct SpinnerCircle: View {
     // MARK: - Dependencies
 
     let color: Color
@@ -70,7 +87,7 @@ fileprivate struct SpinnerCircle: View {
             .onAppear {
                 withAnimation(
                     .linear(duration: 1)
-                    .repeatForever(autoreverses: false)
+                        .repeatForever(autoreverses: false)
                 ) {
                     rotation = 360
                 }
@@ -78,14 +95,14 @@ fileprivate struct SpinnerCircle: View {
     }
 }
 
-fileprivate struct PartialCircleShape: Shape {
+private struct PartialCircleShape: Shape {
     let diameter: CGFloat
     let isFullCircle: Bool
 
     func path(in rect: CGRect) -> Path {
         var path = Path()
         let center = CGPoint(x: rect.midX, y: rect.midY)
-        let radius = diameter / 2
+        let radius = diameter/2
 
         path.addArc(
             center: center,
