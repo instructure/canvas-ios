9d1bf9181a32fd7a01be62d9263c22f70ba56524
reabbotted <reabbotted@gmail.com>
2025-01-08 11:22:01 -0700

    Fixing some things. The font can be passed in. Disallowing overlapping highlighting.

--- packages/HorizonUI/Sources/HorizonUI/Sources/Components/AnnotationBar/HorizonUI.AnnotationBar.swift
+++ packages/HorizonUI/Sources/HorizonUI/Sources/Components/AnnotationBar/HorizonUI.AnnotationBar.swift
@@ -17,107 +17,207 @@
 //
 
 import SwiftUI
+import UIKit
 
-extension HorizonUI.ButtonStyles {
-    fileprivate struct Typography: ButtonStyle {
-        @Environment(\.isEnabled) private var isEnabled
-        private let typography: HorizonUI.Typography.Name
+typealias OnAction = (Action, Int, Int) -> Void
+typealias GetActionsCallback = (UITextView, UITextRange) -> [UIAction]
 
-        init(_ typography: HorizonUI.Typography.Name) {
-            self.typography = typography
+private class ActionableUITextView: UITextView {
+
+    let getActionsCallback: GetActionsCallback?
+    let onAction: OnAction?
+
+    init(
+        frame: CGRect,
+        textContainer: NSTextContainer?,
+        getActionsCallback: @escaping GetActionsCallback,
+        onAction: OnAction? = nil
+    ) {
+        self.getActionsCallback = getActionsCallback
+        self.onAction = onAction
+        super.init(frame: frame, textContainer: textContainer)
+    }
+
+    required init?(coder: NSCoder) {
+        self.onAction = nil
+        self.getActionsCallback = nil
+        super.init(coder: coder)
+    }
+
+    override func editMenu(for textRange: UITextRange, suggestedActions: [UIMenuElement]) -> UIMenu? {
+        var menuActions = suggestedActions
+
+        self.getActionsCallback?(self, textRange).forEach { action in
+            menuActions.insert(action, at: 0)
         }
 
-        func makeBody(configuration: Configuration) -> some View {
-            configuration.label
-                .huiTypography(typography)
-                .opacity(isEnabled ? (configuration.isPressed ? 0.8 : 1.0) : 0.5)
+        return UIMenu(title: "", children: menuActions)
+    }
+
+}
+
+struct TextSelection {
+    let range: NSRange
+    let action: Action
+}
+
+public struct NoteableTextView: UIViewRepresentable {
+    private let text: String
+    private let typography: HorizonUI.Typography.Name
+    private let viewModel: NoteableTextViewModel
+
+    public init(
+        _ text: String,
+        key: String,
+        typography: HorizonUI.Typography.Name = .p1
+    ) {
+        self.text = text
+        self.viewModel = NoteableTextViewModel(key: key)
+        self.typography = typography
+    }
+
+    public func makeUIView(context: Context) -> UITextView {
+        let textView = ActionableUITextView(
+            frame: .zero,
+            textContainer: nil,
+            getActionsCallback: viewModel.getActions
+        )
+
+        textView.isEditable = false
+        textView.isSelectable = true
+        textView.attributedText = NSAttributedString(string: text)
+        textView.backgroundColor = .clear
+        textView.isScrollEnabled = false
+//        textView.setContentHuggingPriority(.defaultHigh, for: .vertical)
+//        textView.setContentCompressionResistancePriority(.defaultHigh, for: .vertical)
+        textView.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        textView.textContainer.maximumNumberOfLines = 0
+        textView.textContainer.lineBreakMode = .byWordWrapping
+        textView.sizeToFit()
+
+        return textView
+    }
+
+    public func updateUIView(_ uiView: UITextView, context: Context) {
+        let attributedText = NSMutableAttributedString(string: text)
+
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = typography.lineSpacing
+
+        attributedText.addAttributes(
+            [
+                .font: HorizonUI.fonts.uiFont(font: typography.font),
+                .kern: typography.letterSpacing,
+                .paragraphStyle: paragraphStyle
+            ],
+            range: NSRange(location: 0, length: text.count)
+        )
+
+        viewModel.selections.forEach { selection in
+            attributedText.addAttribute(
+                .backgroundColor,
+                value: UIColor(selection.action.color).withAlphaComponent(0.25),
+                range: selection.range
+            )
+        }
+
+        uiView.attributedText = attributedText
+        uiView.sizeToFit()
+    }
+}
+
+enum Action {
+    case confusing
+    case important
+    case addNote
+
+    var color: Color {
+        switch self {
+        case .confusing: return Color.red
+        case .important: return Color.blue
+        case .addNote: return Color.green
+        }
+    }
+
+    var label: String {
+        switch self {
+        case .confusing: return "Confusing"
+        case .important: return "Important"
+        case .addNote: return "Add a Note"
         }
     }
 }
 
-extension HorizonUI {
-    public struct HUIAnnotationBar: ViewModifier {
+@Observable
+@MainActor
+public class NoteableTextViewModel {
+    let key: String
 
-        enum ButtonType: CaseIterable, Equatable, Identifiable {
-            case confusing
-            case important
-            case addNote
+    var selections: [TextSelection] = []
 
-            var id: Self { self }
+    init(key: String) {
+        self.key = key
+    }
 
-            var label: String {
-                switch self {
-                case .confusing:
-                    return String(localized: "Confusing", locale: .current)
-                case .important:
-                    return String(localized: "Important", locale: .current)
-                case .addNote:
-                    return String(localized: "Add a Note", locale: .current)
-                }
+    func getActions(textView: UITextView, textRange: UITextRange) -> [UIAction] {
+        let allActions = [
+            Action.confusing,
+            Action.important,
+            Action.addNote,
+        ]
+
+        //if the selection overlaps another section, don't show the actions
+        let start = textView.offset(from: textView.beginningOfDocument, to: textRange.start)
+        let end = textView.offset(from: textView.beginningOfDocument, to: textRange.end)
+        if isOverlapping(start: start, end: end) {
+            return []
+        }
+
+        return allActions.map { action in
+            UIAction(title: action.label) { uiAction in
+                self.onSelection(textView: textView, textRange: textRange, action: action, uiAction: uiAction)
             }
         }
-
-        var isPresented: Binding<Bool>
-
-        public init(isPresented: Binding<Bool>) {
-            self.isPresented = isPresented
-        }
-
-        public func body(content: Content) -> some View {
-            content
-                .popover(
-                    isPresented: isPresented,
-                    arrowEdge: .bottom
-                ) {
-                    ScrollView(.horizontal) {
-                        HStack {
-                            ForEach(Array(ButtonType.allCases.enumerated()), id: \.element.id) { index, type in
-
-                                HStack {
-                                    Button(
-                                        type.label
-                                    ) {}
-                                    .lineLimit(1)
-                                    .frame(maxHeight: .infinity)
-                                    .padding(.horizontal, 8)
-                                    .buttonStyle(HorizonUI.ButtonStyles.Typography(.p2))
-
-                                    if index != ButtonType.allCases.count - 1 {
-                                        Divider()
-                                            .frame(maxHeight: .infinity)
-                                            .background(Color(hexString: "#3D3D3D80"))
-                                    }
-                                }
-
-                            }
-                        }
-                        .padding(.horizontal, 16)
-                    }
-                    .frame(maxHeight: .infinity)
-                    .presentationBackground(Color(hexString: "#F2F2F2"))
-                    .presentationCompactAdaptation(.popover)
-                }
-                .frame(maxHeight: 40)
-                .fixedSize(horizontal: false, vertical: true)
-        }
     }
-}
 
-extension View {
-    public func huiAnnotationBar(isPresented: Binding<Bool>) -> some View {
-        self.modifier(HorizonUI.HUIAnnotationBar(isPresented: isPresented))
+    private func onSelection(
+        textView: UITextView,
+        textRange: UITextRange,
+        action: Action,
+        uiAction: UIAction
+    ) {
+        let start = textView.offset(from: textView.beginningOfDocument, to: textRange.start)
+        let end = textView.offset(from: textView.beginningOfDocument, to: textRange.end)
+
+        //don't allow overlap
+        if isOverlapping(start: start, end: end) {
+            return
+        }
+
+        //otherwise, add a new selection
+        let range = NSRange(location: start, length: end - start)
+        let textSelection = TextSelection(range: range, action: action)
+        selections.append(textSelection)
+    }
+
+    private func isOverlapping(start: Int, end: Int) -> Bool {
+        selections.contains { selection in
+            let selectionStart = selection.range.location
+            let selectionEnd = selection.range.location + selection.range.length
+            return (start < selectionStart && end > selectionStart) || (start < selectionEnd && end > selectionEnd)
+                || (start >= selectionStart && end <= selectionEnd)
+        }
     }
 }
 
 #Preview {
-    @State
-    @Previewable
-    var isPresented: Bool = false
-
     VStack {
-        Text("This is some text. You may select some of this text")
-            .frame(maxHeight: .infinity)
-            .textSelection(.enabled)
-            .huiAnnotationBar(isPresented: $isPresented)
+        Text("This text is not selectable. This text is not selectable. This text is not selectable.")
+        NoteableTextView(
+            "This is some text. You may select some of this text. This is some text. You may select some of this text. This is some text. You may select some of this text.",
+            key: "Test",
+            typography: .h3
+        )
+        Text("Again this is not selectable. Where is it? Again this is not selectable. Where is it? Again this is not selectable. Where is it? Again this is not selectable. Where is it? Again this is not selectable. Where is it? Again this is not selectable. Where is it? Again this is not selectable. Where is it? Again this is not selectable. Where is it? Again this is not selectable. Where is it?")
     }
 }
