184729fec57777c8495fa920ee133a156cee8573
Suhaib Al-Absi <suhaib.alabsi@instructure.com>
2025-01-15 13:17:26 +0300

    Fix CI tests issues

--- Teacher/TeacherTests/SpeedGrader/SubmissionCommentLibraryViewModelTests.swift
+++ Teacher/TeacherTests/SpeedGrader/SubmissionCommentLibraryViewModelTests.swift
@@ -26,7 +26,17 @@ class SubmissionCommentLibraryViewModelTests: TeacherTestCase {
         super.setUp()
         let comments = [APICommentLibraryResponse.CommentBankItem(id: "1", comment: "First comment"),
                         APICommentLibraryResponse.CommentBankItem(id: "2", comment: "Second comment") ]
-        let response =  APICommentLibraryResponse(data: .init(user: .init(id: "1", commentBankItems: .init(nodes: comments ))))
+        let response =  APICommentLibraryResponse(
+            data: .init(
+                user: .init(
+                    id: "1",
+                    commentBankItems: .init(
+                        nodes: comments,
+                        pageInfo: nil
+                    )
+                )
+            )
+        )
         api.mock(APICommentLibraryRequest(userId: "1"), value: response)
     }
 
