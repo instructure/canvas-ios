d1a380d869c2dd4b4ea82e750291c984a3500d84
Andras Szasz <andras.szasz@instructure.com>
2025-01-28 12:55:56 +0100

    requested changes - remove unnecessary stuff

--- Core/Core/Features/Planner/CalendarEvent/View/EditCalendarEventScreen.swift
+++ Core/Core/Features/Planner/CalendarEvent/View/EditCalendarEventScreen.swift
@@ -123,7 +123,6 @@ struct EditCalendarEventScreen: View, ScreenViewTrackable {
                         .id("details")
                         .focused($focusedInput, equals: .details)
                     }
-                    .frame(maxWidth: geometry.size.width)
                     .animation(.default, value: viewModel.isAllDay)
 
                     // defocus inputs when otherwise non-tappable area is tapped
