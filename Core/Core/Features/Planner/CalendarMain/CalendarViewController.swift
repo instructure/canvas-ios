531ea97d3daa190bb4f7f90231ee678ef8d5b0b5
Andras Szasz <andras.szasz@instructure.com>
2025-01-31 18:19:40 +0100

    requested changes - private func for updating month button accessibility value

--- Core/Core/Features/Planner/CalendarMain/CalendarViewController.swift
+++ Core/Core/Features/Planner/CalendarMain/CalendarViewController.swift
@@ -95,9 +95,7 @@ class CalendarViewController: ScreenViewTrackableViewController {
         // trailing = 8 (text-image spacing) + 20 (image width) + 4 (image right padding)
         monthButton.configuration?.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 32)
         monthButton.accessibilityLabel = String(localized: "Show a month at a time", bundle: .core)
-        monthButton.accessibilityValue = {
-            isExpanded ? String(localized: "Expanded", bundle: .core) : String(localized: "Collapsed", bundle:.core)
-        }()
+        updateMonthButtonAccessibilityValue()
 
         filterButton.setTitle(String(localized: "Calendars", bundle: .core), for: .normal)
         filterButton.titleLabel?.font = .scaledNamedFont(.regular16)
@@ -149,9 +147,15 @@ class CalendarViewController: ScreenViewTrackableViewController {
 
     @IBAction func toggleExpanded() {
         setExpanded(!isExpanded)
-        monthButton.accessibilityValue = {
-            isExpanded ? String(localized: "Expanded", bundle: .core) : String(localized: "Collapsed", bundle:.core)
-        }()
+        updateMonthButtonAccessibilityValue()
+    }
+
+    private func updateMonthButtonAccessibilityValue() {
+//        monthButton.accessibilityValue = {
+//            isExpanded ? String(localized: "Expanded", bundle: .core) : String(localized: "Collapsed", bundle:.core)
+//        }()
+
+        monthButton.accessibilityValue = isExpanded ? String(localized: "Expanded", bundle: .core) : String(localized: "Collapsed", bundle:.core)
     }
 
     func setExpanded(_ flag: Bool) {
