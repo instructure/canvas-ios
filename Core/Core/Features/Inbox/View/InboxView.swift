ffd74a103e8d964d6b79dce5ddd8395e22346340
domonkosadam <domonkosadam01@gmail.com>
2025-02-20 13:40:50 +0100

    Fix navigation bar title

--- Core/Core/Features/Inbox/View/InboxView.swift
+++ Core/Core/Features/Inbox/View/InboxView.swift
@@ -75,6 +75,8 @@ public struct InboxView: View, ScreenViewTrackable {
         .snackBar(viewModel: model.snackBarViewModel)
         .background(Color.backgroundLightest)
         .navigationBarItems(leading: model.isShowMenuButton ? menuButton : nil, trailing: newMessageButton)
+        .navigationBarTitleView(String(localized: "Inbox", bundle: .core))
+        .navigationBarStyle(.global)
     }
 
     private var messagesList: some View {
