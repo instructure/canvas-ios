cd9f29c244bef7ef837f4b0c731465b26f79552b
domonkosadam <domonkosadam01@gmail.com>
2025-02-25 13:10:44 +0100

    Update ComposeMessageViewModelTests.swift

--- Core/CoreTests/Features/Inbox/ComposeMessage/ViewModel/ComposeMessageViewModelTests.swift
+++ Core/CoreTests/Features/Inbox/ComposeMessage/ViewModel/ComposeMessageViewModelTests.swift
@@ -712,7 +712,7 @@ class ComposeMessageViewModelTests: CoreTestCase {
         let exp = expectation(description: "signatureLoaded")
         var initFlag = false
         inboxSettingsInteractor.signature
-            .sink { ( _ in
+            .sink { _ in
                 if initFlag { exp.fulfill() }
                 initFlag = true
             }
