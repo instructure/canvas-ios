7b1629b0ba1d167ec67e9f16f2ea23880c4a8bb5
domonkosadam <domonkosadam01@gmail.com>
2025-03-04 10:20:11 +0100

    Fix lint

--- Core/Core/Features/Profile/Settings/ProfileSettingsViewController.swift
+++ Core/Core/Features/Profile/Settings/ProfileSettingsViewController.swift
@@ -201,7 +201,7 @@ public class ProfileSettingsViewController: ScreenViewTrackableViewController {
         let detailLabel = self.isInboxSignatureEnabled
             ? String(localized: "Enabled", bundle: .core)
             : String(localized: "Not set", bundle: .core)
-        
+
         return Section(String(localized: "Inbox", bundle: .core), rows: [
                 Row(String(localized: "Inbox Signature", bundle: .core),
                     detail: detailLabel,
