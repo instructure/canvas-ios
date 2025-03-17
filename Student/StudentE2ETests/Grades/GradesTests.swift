67066140f29141362353908fe35b2ddd0c5fd373
Attila Varga <72396990+vargaat@users.noreply.github.com>
2025-03-17 14:52:41 +0100

    Update E2E tests (#3229)
    refs: MBL-18658
    affects: none
    release note:  none
    
    test plan: E2E tests to pass.

--- Student/StudentE2ETests/Grades/GradesTests.swift
+++ Student/StudentE2ETests/Grades/GradesTests.swift
@@ -304,7 +304,7 @@ class GradesTests: E2ETestCase {
         XCTAssertTrue(totalGradeLabel.hasLabel(label: "Total grade is \(expectedTotalGrade)"))
 
         let filterButton = GradesHelper.filterButton.waitUntil(.visible)
-        let upcomingAssignmentsLabel = GradesHelper.upcomingAssignmentsLabel.waitUntil(.visible)
+        let upcomingAssignmentsLabel = GradesHelper.upcomingAssignmentsSectionTitle(numberOfItems: 2).waitUntil(.visible)
         XCTAssertTrue(filterButton.isVisible)
         XCTAssertTrue(upcomingAssignmentsLabel.isVisible)
 
@@ -322,8 +322,8 @@ class GradesTests: E2ETestCase {
 
         saveButton.hit()
 
-        let labelOfAG1 = GradesHelper.labelOfAG(assignmentGroup: testAG1).waitUntil(.visible)
-        let labelOfAG2 = GradesHelper.labelOfAG(assignmentGroup: testAG2).waitUntil(.visible)
+        let labelOfAG1 = GradesHelper.labelOfAssignmentGroup(testAG1, numberOfItems: 1).waitUntil(.visible)
+        let labelOfAG2 = GradesHelper.labelOfAssignmentGroup(testAG2, numberOfItems: 1).waitUntil(.visible)
         XCTAssertTrue(labelOfAG1.isVisible)
         XCTAssertTrue(labelOfAG2.isVisible)
 
@@ -399,13 +399,13 @@ class GradesTests: E2ETestCase {
         let totalGrade = GradesHelper.totalGrade.waitUntil(.visible)
         totalGrade.waitUntil(.label(expected: basedOnGradedExpected))
         XCTAssertTrue(basedOnGradedSwitch.isVisible)
-        XCTAssertTrue(basedOnGradedSwitch.hasValue(value: "1"))
+        XCTAssertTrue(basedOnGradedSwitch.hasValue(value: "on"))
         XCTAssertTrue(totalGrade.isVisible)
         XCTAssertTrue(totalGrade.hasLabel(label: basedOnGradedExpected, strict: false))
 
         // MARK: Toggle switch, check Total Grade again
         basedOnGradedSwitch.hit()
-        XCTAssertTrue(basedOnGradedSwitch.waitUntil(.value(expected: "0")).hasValue(value: "0"))
+        XCTAssertTrue(basedOnGradedSwitch.waitUntil(.value(expected: "off")).hasValue(value: "off"))
         XCTAssertTrue(totalGrade.isVisible)
         XCTAssertTrue(totalGrade.hasLabel(label: notBasedOnGradedExpected, strict: false))
     }
