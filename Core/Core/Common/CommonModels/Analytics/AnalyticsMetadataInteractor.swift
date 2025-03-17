28f2ee9428f8175c5ddfd7bd04861ada79929e7d
szabinst <szabolcs.fuzi@instructure.com>
2025-03-17 19:37:41 +0100

    chore: AnalyticsMetadata

---
+++ Core/Core/Common/CommonModels/Analytics/AnalyticsMetadataInteractor.swift
@@ -0,0 +1,55 @@
+//
+// This file is part of Canvas.
+// Copyright (C) 2025-present  Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+public protocol AnalyticsMetadataInteractor {
+    func getMetadata() -> AnalyticsMetadata
+}
+
+public class AnalyticsMetadataInteractorLive: AnalyticsMetadataInteractor {
+    private let loginSession: LoginSession
+    private let environmentFeatureFlags: Store<GetEnvironmentFeatureFlags>
+
+    public init?(
+        loginSession: LoginSession?,
+        environmentFeatureFlags: Store<GetEnvironmentFeatureFlags>
+    ) {
+        if let loginSession {
+            self.loginSession = loginSession
+        } else {
+            return nil
+        }
+        self.environmentFeatureFlags = environmentFeatureFlags
+    }
+
+    public func getMetadata() -> AnalyticsMetadata {
+        let userId = loginSession.hashedUserId()
+        let accountId = ""
+        return AnalyticsMetadata(
+            userId: userId,
+            accountUUID: accountId,
+            visitorData: .init(
+                id: userId,
+                locale: loginSession.locale ?? ""
+            ),
+            accountData: .init(
+                id: accountId,
+                surveyOptOut: environmentFeatureFlags.isFeatureEnabled(.account_survey_notifications)
+            )
+        )
+    }
+}
