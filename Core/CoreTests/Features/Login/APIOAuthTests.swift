981126d16df1669080cf5fb2c3bc09f03f70c5e8
szabinst <szabolcs.fuzi@instructure.com>
2025-03-28 14:50:09 +0100

    chore: re-enable and extend APIOAuthTests

--- Core/CoreTests/Features/Login/APIOAuthTests.swift
+++ Core/CoreTests/Features/Login/APIOAuthTests.swift
@@ -16,70 +16,93 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
-import XCTest
-import TestsFoundation
 @testable import Core
+import TestsFoundation
+import XCTest
 
 class APIOAuthTests: CoreTestCase {
-//    func testGetMobileVerifyRequest() {
-//        XCTAssertEqual(GetMobileVerifyRequest(domain: "cgnu").path, "https://canvas.instructure.com/api/v1/mobile_verify.json")
-//        XCTAssertEqual(GetMobileVerifyRequest(domain: "cgnu").queryItems, [
-//            URLQueryItem(name: "domain", value: "cgnu")
-//        ])
-//    }
-//
-//    func testPostLoginOAuthRequestCode() {
-//        let client = APIVerifyClient(authorized: true, base_url: URL(string: "https://cgnuonline-eniversity.edu"), client_id: "cgnu", client_secret: "dna evidence")
-//        XCTAssertEqual(PostLoginOAuthRequest(client: client, code: "1234").method, .post)
-//        XCTAssertEqual(PostLoginOAuthRequest(client: client, code: "1234").path, "https://cgnuonline-eniversity.edu/login/oauth2/token")
-//        XCTAssertEqual(PostLoginOAuthRequest(client: client, code: "1234").body?.client_id, "cgnu")
-//        XCTAssertEqual(PostLoginOAuthRequest(client: client, code: "1234").body?.client_secret, "dna evidence")
-//        XCTAssertEqual(PostLoginOAuthRequest(client: client, code: "1234").body?.grant_type, "authorization_code")
-//        XCTAssertEqual(PostLoginOAuthRequest(client: client, code: "1234").body?.code, "1234")
-//    }
-//
-//    func testPostLoginOAuthUnauthorized() {
-//        let client = APIVerifyClient(authorized: false, base_url: nil, client_id: nil, client_secret: nil)
-//        XCTAssertEqual(PostLoginOAuthRequest(client: client, code: "1234").path, "login/oauth2/token")
-//        XCTAssertEqual(PostLoginOAuthRequest(client: client, code: "1234").body?.client_id, "")
-//        XCTAssertEqual(PostLoginOAuthRequest(client: client, code: "1234").body?.client_secret, "")
-//        XCTAssertEqual(PostLoginOAuthRequest(client: client, code: "1234").body?.grant_type, "authorization_code")
-//        XCTAssertEqual(PostLoginOAuthRequest(client: client, code: "1234").body?.code, "1234")
-//    }
-//
-//    func testPostLoginOAuthRequestRefreshToken() {
-//        let client = APIVerifyClient(authorized: true, base_url: URL(string: "https://cgnuonline-eniversity.edu"), client_id: "cgnu", client_secret: "dna evidence")
-//        XCTAssertEqual(PostLoginOAuthRequest(client: client, refreshToken: "1234").method, .post)
-//        XCTAssertEqual(PostLoginOAuthRequest(client: client, refreshToken: "1234").path, "https://cgnuonline-eniversity.edu/login/oauth2/token")
-//        XCTAssertEqual(PostLoginOAuthRequest(client: client, refreshToken: "1234").body?.client_id, "cgnu")
-//        XCTAssertEqual(PostLoginOAuthRequest(client: client, refreshToken: "1234").body?.client_secret, "dna evidence")
-//        XCTAssertEqual(PostLoginOAuthRequest(client: client, refreshToken: "1234").body?.grant_type, "refresh_token")
-//        XCTAssertEqual(PostLoginOAuthRequest(client: client, refreshToken: "1234").body?.refresh_token, "1234")
-//    }
-//
-//    func testDeleteLoginOAuthRequest() {
-//        XCTAssertEqual(DeleteLoginOAuthRequest().method, .delete)
-//        XCTAssertEqual(DeleteLoginOAuthRequest().path, "/login/oauth2/token")
-//    }
-//
-//    func testGetWebSessionRequestWhenNotMasquerading() {
-//        XCTAssertEqual(GetWebSessionRequest(to: nil).path, "/login/session_token")
-//        XCTAssertEqual(GetWebSessionRequest(to: nil).queryItems, [])
-//        XCTAssertEqual(GetWebSessionRequest(to: URL(string: "/")).queryItems, [
-//            URLQueryItem(name: "return_to", value: "/?display=borderless")
-//        ])
-//    }
-//
-//    func testGetWebSessionRequestWhenMasquerading() {
-//        environment.currentSession = .make(
-//            masquerader: URL(string: "something"),
-//            userID: "42"
-//        )
-//
-//        XCTAssertEqual(GetWebSessionRequest(to: nil).path, "/api/v1/login/session_token")
-//        XCTAssertEqual(GetWebSessionRequest(to: nil).queryItems, [])
-//        XCTAssertEqual(GetWebSessionRequest(to: URL(string: "/")).queryItems, [
-//            URLQueryItem(name: "return_to", value: "/?display=borderless&as_user_id=42")
-//        ])
-//    }
+    func testGetMobileVerifyRequest() {
+        XCTAssertEqual(GetMobileVerifyRequest(domain: "cgnu").path, "https://canvas.instructure.com/api/v1/mobile_verify.json")
+        XCTAssertEqual(GetMobileVerifyRequest(domain: "cgnu").queryItems, [
+            URLQueryItem(name: "domain", value: "cgnu")
+        ])
+    }
+
+    func testPostLoginManualOAuthRequestCode() {
+        let client = APIVerifyClient(authorized: true, base_url: URL(string: "https://cgnuonline-eniversity.edu"), client_id: "cgnu", client_secret: "dna evidence")
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: .manual(.init(client: client)), code: "1234").method, .post)
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: .manual(.init(client: client)), code: "1234").path, "https://cgnuonline-eniversity.edu/login/oauth2/token")
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: .manual(.init(client: client)), code: "1234").body?.client_id, "cgnu")
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: .manual(.init(client: client)), code: "1234").body?.client_secret, "dna evidence")
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: .manual(.init(client: client)), code: "1234").body?.code_verifier, nil)
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: .manual(.init(client: client)), code: "1234").body?.grant_type, "authorization_code")
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: .manual(.init(client: client)), code: "1234").body?.code, "1234")
+    }
+
+    func testPostLoginPKCEOAuthRequestCode() {
+        let pkce = OAuthType.pkce(PKCEOAuthAttributes(baseURL: URL(string: "https://canvas.instructure.com")!, clientID: "client-id", codeVerifier: "code-verifier"))
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: pkce, code: "1234").method, .post)
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: pkce, code: "1234").path, "https://canvas.instructure.com/login/oauth2/token")
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: pkce, code: "1234").body?.client_id, "client-id")
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: pkce, code: "1234").body?.code_verifier, "code-verifier")
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: pkce, code: "1234").body?.client_secret, nil)
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: pkce, code: "1234").body?.grant_type, "authorization_code")
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: pkce, code: "1234").body?.code, "1234")
+    }
+
+    func testPostLoginOAuthUnauthorized() {
+        let client = APIVerifyClient(authorized: false, base_url: nil, client_id: nil, client_secret: nil)
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: .manual(.init(client: client)), code: "1234").path, "login/oauth2/token")
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: .manual(.init(client: client)), code: "1234").body?.client_id, "")
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: .manual(.init(client: client)), code: "1234").body?.client_secret, nil)
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: .manual(.init(client: client)), code: "1234").body?.grant_type, "authorization_code")
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: .manual(.init(client: client)), code: "1234").body?.code, "1234")
+    }
+
+    func testPostLoginManualOAuthRequestRefreshToken() {
+        let client = APIVerifyClient(authorized: true, base_url: URL(string: "https://cgnuonline-eniversity.edu"), client_id: "cgnu", client_secret: "dna evidence")
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: .manual(.init(client: client)), refreshToken: "1234").method, .post)
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: .manual(.init(client: client)), refreshToken: "1234").path, "https://cgnuonline-eniversity.edu/login/oauth2/token")
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: .manual(.init(client: client)), refreshToken: "1234").body?.client_id, "cgnu")
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: .manual(.init(client: client)), refreshToken: "1234").body?.client_secret, "dna evidence")
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: .manual(.init(client: client)), refreshToken: "1234").body?.grant_type, "refresh_token")
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: .manual(.init(client: client)), refreshToken: "1234").body?.refresh_token, "1234")
+    }
+
+    func testPostLoginPKCEOAuthRequestRefreshToken() {
+        let pkce = OAuthType.pkce(PKCEOAuthAttributes(baseURL: URL(string: "https://canvas.instructure.com")!, clientID: "client-id", codeVerifier: "code-verifier"))
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: pkce, refreshToken: "1234").method, .post)
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: pkce, refreshToken: "1234").path, "https://canvas.instructure.com/login/oauth2/token")
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: pkce, refreshToken: "1234").body?.client_id, "client-id")
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: pkce, refreshToken: "1234").body?.client_secret, nil)
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: pkce, refreshToken: "1234").body?.code_verifier, "code-verifier")
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: pkce, refreshToken: "1234").body?.grant_type, "refresh_token")
+        XCTAssertEqual(PostLoginOAuthRequest(oauthType: pkce, refreshToken: "1234").body?.refresh_token, "1234")
+    }
+
+    func testDeleteLoginOAuthRequest() {
+        XCTAssertEqual(DeleteLoginOAuthRequest().method, .delete)
+        XCTAssertEqual(DeleteLoginOAuthRequest().path, "/login/oauth2/token")
+    }
+
+    func testGetWebSessionRequestWhenNotMasquerading() {
+        XCTAssertEqual(GetWebSessionRequest(to: nil).path, "/login/session_token")
+        XCTAssertEqual(GetWebSessionRequest(to: nil).queryItems, [])
+        XCTAssertEqual(GetWebSessionRequest(to: URL(string: "/")).queryItems, [
+            URLQueryItem(name: "return_to", value: "/?display=borderless")
+        ])
+    }
+
+    func testGetWebSessionRequestWhenMasquerading() {
+        environment.currentSession = .make(
+            masquerader: URL(string: "something"),
+            userID: "42"
+        )
+
+        XCTAssertEqual(GetWebSessionRequest(to: nil).path, "/api/v1/login/session_token")
+        XCTAssertEqual(GetWebSessionRequest(to: nil).queryItems, [])
+        XCTAssertEqual(GetWebSessionRequest(to: URL(string: "/")).queryItems, [
+            URLQueryItem(name: "return_to", value: "/?display=borderless&as_user_id=42")
+        ])
+    }
 }
