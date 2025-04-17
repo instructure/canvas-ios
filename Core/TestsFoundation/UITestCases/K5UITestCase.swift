9f2d65fefa333d1c699e92af75b7dba02cc1b6d2
szabinst <szabolcs.fuzi@instructure.com>
2025-04-17 14:16:02 +0200

    fix: conflicts

--- Core/TestsFoundation/UITestCases/K5UITestCase.swift
+++ Core/TestsFoundation/UITestCases/K5UITestCase.swift
@@ -1,10 +1,6 @@
 //
 // This file is part of Canvas.
-<<<<<<<< HEAD:Core/CoreTests/Features/Courses/SmartSearch/Model/CourseSmartSearchViewAttributesTests.swift
 // Copyright (C) 2025-present  Instructure, Inc.
-========
-// Copyright (C) 2021-present  Instructure, Inc.
->>>>>>>> origin/master:Core/TestsFoundation/UITestCases/K5UITestCase.swift
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
@@ -20,31 +16,11 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
-import XCTest
 @testable import Core
+import XCTest
 
-<<<<<<<< HEAD:Core/CoreTests/Features/Courses/SmartSearch/Model/CourseSmartSearchViewAttributesTests.swift
-class CourseSmartSearchViewAttributesTests: CoreTestCase {
-
-    func test_default_properties() {
-        let testee = CourseSmartSearchViewAttributes.default
-
-        XCTAssertEqual(testee.context, .currentUser)
-        XCTAssertNil(testee.accentColor)
-    }
-
-    func test_custom_properties() {
-        let testee = CourseSmartSearchViewAttributes(
-            context: .course("1"),
-            color: .red
-        )
-
-        XCTAssertEqual(testee.context, .course("1"))
-        XCTAssertEqual(testee.accentColor, .red)
-        XCTAssertEqual(testee.searchPrompt, String(localized: "Search in this course", bundle: .core))
-========
 open class K5UITestCase: CoreUITestCase {
-    override open var experimentalFeatures: [ExperimentalFeature] { return [ExperimentalFeature.K5Dashboard]}
+    override open var experimentalFeatures: [ExperimentalFeature] { return [ExperimentalFeature.K5Dashboard] }
 
     open func resetAppStateForK5() {
         app.terminate()
@@ -52,7 +28,7 @@ open class K5UITestCase: CoreUITestCase {
         sleep(5)
     }
 
-    open override var user: UITestUser? {
+    override open var user: UITestUser? {
         .readStudentK5
     }
 
@@ -61,6 +37,5 @@ open class K5UITestCase: CoreUITestCase {
         resetAppStateForK5()
         app.pullToRefresh()
         K5Helper.homeroom.waitUntil(.visible)
->>>>>>>> origin/master:Core/TestsFoundation/UITestCases/K5UITestCase.swift
     }
 }
