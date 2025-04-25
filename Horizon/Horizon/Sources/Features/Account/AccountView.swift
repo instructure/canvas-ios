0a318516beb7183fa2f45560c2b4e06aa7b70ca1
Ahmed Naguib <Ahmed.Naguib@instructure.com>
2025-04-25 15:34:31 +0300

    feat: add empty view for scores
    [ignore-commit-lint]

--- Horizon/Horizon/Sources/Features/Account/AccountView.swift
+++ Horizon/Horizon/Sources/Features/Account/AccountView.swift
@@ -67,13 +67,14 @@ struct AccountView: View {
                         viewModel.profileDidTap(viewController: viewController)
                     }
                 )
-                divider
-                AccountEntryRowView(
-                    title: String(localized: "Password", bundle: .horizon),
-                    didTapRow: {
-                        viewModel.passwordDidTap()
-                    }
-                )
+                // TODO: Uncomment after implementing the functionality
+//                divider
+//                AccountEntryRowView(
+//                    title: String(localized: "Password", bundle: .horizon),
+//                    didTapRow: {
+//                        viewModel.passwordDidTap()
+//                    }
+//                )
                 divider
                 AccountEntryRowView(
                     title: String(localized: "Notifications", bundle: .horizon),
