9f8d77d56c99ec0b520f44408981622b624269d6
szabinst <szabolcs.fuzi@instructure.com>
2025-05-19 16:04:32 +0200

    chore: core data entities and api requests
    [ignore-commit-lint]

---
+++ Core/Core/Features/Courses/CourseProgression/GetCoursesProgressionUseCase.swift
@@ -0,0 +1,73 @@
+//
+// This file is part of Canvas.
+// Copyright (C) 2024-present  Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import CoreData
+import Foundation
+
+public class GetCoursesProgressionUseCase: APIUseCase {
+    // MARK: - Typealias
+
+    public typealias Model = CDCourse
+    public typealias Request = GetCoursesProgressionRequest
+
+    // MARK: - Properties
+
+    public var cacheKey: String? {
+        if let courseId {
+            return "courses-progression-\(courseId)"
+        } else {
+            return "courses-progression"
+        }
+    }
+
+    private let courseId: String?
+    private let userId: String
+    private let horizonCourses: Bool
+
+    public var request: GetCoursesProgressionRequest {
+        .init(userId: userId, horizonCourses: horizonCourses)
+    }
+
+    // MARK: - Init
+
+    public init(userId: String, courseId: String? = nil, horizonCourses: Bool = false) {
+        self.userId = userId
+        self.courseId = courseId
+        self.horizonCourses = horizonCourses
+    }
+
+    // MARK: - Functions
+
+    public func write(
+        response: GetCoursesProgressionResponse?,
+        urlResponse _: URLResponse?,
+        to client: NSManagedObjectContext
+    ) {
+        let enrollments = response?.data?.user?.enrollments ?? []
+        enrollments.forEach { enrollment in
+            CDCourse.save(enrollment, in: client)
+        }
+    }
+
+    public var scope: Scope {
+        if let courseId = courseId {
+            return .where(#keyPath(CDCourse.courseID), equals: courseId)
+        }
+        return .all
+    }
+}
