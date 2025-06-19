41d11ebaa9ed55940fcec324437ee2f21b2cfcbe
Suhaib Al-Absi <suhaib.alabsi@instructure.com>
2025-06-19 11:27:59 +0300

    Fixed dynamic font size issue with navigation bar subtitled header (#3442)
    refs: MBL-18880
    affects: Student, Teacher, Parent
    release note: Fixed font size issue with navigation bar subtitled header on (200%+) accessibility size

--- Core/CoreTests/Common/CommonUI/Fonts/UIFontExtensionsTests.swift
+++ Core/CoreTests/Common/CommonUI/Fonts/UIFontExtensionsTests.swift
@@ -27,6 +27,52 @@ class UIFontExtensionsTests: XCTestCase {
         AppEnvironment.shared.k5.userDidLogout()
     }
 
+    func testApplicationFontName() {
+        var fontName: String = ""
+
+        [UIFont.Weight.black, .heavy].forEach { weight in
+            fontName = UIFont.applicationFontName(weight: weight)
+            XCTAssertEqual(fontName, "Lato-Black")
+
+            fontName = UIFont.applicationFontName(weight: weight, isItalic: true)
+            XCTAssertEqual(fontName, "Lato-Black")
+        }
+
+        [UIFont.Weight.bold, .medium].forEach { weight in
+            fontName = UIFont.applicationFontName(weight: weight)
+            XCTAssertEqual(fontName, "Lato-Bold")
+
+            fontName = UIFont.applicationFontName(weight: weight, isItalic: true)
+            XCTAssertEqual(fontName, "Lato-BoldItalic")
+        }
+
+        fontName = UIFont.applicationFontName(weight: .semibold)
+        XCTAssertEqual(fontName, "Lato-SemiBold")
+
+        fontName = UIFont.applicationFontName(weight: .semibold, isItalic: true)
+        XCTAssertEqual(fontName, "Lato-SemiBoldItalic")
+
+        [UIFont.Weight.regular, .light, .thin, .ultraLight].forEach { weight in
+            fontName = UIFont.applicationFontName(weight: weight)
+            XCTAssertEqual(fontName, "Lato-Regular")
+
+            fontName = UIFont.applicationFontName(weight: weight, isItalic: true)
+            XCTAssertEqual(fontName, "Lato-Italic")
+        }
+
+        mockK5Mode()
+
+        [UIFont.Weight.black, .heavy].forEach { weight in
+            fontName = UIFont.applicationFontName(weight: weight)
+            XCTAssertEqual(fontName, "BalsamiqSans-Bold")
+        }
+
+        [UIFont.Weight.bold, .medium, .semibold, .regular, .light, .thin, .ultraLight].forEach { weight in
+            fontName = UIFont.applicationFontName(weight: weight)
+            XCTAssertEqual(fontName, "BalsamiqSans-Regular")
+        }
+    }
+
     func testScaledNamedFont() {
         for name in UIFont.Name.allCases {
             XCTAssertNotNil(UIFont.scaledNamedFont(name))
@@ -34,14 +80,63 @@ class UIFontExtensionsTests: XCTestCase {
     }
 
     func testScaledK5Font() {
+        mockK5Mode()
+
+        for name in UIFont.Name.allCases {
+            XCTAssertNotNil(UIFont.scaledNamedFont(name))
+        }
+    }
+
+    func testFontNameAttributes() {
+        XCTAssertEqual(UIFont.Name.bold10.weight, .bold)
+        XCTAssertEqual(UIFont.Name.bold16.weight, .bold)
+        XCTAssertEqual(UIFont.Name.medium10.weight, .medium)
+        XCTAssertEqual(UIFont.Name.medium14.weight, .medium)
+        XCTAssertEqual(UIFont.Name.semibold13.weight, .semibold)
+        XCTAssertEqual(UIFont.Name.semibold18.weight, .semibold)
+        XCTAssertEqual(UIFont.Name.regular10.weight, .regular)
+        XCTAssertEqual(UIFont.Name.regular13.weight, .regular)
+        XCTAssertEqual(UIFont.Name.heavy24.weight, .heavy)
+
+        XCTAssertFalse(UIFont.Name.regular15.isItalic)
+        XCTAssertTrue(UIFont.Name.regular14Italic.isItalic)
+        XCTAssertTrue(UIFont.Name.semibold16Italic.isItalic)
+
+        XCTAssertEqual(UIFont.Name.regular15.fontSize, 15)
+        XCTAssertEqual(UIFont.Name.semibold13.fontSize, 13)
+        XCTAssertEqual(UIFont.Name.medium14.fontSize, 14)
+        XCTAssertEqual(UIFont.Name.heavy24.fontSize, 24)
+        XCTAssertEqual(UIFont.Name.bold16.fontSize, 16)
+
+        XCTAssertTrue(UIFont.Name.regular11Monodigit.isMonospaced)
+        XCTAssertTrue(UIFont.Name.regular20Monodigit.isMonospaced)
+        XCTAssertFalse(UIFont.Name.regular10.isMonospaced)
+
+        XCTAssertEqual(UIFont.Name.regular10.style, .body)
+        XCTAssertEqual(UIFont.Name.semibold12.style, .body)
+
+        XCTAssertEqual(UIFont.Name.semibold16.style, .callout)
+        XCTAssertEqual(UIFont.Name.regular20.style, .callout)
+
+        XCTAssertEqual(UIFont.Name.medium20.style, .title)
+        XCTAssertEqual(UIFont.Name.heavy24.style, .title)
+
+        XCTAssertEqual(UIFont.Name.medium16.style, .title2)
+        XCTAssertEqual(UIFont.Name.bold15.style, .title2)
+
+        XCTAssertEqual(UIFont.Name.bold22.style, .title3)
+        XCTAssertEqual(UIFont.Name.medium10.style, .title3)
+
+        XCTAssertEqual(UIFont.Name.bold34.style, .largeTitle)
+    }
+
+    // MARK: Utils
+
+    private func mockK5Mode() {
         ExperimentalFeature.K5Dashboard.isEnabled = true
         let environment = AppEnvironment.shared
         environment.userDefaults = .fallback
         environment.userDefaults?.isElementaryViewEnabled = true
         environment.k5.userDidLogin(isK5Account: true)
-
-        for name in UIFont.Name.allCases {
-            XCTAssertNotNil(UIFont.scaledNamedFont(name))
-        }
     }
 }
