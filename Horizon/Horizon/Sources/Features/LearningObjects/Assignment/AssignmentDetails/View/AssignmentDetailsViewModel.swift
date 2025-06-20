0a64c8e9dcd40d7763f7dfc4f99a7fab24b5da93
Ahmed Naguib <Ahmed.Naguib@instructure.com>
2025-06-20 15:21:59 +0300

    [Horizon]  Add View Attempts for Assignments (#3452)

--- Horizon/Horizon/Sources/Features/LearningObjects/Assignment/AssignmentDetails/View/AssignmentDetailsViewModel.swift
+++ Horizon/Horizon/Sources/Features/LearningObjects/Assignment/AssignmentDetails/View/AssignmentDetailsViewModel.swift
@@ -51,6 +51,7 @@ final class AssignmentDetailsViewModel {
     private(set) var selectedSubmission: AssignmentSubmissionType = .text
     private(set) var externalURL: URL?
     private(set) var submissionProperties: SubmissionProperties?
+    private(set) var shouldShowViewAttempts = false
     var isStartTyping = false
     var assignmentPreference: AssignmentPreferenceKeyType?
     var isSubmitButtonHidden: Bool {
@@ -169,6 +170,7 @@ final class AssignmentDetailsViewModel {
             }
             hasSubmittedBefore = true
             submission = selectedSubmission
+            shouldShowViewAttempts = selectedSubmission != submissions.first
             submissionProperties?.hasUnreadComments = hasUnreadComments(for: selectedSubmission?.attempt)
             didLoadAssignment(submissionProperties)
         }
