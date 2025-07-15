6a6ab1276c7032ecdf8e3f02156bd6c14e727ecc
reabbotted <reabbotted@gmail.com>
2025-07-15 12:13:11 -0600

    Removing an import statement for SwiftUICore, opting for SwiftUI instead so that the automated deployment to the app store works

--- Horizon/Horizon/Sources/Features/LearningObjects/Assignment/AssignmentDetails/View/AssignmentPreferences.swift
+++ Horizon/Horizon/Sources/Features/LearningObjects/Assignment/AssignmentDetails/View/AssignmentPreferences.swift
@@ -16,7 +16,7 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
-import SwiftUICore
+import SwiftUI
 
 enum AssignmentPreferenceKeyType: Equatable {
     case confirmation(viewModel: SubmissionAlertViewModel)
