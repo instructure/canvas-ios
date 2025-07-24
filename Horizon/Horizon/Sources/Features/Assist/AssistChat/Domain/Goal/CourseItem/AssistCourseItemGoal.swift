3cd3bc16ead93f86334ebcfa9fb10728d3a73c6f
reabbotted <reabbotted@gmail.com>
2025-07-24 06:17:57 -0600

    Updating Assist suggestions [ignore-commit-lint]

--- Horizon/Horizon/Sources/Features/Assist/AssistChat/Domain/Goal/CourseItem/AssistCourseItemGoal.swift
+++ Horizon/Horizon/Sources/Features/Assist/AssistChat/Domain/Goal/CourseItem/AssistCourseItemGoal.swift
@@ -23,12 +23,12 @@ import Combine
 class AssistCourseItemGoal: AssistGoal {
 
     enum Option: String, CaseIterable {
-        case Summarize = "Summarize"
-        case KeyTakeaways = "Key Takeaways"
-        case TellMeMore = "Tell me more"
-        case FlashCards = "Flash Cards"
-        case Quiz = "Quiz Questions"
-        case Rephrase = "Rephrase Content"
+        case Summarize = "Summarize the Content"
+        case KeyTakeaways = "What are the Key Takeaways?"
+        case TellMeMore = "Tell Me More"
+        case FlashCards = "Generate Flash Cards"
+        case Quiz = "Generate Quiz Questions"
+        case Rephrase = "Rephrase the Content"
     }
 
     // MARK: - Properties
