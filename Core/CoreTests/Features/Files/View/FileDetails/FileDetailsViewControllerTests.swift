f0d17cf270f16612e0fe73e182fa98306f226fa4
Ganesh Navale <ganesh.navale@instructure.com>
2025-09-04 13:19:04 +0530

    Reusing shared StudentAccessInteractorMock
    builds: student

--- Core/CoreTests/Features/Files/View/FileDetails/FileDetailsViewControllerTests.swift
+++ Core/CoreTests/Features/Files/View/FileDetails/FileDetailsViewControllerTests.swift
@@ -351,20 +351,3 @@ extension FileDetailsViewControllerTests: PSPDFKit.PDFDocumentDelegate {
         saveWasCalled = true // although it may have failed, it was called
     }
 }
-
-private class StudentAccessInteractorMock: StudentAccessInteractor {
-    private let isStudentAccessRestricted: CurrentValueSubject<Bool, Never>
-
-    init(isRestricted: Bool = false) {
-        isStudentAccessRestricted = CurrentValueSubject(isRestricted)
-    }
-
-    func isRestricted() -> AnyPublisher<Bool, Never> {
-        isStudentAccessRestricted.eraseToAnyPublisher()
-    }
-
-    // Optional: allow changing value in test
-    func setRestricted(_ value: Bool) {
-        isStudentAccessRestricted.send(value)
-    }
-}
