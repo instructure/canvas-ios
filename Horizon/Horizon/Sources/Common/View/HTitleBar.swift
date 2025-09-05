db905fcebc9346664c840b395a3f1b21f46b6218
reabbotted <reabbotted@gmail.com>
2025-09-05 16:02:05 -0600

    Just adding some comments

--- Horizon/Horizon/Sources/Common/View/HTitleBar.swift
+++ Horizon/Horizon/Sources/Common/View/HTitleBar.swift
@@ -24,6 +24,8 @@ struct HTitleBar: View {
 
     // MARK: - Types
     typealias Callback = (Action) -> Void
+
+    /// These are the possible actions that can be taken on the title bar
     enum Action {
         case back
         case close
@@ -33,12 +35,15 @@ struct HTitleBar: View {
         case notifications
     }
 
+    /// These are the possible states for each action
+    /// These can be passed in to modify the styling of the action button
     enum ActionState {
         case disabled
         case enabled
         case hidden
     }
 
+    /// These are the possible pages that the title bar can be used on
     enum Page {
         case assist
         case assistQuiz
@@ -58,6 +63,7 @@ struct HTitleBar: View {
     }
 
     // MARK: - Constants
+    /// Configuring the icons for each action
     private static let actionIcon: [Action: Image] = [
         .back: .huiIcons.arrowBack,
         .close: .huiIcons.close,
@@ -66,11 +72,13 @@ struct HTitleBar: View {
         .notifications: .huiIcons.notifications
     ]
 
+    /// Configuring the background color for each page
     private static let backgroundColorMap: [Page: Color] = [
         .assist: .clear,
         .createMessage: HorizonUI.colors.surface.pageSecondary
     ]
 
+    /// configuring which pages have a back button
     private static let backButtonPages: [Page] = [
         .assistFlashCards,
         .assistQuiz,
@@ -83,6 +91,8 @@ struct HTitleBar: View {
         .settingsNotifications,
         .settingsProfile
     ]
+
+    /// configuring which pages have trailing buttons and what those buttons are
     private static let trailingButtonPages: [Page: [Action]] = [
         .assist: [.back, .close],
         .createMessage: [.close],
@@ -93,19 +103,27 @@ struct HTitleBar: View {
         ],
         .note: [.close]
     ]
+
+    /// configuring the button type for each page's action buttons
     private static let actionButtonType: [Page: HorizonUI.ButtonStyles.ButtonType] = [
         .assist: .whiteOutline
     ]
+
+    /// configuring the icon for each page title, when a page has an icon in the title
     private static let iconPages: [Page: Image] = [
         .assist: .huiIcons.aiFilled,
         .inboxAnnouncement: .huiIcons.announcement,
         .note: .huiIcons.menuBookNotebook,
         .notebook: .huiIcons.menuBookNotebook
     ]
+
+    /// configuring which pages show the institution logo in the leading position
     private static let institutionLogoPages: [Page] = [
         .dashboard,
         .skillspace
     ]
+
+    /// configuring the titles for each page
     private static let titles: [Page: String] = [
         .createMessage: .init(localized: "Create message", bundle: .horizon),
         .note: .init(localized: "Notebook", bundle: .horizon),
@@ -117,7 +135,6 @@ struct HTitleBar: View {
     ]
 
     // MARK: - Dependencies
-
     private let background: Color
     private let leading: AnyView?
     private let title: AnyView?
@@ -125,7 +142,6 @@ struct HTitleBar: View {
     private let trailingSpace: CGFloat
 
     // MARK: - Init
-
     init(
         page: Page,
         title: String? = nil,
@@ -192,7 +208,6 @@ struct HTitleBar: View {
     }
 
     // MARK: - Body
-
     var body: some View {
         let leadingView = leading == nil && trailing != nil ? AnyView(trailing?.opacity(0)) : leading
         let trailingView = trailing == nil && leading != nil ? AnyView(leading?.opacity(0)) : trailing
