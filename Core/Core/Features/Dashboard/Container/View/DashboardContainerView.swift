6bd7e394b48d5ba410828b526c8b81b304326a7a
Petky Benedek <petkybenedek@gmail.com>
2025-11-21 20:26:43 +0100

    Correct license header

--- Core/Core/Features/Dashboard/Container/View/DashboardContainerView.swift
+++ Core/Core/Features/Dashboard/Container/View/DashboardContainerView.swift
@@ -1,6 +1,8 @@
 //
 // This file is part of Canvas.
+// Copyright (C) 2020-present  Instructure, Inc.
 //
+// This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
