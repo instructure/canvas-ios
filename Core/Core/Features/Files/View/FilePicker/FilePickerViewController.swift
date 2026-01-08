e6a8db5995fc8e2974735868954f7eb73d0d941a
Suhaib Al-Absi <suhaib.alabsi@instructure.com>
2026-01-08 15:54:51 +0300

    [S] Disable Merging scanned images into PDF feature (#3832)
    build: Student
    [ignore-commit-lint]

--- Core/Core/Features/Files/View/FilePicker/FilePickerViewController.swift
+++ Core/Core/Features/Files/View/FilePicker/FilePickerViewController.swift
@@ -388,7 +388,9 @@ extension FilePickerViewController: UITableViewDelegate, UITableViewDataSource {
 extension FilePickerViewController: VNDocumentCameraViewControllerDelegate {
 
     private var shouldMergeScannedImagesIntoPDF: Bool {
-        utis.contains(where: { $0.isPDF }) && utis.contains(where: { $0.isImage }) == false
+        // TODO: Enable this back later. https://instructure.atlassian.net/browse/MBL-19654
+        // utis.contains(where: { $0.isPDF }) && utis.contains(where: { $0.isImage }) == false
+        return false
     }
 
     private func proposeFilenameForNewPDFScan() -> String {
