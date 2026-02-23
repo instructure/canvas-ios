87431ba3a20fb60f96bae8a3cd9337c85bdf03c9
Attila Varga <attila.varga@instructure.com>
2026-02-23 18:43:21 +0100

    Add business logic package.

---
+++ packages/BusinessLogic/Sources/BusinessLogic/BusinessLogic.CourseMock.swift
@@ -0,0 +1,38 @@
+// This file is part of Canvas.
+// Copyright (C) 2026-present  Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#if DEBUG
+
+extension BusinessLogic {
+
+    public final class CourseMock: Course {
+
+        public var shouldShowAsInvitedCourseReturnValue = false
+        public private(set) var shouldShowAsInvitedCourseReceivedArguments: (isCourseClosed: Bool, hasInvitedEnrollment: Bool)?
+
+        public init(shouldShowAsInvitedCourseReturnValue: Bool = false) {
+            self.shouldShowAsInvitedCourseReturnValue = shouldShowAsInvitedCourseReturnValue
+        }
+
+        public func shouldShowAsInvitedCourse(isCourseClosed: Bool, hasInvitedEnrollment: Bool) -> Bool {
+            shouldShowAsInvitedCourseReceivedArguments = (isCourseClosed, hasInvitedEnrollment)
+            return shouldShowAsInvitedCourseReturnValue
+        }
+    }
+}
+
+#endif
---
+++ packages/BusinessLogic/Tests/BusinessLogicTests/BusinessLogic.CourseTests.swift
@@ -0,0 +1,31 @@
+// This file is part of Canvas.
+// Copyright (C) 2025-present  Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import Testing
+@testable import BusinessLogic
+
+struct BusinessLogicCourseTests {
+    private let testee = BusinessLogic.CourseLive()
+
+    @Test
+    func shouldShowAsInvitedCourse() {
+        #expect(testee.shouldShowAsInvitedCourse(isCourseClosed: false, hasInvitedEnrollment: true))
+        #expect(!testee.shouldShowAsInvitedCourse(isCourseClosed: true, hasInvitedEnrollment: true))
+        #expect(!testee.shouldShowAsInvitedCourse(isCourseClosed: false, hasInvitedEnrollment: false))
+        #expect(!testee.shouldShowAsInvitedCourse(isCourseClosed: true, hasInvitedEnrollment: false))
+    }
+}
