82da10d72cc33f211425bedb8d2be116f175fed3
Petky Benedek <petkybenedek@gmail.com>
2026-03-20 19:16:49 +0100

    Remove unneccesary instanceID

--- Student/Student/LearnerDashboard/Widgets/WeeklySummaryWidget/ViewModel/WeeklySummaryWidgetViewModel.swift
+++ Student/Student/LearnerDashboard/Widgets/WeeklySummaryWidget/ViewModel/WeeklySummaryWidgetViewModel.swift
@@ -24,7 +24,6 @@ import SwiftUI
 @Observable
 final class WeeklySummaryWidgetViewModel: DashboardWidgetViewModel {
     let id: String = EditableWidgetIdentifier.weeklySummary.rawValue
-    let instanceID = UUID()
 
     private(set) var state: InstUI.ScreenState = .loading
     let config: DashboardWidgetConfig
