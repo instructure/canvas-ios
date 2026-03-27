03e02f8e326c45596c9176c486578fae4712fd63
petkybenedek <petkybenedek@gmail.com>
2026-03-27 16:15:59 +0100

    Remove newline

--- Horizon/Horizon/Sources/Features/LearningObjects/Assignment/SubmissionComment/View/SubmissionCommentViewModel.swift
+++ Horizon/Horizon/Sources/Features/LearningObjects/Assignment/SubmissionComment/View/SubmissionCommentViewModel.swift
@@ -121,7 +121,6 @@ final class SubmissionCommentViewModel {
             return
         }
         weak let weakSelf = self
-        
         fileState = .loading
         fileSubscription = fileInteractor.download(
             remoteURL: url,
