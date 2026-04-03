29571fc7ffb4daa3de1e971b89ec50830a9351b9
Suhaib Al-Absi <suhaib.alabsi@instructure.com>
2026-04-03 04:47:28 +0300

    Update FileDetailsViewController.swift

--- Core/Core/Features/Files/View/FileDetails/FileDetailsViewController.swift
+++ Core/Core/Features/Files/View/FileDetails/FileDetailsViewController.swift
@@ -820,7 +820,6 @@ extension FileDetailsViewController: PDFViewControllerDelegate, FlexibleToolbarC
     }
 
     public func coreWebView(_ webView: CoreWebView, didFinishAttachmentDownload attachment: CoreWebAttachment) {
-
         localURL = attachment.url
         shareButton.isEnabled = true
         spinnerView.isHidden = true
