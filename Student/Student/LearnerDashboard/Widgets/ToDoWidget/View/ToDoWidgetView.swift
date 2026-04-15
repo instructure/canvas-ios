8ac1549ae4e3328d942de36d962c71a897d4366e
Petky Benedek <petkybenedek@gmail.com>
2026-04-15 15:30:11 +0200

    Restore to-do widget accessibility mechanism

--- Student/Student/LearnerDashboard/Widgets/ToDoWidget/View/ToDoWidgetView.swift
+++ Student/Student/LearnerDashboard/Widgets/ToDoWidget/View/ToDoWidgetView.swift
@@ -162,6 +162,13 @@ struct ToDoWidgetView: View {
             weekDaysForOffset: { viewModel.weekDays(forOffset: $0) }
         )
         .padding(.horizontal, 32)
+        .accessibilityRepresentation {
+            HStack(spacing: 0) {
+                weekNavigationButton(toPrevious: true)
+                ToDoWidgetWeekView(weekDays: viewModel.currentWeekDays, viewModel: viewModel)
+                weekNavigationButton(toPrevious: false)
+            }
+        }
     }
 
     private var weekNavigationButtons: some View {
@@ -172,6 +179,7 @@ struct ToDoWidgetView: View {
             weekNavigationButton(toPrevious: false)
             .offset(x: 8)
         }
+        .accessibilityHidden(true)
     }
 
     private func weekNavigationButton(toPrevious: Bool) -> some View {
