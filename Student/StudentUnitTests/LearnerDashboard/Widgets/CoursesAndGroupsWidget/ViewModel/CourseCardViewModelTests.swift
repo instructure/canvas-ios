1f4cea6878a3ce3fd494d88e882079431ae8622c
Petky Benedek <petkybenedek@gmail.com>
2026-04-24 13:11:34 +0200

    Correct tests

--- Student/StudentUnitTests/LearnerDashboard/Widgets/CoursesAndGroupsWidget/ViewModel/CourseCardViewModelTests.swift
+++ Student/StudentUnitTests/LearnerDashboard/Widgets/CoursesAndGroupsWidget/ViewModel/CourseCardViewModelTests.swift
@@ -67,7 +67,6 @@ final class CourseCardViewModelTests: StudentTestCase {
         ))
 
         XCTAssertEqual(testee.courseID, testData.id)
-        XCTAssertEqual(testee.id, testData.id + "false")
         XCTAssertEqual(testee.title, testData.title)
         XCTAssertEqual(testee.courseColor, testData.color)
         XCTAssertEqual(testee.imageUrl, testData.imageUrl)
@@ -82,17 +81,14 @@ final class CourseCardViewModelTests: StudentTestCase {
 
         // selection contains id
         env.userDefaults?.offlineSyncSelections = ["courses/\(testData.id)"]
-        XCTAssertEqual(testee.id, testData.id + "true")
         XCTAssertEqual(testee.isAvailableOffline, true)
 
         // selection does not contain id
         env.userDefaults?.offlineSyncSelections = ["courses/other_course"]
-        XCTAssertEqual(testee.id, testData.id + "false")
         XCTAssertEqual(testee.isAvailableOffline, false)
 
         // selection is empty
         env.userDefaults?.offlineSyncSelections = []
-        XCTAssertEqual(testee.id, testData.id + "false")
         XCTAssertEqual(testee.isAvailableOffline, false)
     }
 
